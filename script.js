bb2a75c41154fcc5fb5ddade953fc25326dad8bb
Jander Liborio <jandernunes14@gmail.com>
2021-03-09 19:15:48 -0400

    tetando criar divs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-window.onload = function() {
-  
+// const color = [black, red, blue, green];
+const colorPalette = document.querySelector('#color-palette');
+for (let index = 0; index < colorPalette.length; index += 1) {
+  let colorGround = color[index];
 }
