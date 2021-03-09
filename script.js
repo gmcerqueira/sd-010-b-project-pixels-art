f25208f7716976de3b5920f220a2923912b37250
Junior030 <juniorhenrique030@gmail.com>
2021-03-09 18:15:35 -0300

    Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel.

--- script.js
+++ script.js
@@ -23,16 +23,13 @@ function addQuadradoPixels() {
   const boxPixel = document.getElementById('pixel-board');
   const table = document.createElement('table');
   boxPixel.appendChild(table);
-
-  const rowPixel = document.getElementById('pixel-board').getElementsByTagName('table');
- 
+  const rowPixel = boxPixel.getElementsByTagName('table');
   for (let index = 0; index < 5; index += 1) {
     const tr = document.createElement('tr');
     rowPixel[0].appendChild(tr);
   }
-
-  const columnPixel = document.getElementById('pixel-board').getElementsByTagName('tr');
-  for (let i = 0; i < 5; i += 1) {    
+  const columnPixel = boxPixel.getElementsByTagName('tr');
+  for (let i = 0; i < 5; i += 1) {
     for (let j = 0; j < 5; j += 1) {
       const td = document.createElement('td');
       td.className = 'pixel';
