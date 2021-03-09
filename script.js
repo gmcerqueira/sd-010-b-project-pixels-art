dfe14d615fd6c099368d077e752b6c4aacfb91ba
hzozimo <hzozimo@gmail.com>
2021-03-09 19:05:21 -0300

    desadio 8 feito

--- script.js
+++ script.js
@@ -3,8 +3,28 @@ function coresAleatórias() {
   return cor;
 }
 
+function criaPixels() {
+  const n = 5;
+  const tabela = document.getElementById('pixel-board');
+  for (let linha = 0; linha < n; linha += 1) {
+    const criaLinha = document.createElement('tr');
+    tabela.appendChild(criaLinha);
+    for (let celula = 0; celula < n; celula += 1) {
+      const criaCelula = document.createElement('td');
+      tabela.appendChild(criaCelula).className = 'pixel';
+      document.getElementsByClassName('pixel');
+      let apoio = document.getElementsByClassName('pixel');
+      apoio[apoio.length - 1].id = `cell-${linha+1}-${celula+1}`;
+      apoio[apoio.length - 1].style.background = '#ffffff';
+    }
+  }
+}
+
+criaPixels();
+
 function colocaCores() {
   const pixelCores = document.getElementsByClassName('color');
+  pixelCores[0].style.background = '#000000';
   for (let index = 1; index < pixelCores.length; index += 1) {
     pixelCores[index].style.background = coresAleatórias();
   }
@@ -64,20 +84,6 @@ verificaCoresUm();
 verificaCoresDois();
 verificaCoresTres();
 
-function criaPixels() {
-  const n = 5;
-  const tabela = document.getElementById('pixel-board');
-  for (let linha = 0; linha < n; linha += 1) {
-    const criaLinha = document.createElement('tr');
-    tabela.appendChild(criaLinha);
-    for (let celula = 0; celula < n; celula += 1) {
-      const criaCelula = document.createElement('td');
-      tabela.appendChild(criaCelula).className = 'pixel';
-    }
-  }
-}
-
-criaPixels();
 let pixelPreto = document.getElementById('black');
 let pixelCorUm = document.getElementById('corUm');
 let pixelCorDois = document.getElementById('corDois');
@@ -108,3 +114,12 @@ pixelPreto.addEventListener('click', pegaCorPreta);
 pixelCorUm.addEventListener('click', pegaCorUm);
 pixelCorDois.addEventListener('click', pegaCorDois);
 pixelCorTres.addEventListener('click', pegaCorTres);
+
+function criarListenerCelulas() {
+  let quadro = document.getElementById('pixel-board');
+  quadro.addEventListener('click', function pintar(event){
+    event.target.style.background = document.getElementsByClassName('selected')[0].style.background;
+  });
+}
+
+criarListenerCelulas();
