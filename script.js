893129caba26db743fcbeb644242cd0cffd5d633
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-09 18:09:40 -0300

    Consertando o bonus

--- script.js
+++ script.js
@@ -10,28 +10,40 @@ firstBoxColor.nextElementSibling.nextElementSibling.nextElementSibling.style.bac
 
 //Criando um quadro de pixels dinamicamente
 const inputNumber = document.querySelector("#board-size")
+let count = 1;
 
 function buildBoard(){
    
 
-    if(inputNumber.value == ''){
-        alert("Board inválido!")
-        console.log(inputNumber.value);
+    if(inputNumber.value == '' ){
+        if(count !== 1){
+            alert("Board inválido!");
+        }
+        lines = 5;
+        columns = 5;
+        count += 1;
+    }
+    else if(inputNumber.value >= 5 && inputNumber.value <= 50){
+        lines = inputNumber.value;
+        columns = inputNumber.value;   
+    }
+    else if(inputNumber.value < 5){
+        lines = 5;
+        columns = 5; 
     }
     else{
-        if(document.querySelector('#pixel-board') !== null){
-            document.querySelector('#pixel-board').remove()
-            lines = inputNumber.value;
-            columns = inputNumber.value;
-            divTable = document.createElement('div');
-            divTable.id = 'pixel-board';
-        }    
-        else{
-            lines = inputNumber.value;
-            columns = inputNumber.value;
-            divTable = document.createElement('div');
-            divTable.id = 'pixel-board';
-        }
+        lines = 50;
+        columns = 50;
+    }
+
+    if(document.querySelector('#pixel-board') !== null){
+        document.querySelector('#pixel-board').remove();
+        divTable = document.createElement('div');
+        divTable.id = 'pixel-board';
+    }    
+    else{
+        divTable = document.createElement('div');
+        divTable.id = 'pixel-board';
     }
 
     document.body.appendChild(divTable);
@@ -48,26 +60,14 @@ function buildBoard(){
         }
     }
 
-    let colorArray = document.querySelectorAll(".color")
-    for (let i = 0; i < colorArray.length; i += 1) {
-        colorArray[i].addEventListener("click", function(event) {
-            for (let j = 0; j < colorArray.length; j += 1){
-                colorArray[j].className = 'color';
-            }
-            event.target.className = 'color selected';
-        }) 
-    }
-
 
     // console.log(document.querySelector('.color').className)
     let pixelArray = document.querySelectorAll(".pixel");
 
     for (let i = 0; i < pixelArray.length; i += 1) {
-        console.log('aqui1');
         pixelArray[i].addEventListener("click", function(event) {
             let selectedColor = document.querySelector(".color.selected")
             event.target.style.backgroundColor = selectedColor.style.backgroundColor;
-            console.log('aqui');
         }) 
     }
 
@@ -79,7 +79,20 @@ function buildBoard(){
         } 
     })
 
-}
+    }
+
+
+    let colorArray = document.querySelectorAll(".color")
+        for (let i = 0; i < colorArray.length; i += 1) {
+            colorArray[i].addEventListener("click", function(event) {
+                for (let j = 0; j < colorArray.length; j += 1){
+                    colorArray[j].className = 'color';
+                }
+                event.target.className = 'color selected';
+            }) 
+    }
+
+buildBoard();
 
 let generateButton = document.querySelector("#generate-board");
 generateButton.addEventListener("click", buildBoard);
