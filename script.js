a950846919d20e5a217998eb6bdd0b807845dd6e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-09 15:34:09 -0300

    Requisito 1 realizado

---
+++ script.js
@@ -0,0 +1,6 @@
+const elementMain = document.querySelector('main');
+let title = document.createElement('h1');
+title.id = 'title';
+title.innerText = "Paleta de Cores";
+
+elementMain.appendChild(title);
