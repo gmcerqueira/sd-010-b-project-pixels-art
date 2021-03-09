0300800b057560ac7bf0fc2db289f2412a673114
Matheus Bodra <matheusbodra@gmail.com>
2021-03-09 09:25:08 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ let coresDaPaleta = document.getElementsByClassName("color");
 coresDaPaleta[0].style.backgroundColor = "black";
 coresDaPaleta[1].style.backgroundColor = "purple";
 coresDaPaleta[2].style.backgroundColor = "pink";
-coresDaPaleta[3].style.backgroundColor = "magenta"
+coresDaPaleta[3].style.backgroundColor = "magenta";
+
+let quadroDePixels = document.createElement("table");
+quadroDePixels.id = "pixel-board";
+document.body.appendChild(quadroDePixels);
+
+for (i=0; i<5; i++) {
+    let tr = document.createElement("tr");
+    quadroDePixels.appendChild(tr);
+}
+
+let linhasTabela = document.querySelectorAll("#pixel-board tr");
+for (i=0; i<linhasTabela.length; i++) {
+    for (j=0; j<linhasTabela.length; j++) {
+        let cedulasTabela = document.createElement("td");
+        cedulasTabela.className = "pixel"
+        linhasTabela[i].appendChild(cedulasTabela);
+    }
+}
