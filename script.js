7790c23f15eff4c8d5bb62d382774f8f96852b17
Italo Matos <italomatos764@gmail.com>
2021-03-09 17:00:37 -0400

    Adicionando paleta de cores

---
+++ script.js
@@ -0,0 +1,11 @@
+var corBlack = document.querySelectorAll('.color')[0];
+corBlack.style.backgroundColor = 'black';
+
+var corRed = document.querySelectorAll('.color')[1];
+corRed.style.backgroundColor = 'red';
+
+var corBlue = document.querySelectorAll('.color')[2];
+corBlue.style.backgroundColor = 'blue';
+
+var corGreen = document.querySelectorAll('.color')[3];
+corGreen.style.backgroundColor = 'green';
