7621c8a6b227a4b2be549f62aa6006c7dadbe5d8
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-09 16:36:34 -0300

    add exercicio 4 - add o quadro de pixels

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createPaletteColors() {
   const createBoxColor = document.createElement('div');
   createBoxColor.className = 'color';
-  let colors = ['black', 'blue', 'green', 'red'];
+  const colors = ['black', 'blue', 'green', 'red'];
   const boxColor = document.getElementsByClassName('color');
 
   for (let index = 0; index < boxColor.length; index += 1) {
