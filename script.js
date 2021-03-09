65346c460f5e20e4fa5951cda082ad857332c5dd
Misael Bueno <misa.hbueno@gmail.com>
2021-03-09 17:46:49 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+let selected = document.querySelectorAll(".color");
 
+for (let index = 0; index < selected.length; index+=1) {
+  selected[index].addEventListener("click", changeSelected)
+} 
 
+function changeSelected(event) {
+  let selectElement = document.querySelector(".selected")
+  selectElement.classList.remove("selected")
+  event.target.classList.add("selected")
+}
