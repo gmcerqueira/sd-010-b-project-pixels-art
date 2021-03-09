506b23ab674ddea13ac0da3975b91e9b109bcfa2
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-09 16:49:13 -0300

    16:49

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ for (let index = 0; index < 25; index += 1) {
   li.className = 'pixel';
   ul.appendChild(li);
 }
-
-// ### 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
-
-// - O elemento da cor preta deve possuir, inicialmente, a `classe` `selected`;
-
-// - Note que o elemento que deverá receber a classe `selected` deve ser um dos elementos que possuem a classe `color`, como especificado no **requisito 2**.
-
 document.getElementsByClassName('color')[0].className += ' selected';
+
+// A `classe` `selected` deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
+
+// - Somente uma das cores da paleta deve ter a `classe` `selected` de cada vez;
+
+// - Note que os elementos que deverão receber a classe `selected` devem ser os mesmos elementos que possuem a classe `color`, como especificado no **requisito 2**.
+
