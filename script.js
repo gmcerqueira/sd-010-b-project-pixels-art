205b985d7d82106f845dcc2aa68d0239cc3bb660
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-09 16:46:15 -0300

    Adiciona requisito 4. Fiz com tabela, mas acredito não ser a melhor forma. Verificar depois.

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+}
+
+// //Cria pixels na matriz.
+// function createPixel(className) {
+//   let pixel = document.createElement("div");
+//   pixel.className = className;
+//   return pixel;
+// }
+
+
