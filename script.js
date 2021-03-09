dc0ff8bcf0a91e4b37f3a0f310a7917e116c764c
Paulo Xavier <pxavierf@gmail.com>
2021-03-09 16:09:25 -0300

    corrigindo lint, id titlo no h1, bcg dos pixels, tamanha da section que engloba os pixels

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function createPixelBoard (numberPixels) {
-  const pixelBoard = document.getElementById('pixel-board');  
+function createPixelBoard(numberPixels) {
+  const pixelBoard = document.getElementById('pixel-board');
   for (let column = 0; column < numberPixels; column += 1) {
     const createColumn = document.createElement('div');
     createColumn.className = 'tr';   
-    pixelBoard.appendChild(createColumn)
+    pixelBoard.appendChild(createColumn);
     for (let row = 0; row < numberPixels; row += 1) {
       const createRow = document.createElement('div');
       createRow.className = 'pixel td';
-      createColumn.appendChild(createRow)
+      createColumn.appendChild(createRow);
     }
-  }  
-}
-
-createPixelBoard(5)
-
+  }
+};
+createPixelBoard(5);
