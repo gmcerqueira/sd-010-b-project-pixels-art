abdb637e5470e7a37548bf032e113013b4faff0f
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-09 16:01:28 -0300

    Adiciona à página um quadro de pixels, com 25 pixels

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 
 firstBoxColor = document.querySelector('.color');
 
-firstBoxColor.style.backgroundColor = 'black'
-firstBoxColor.nextElementSibling.style.backgroundColor = '#CEA968'
-firstBoxColor.nextElementSibling.nextElementSibling.style.backgroundColor = '#E4C083'
-firstBoxColor.nextElementSibling.nextElementSibling.nextElementSibling.style.backgroundColor = '#08AAE3'
+firstBoxColor.style.backgroundColor = 'black';
+firstBoxColor.nextElementSibling.style.backgroundColor = '#CEA968';
+firstBoxColor.nextElementSibling.nextElementSibling.style.backgroundColor = '#E4C083';
+firstBoxColor.nextElementSibling.nextElementSibling.nextElementSibling.style.backgroundColor = '#08AAE3';
 // Pegando os elementos de depois
 
+//Criando um quadro de pixels dinamicamente
+
+divTable = document.createElement('div');
+divTable.id = 'pixel-board';
+lines = 5;
+columns = 5;
+
+document.body.appendChild(divTable);
+
+for(let index = 0; index < lines; index +=1){
+    divTr = document.createElement('div');
+    divTr.className = 'tr';
+    divTable.appendChild(divTr);
+    for(let index = 0; index < columns; index +=1){
+        divBox = document.createElement('div');
+        divBox.className = 'pixel';
+        divTr.appendChild(divBox);
+    }
+}
