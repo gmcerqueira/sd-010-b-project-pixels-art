a34db2af72f2caee9bbce70c2d7038531c3bbd1c
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-09 16:28:30 -0300

    primeira cor da paleta preta

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ for (let index = 0; index < 4; index += 1) {
   atribuirPaleta(criarDiv());
 }
 
+// adiciona cores aleatorias as paletas
 function adicionarBgcolor() {
   const getDiv = document.getElementsByClassName('color');
   for (let index = 0; index < getDiv.length; index += 1) {
@@ -24,8 +25,14 @@ function adicionarBgcolor() {
     const randomizer2 = Math.round(Math.random() * 255);
     const randomizer3 = Math.round(Math.random() * 255);
     const randomRgb = `rgb(${randomizer}, ${randomizer2}, ${randomizer3})`;
-    getDiv[index].style.backgroundColor = randomRgb;
-    getDiv[index].id = randomRgb;
+    if (index === 0) {
+      getDiv[0].style.backgroundColor = 'black';
+      getDiv[index].id = 'black';
+    }
+    else {
+      getDiv[index].style.backgroundColor = randomRgb;
+      getDiv[index].id = randomRgb;
+    }
   }
   return getDiv;
 }
