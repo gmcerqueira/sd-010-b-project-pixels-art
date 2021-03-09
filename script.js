74539439261c6f1bf5dd3f291979fe8341d15cd9
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-09 19:13:44 -0300

    Correções

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let selectedColor = 'black';
 let oldColor;
 
-document.addEventListener('click', event => {
+document.addEventListener('click', (event) => {
   if (event.target.classList.contains('color')) {
-      oldColor = document.getElementById(selectedColor);
-      oldColor.classList.remove('selected');
-      selectedColor = event.target.id;
-      event.target.classList.add('selected');
+    oldColor = document.getElementById(selectedColor);
+    oldColor.classList.remove('selected');
+    selectedColor = event.target.id;
+    event.target.classList.add('selected');
   }
 });
