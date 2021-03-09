092f00e6206e3fbbb6b6d6c8f25e51d7cf607897
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-09 19:36:28 -0300

    Resolvendo requisito 3

--- script.js
+++ script.js
@@ -12,46 +12,27 @@ function structureTable () {
     line1.appendChild(column);
   } 
 } structureTable();
-let line = document.getElementsByTagName("div");
-let column = document.querySelectorAll(".color");
 
-// function randomColor () {
-//   let numbers = "0123456789ABCDEF";
-//   let color = "#";
-//   for ( let i = 0; i < 6; i += 1) {
-//     color += numbers[Math.floor(Math.random() * 16)];
-//   } 
-//   return color
-// } // https://stackoverflow.com/questions/1484506/random-color-generator
-
-// function styleTable () {
-//   let random = $(".color").css("background-color", getRandomColor());
-//   for (let index = 0; index < column.length; index += 1) {
-//     column[index] = random;
-//   }
-// } styleTable()
 function styleTable () {
-  for (let index = 0; index < column.length; column += 1){
+  let column = document.querySelectorAll(".color");
+  for (let index = 0; index < column.length; index += 1){
     column[0].style.backgroundColor = "black";
     column[1].style.backgroundColor = "green";
     column[2].style.backgroundColor = "red";
     column[3].style.backgroundColor = "blue";
-  }
-} styleTable();
-// function selectColor () {
-//   for (let index = 0; index < column.length; column += 1){
-//     column.className = "select-color";
-//   }
-// }
+  } 
+} 
+styleTable();
 
 function pixelStructure () {
   for (let lines = 0; lines < 5; lines += 1) {  
     let pixelLine = document.createElement("div");
+    pixelLine.className = "pixelLine"
     pixel.appendChild(pixelLine);
     
     for (let columns = 0; columns < 5; columns += 1) {
       let column1 = document.createElement("div");
-      column1.className = "pixel";
+      column1.className = "pixelColumn";
       pixelLine.appendChild(column1);
     }
   }
