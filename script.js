d1af90e29629930b1dc5574fa064ca62d91d1111
André De Bem <andrebemferraz@gmail.com>
2021-03-09 15:59:50 -0300

    Request II

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //Starting
+
+document.querySelectorAll(".color")[0].style.backgroundColor = "red"
+document.querySelectorAll(".color")[1].style.backgroundColor = "orange"
+document.querySelectorAll(".color")[2].style.backgroundColor = "lightgreen"
+document.querySelectorAll(".color")[3].style.backgroundColor = "blue"
