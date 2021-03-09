6c5991c89c4de3105f7306b4fe1942777fb77e09
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-09 16:06:18 -0300

    Criado o requisito 4 - Adicione à página um quadro de pixels, com 25 pixels

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 window.onload = function () {
+  let numberOfOption = 4;
+  let numberOfItems = 25;
 
-    let numberOfOption = 4;
+  const elementHeader = document.createElement('header');
+  document.body.appendChild(elementHeader);
 
-    const elementHeader = document.createElement('header');
-    document.body.appendChild(elementHeader);
+  const elementTitulo = document.createElement('h1');
+  elementTitulo.id = 'title';
+  elementTitulo.innerText = 'Paleta de Cores';
+  elementHeader.appendChild(elementTitulo);
 
-    const elementTitulo = document.createElement('h1');
-    elementTitulo.id = 'title';
-    elementTitulo.innerText = 'Paleta de Cores';
-    elementHeader.appendChild(elementTitulo);
+  const sectionSelectColors = document.createElement('section');
+  sectionSelectColors.className = 'color-select';
+  sectionSelectColors.id = 'color-palette';
+  document.body.appendChild(sectionSelectColors);
 
-    const sectionSelectColors = document.createElement('section');
-    sectionSelectColors.className = 'color-select';
-    sectionSelectColors.id = 'color-palette';
-    document.body.appendChild(sectionSelectColors);
+  for (let index = 0; index < numberOfOption; index++) {
+    const colorOption = document.createElement('div');
+    colorOption.className = 'color';
+    colorOption.id = 'cor_' + (index + 1);
+    sectionSelectColors.appendChild(colorOption);
+  }
 
-    for (let index = 0; index < numberOfOption; index++) {
-        const colorOption = document.createElement('div');
-        colorOption.className = 'color';
-        colorOption.id = 'cor_'+(index+1);        
-        sectionSelectColors.appendChild(colorOption);        
-    }
+  const pixelBoard = document.createElement('section');
+  pixelBoard.id = 'pixel-board';
+  document.body.appendChild(pixelBoard);
 
-    
+  for (let index = 0; index < numberOfItems; index++) {
+    const itemOption = document.createElement('div');
+    itemOption.className = 'pixel';
+    pixelBoard.appendChild(itemOption);
+  }
+
+
 
 };
