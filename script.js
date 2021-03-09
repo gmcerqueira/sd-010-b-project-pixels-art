ea1925f49781ef5f790b1675767b8cd62d7f92cc
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-09 20:04:14 -0300

    Resolvido erros no lint no requisito 4

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ function createSquarePixel() {
   for (let index = 0; index < 25; index += 1) {
     const box2 = document.createElement('div');
     box2.className = 'pixel';
-    squearePixel.appendChild(box2);  
+    squearePixel.appendChild(box2);
   }
   return squearePixel;
 }
 createSquarePixel();
 
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[0].className += ' selected';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
