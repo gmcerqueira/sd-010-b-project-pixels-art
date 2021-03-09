25a0906448540d74c81562c7b872df0cc1506ace
André De Bem <andrebemferraz@gmail.com>
2021-03-09 16:40:05 -0300

    Request IV

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //Starting
 
+window.onload = whitePixels()
+
 document.querySelectorAll(".color")[0].style.backgroundColor = "black"
 document.querySelectorAll(".color")[1].style.backgroundColor = "red"
 document.querySelectorAll(".color")[2].style.backgroundColor = "lightgreen"
 document.querySelectorAll(".color")[3].style.backgroundColor = "blue"
+
+function whitePixels () {
+  let pixels = document.querySelectorAll(".pixel");
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = "white"
+  }
+}
