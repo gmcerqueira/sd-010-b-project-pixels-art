e1763ec9e9166d3c91486ebaa2e78eca656af1cc
Jessie <miss_sc_jessi@hotmail.com>
2021-03-09 16:05:25 -0300

    requisto 4 e tvz 5

---
+++ script.js
@@ -0,0 +1,4 @@
+let quadrado2 = document.querySelectorAll('.color')[1]
+document.getElementById('#button1').addEventListener('click', function(){
+    quadrado2.style.backgroundColor = 'yellow';
+});
