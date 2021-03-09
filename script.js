899f49467705dd4916a9d0b636a6634bea99fff5
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-09 17:20:07 -0300

    finished the dinamic grid size function

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-//Elements
+window.onload = () => {
+  start();
+};
+
+// Generic Create Element function
+
+const createGenElement = (
+  elName,
+  className = '',
+  contentText = '',
+  idValue = ''
+) => {
+  const elementCreated = document.createElement(elName);
+  elementCreated.textContent = contentText;
+  if (className.length > 0) elementCreated.setAttribute('class', className);
+  if (idValue.length > 0) elementCreated.setAttribute('id', idValue);
+  return elementCreated;
+};
+
+// Elements
 
 const palletes = Array.from(document.querySelectorAll('.color'));
+const pixelBoard = document.querySelector('#pixel-board');
 
-//Setting pallete colors
+// Setting pallete colors
 const setPalleteColors = () => {
   let r = Math.ceil(Math.random() * 84);
   let g = Math.ceil(Math.random() * 84);
@@ -18,4 +38,29 @@ const setPalleteColors = () => {
     }
   });
 };
-setPalleteColors();
+
+// Set Pixel Grid by x Amount
+
+const setPixelGrid = (size) => {
+  for (let row = 0; row < size; row += 1) {
+    const tr = document.createElement('tr');
+
+    for (let column = 0; column < size; column += 1) {
+      const td = createGenElement('td', 'pixel');
+      if (column > 0) {
+        td.style.borderLeft = 0;
+      }
+      if (row > 0) {
+        td.style.borderTop = 0;
+      }
+      tr.appendChild(td);
+    }
+
+    pixelBoard.appendChild(tr);
+  }
+};
+
+function start() {
+  setPalleteColors();
+  setPixelGrid(5);
+}
