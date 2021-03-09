c503d17b83fae1d134978706a01e675a79f31545
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-09 19:33:54 -0300

    Adicionei cores nas paletas

---
+++ script.js
@@ -0,0 +1,10 @@
+const paletaCores = document.getElementsByClassName("color");
+const preto = paletaCores[0];
+let vermelho = paletaCores[1];
+let verde = paletaCores[2];
+let azul = paletaCores[3];
+
+preto.style.backgroundColor = 'black';
+vermelho.style.backgroundColor = 'red';
+verde.style.backgroundColor = 'green';
+azul.style.backgroundColor = 'blue';
