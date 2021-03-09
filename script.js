29d91ebf0876cfbab415b6ea9489f4e626306f96
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-09 17:47:41 -0300

    Criando a paleta de cores dinamicamente

---
+++ script.js
@@ -0,0 +1,17 @@
+const pallete = document.getElementById('color-palette');
+let arrayPallete = [1, 2, 3, 4];
+
+function viewPallete() {
+  for (let index = 0; index < arrayPallete.length; index += 1) {
+    let box = document.createElement('div');
+    box.className = 'color';
+    pallete.appendChild(box);
+  }
+  return pallete;
+}
+viewPallete();
+
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
