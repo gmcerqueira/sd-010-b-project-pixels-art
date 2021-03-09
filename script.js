77cd0b820045fe19712198e6b556cd6806257fd5
limaluizpaulo <limaluizpaulo@live.com>
2021-03-09 16:51:54 -0300

    add function checks that no other color in the palette has the class selected

---
+++ script.js
@@ -0,0 +1,20 @@
+const black = document.getElementsByClassName('black')[0];
+const orangered = document.getElementsByClassName('orangered')[0];
+const green = document.getElementsByClassName('green')[0];
+const blue = document.getElementsByClassName('blue')[0];
+
+const paletaCores = [black, orangered, green, blue];
+
+function selectColor(event) {
+  for (index in paletaCores ) {
+    if (paletaCores[index].classList.contains('selected')) {
+      paletaCores[index].classList.remove('selected');
+    }
+  }
+  event.target.classList.add('selected');
+}
+
+black.addEventListener('click', selectColor);
+orangered.addEventListener('click', selectColor);
+green.addEventListener('click', selectColor);
+blue.addEventListener('click', selectColor);
