4e1b6afbfcde48eb96849930800ececba7b5afc5
Junior030 <juniorhenrique030@gmail.com>
2021-03-09 17:14:20 -0300

    Adicione a cor preta como a primeira cor da paleta de cores.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
   const corRGB = document.querySelectorAll('.color');
   let RGB = '';
 
-  for (let j = 0; j < corRGB.length; j += 1) {
+  for (let j = 1; j < corRGB.length; j += 1) {
     for (let i = 0; i < 3; i += 1) {
       cor = Math.floor(Math.random() * 255);
       if (i === 2){
@@ -13,6 +13,7 @@ window.onload = function () {
       }
     }
     corRGB[j].style.background = 'rgb(' + RGB + ')';
+    corRGB[0].style.background = 'rgb(000, 000, 000)';
     RGB = '';
   }
 }
