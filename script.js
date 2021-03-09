a82d289ff254a93edc0fb15e1e516be52f26c7ef
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-09 15:50:26 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function inicializeColor() {
-  let colorsPalet = document.querySelectorAll(".color");
+  const colorsPalet = document.querySelectorAll(".color");
   for (let color = 0; color < colorsPalet.length; color++) {
     if(color == 0){
       colorsPalet[color].style.backgroundColor = "black";
     }else if (color == 1) {
-      colorsPalet[color].style.backgroundColor = "green";
-    }else if(color == 2){
       colorsPalet[color].style.backgroundColor = "red";
+    }else if(color == 2){
+      colorsPalet[color].style.backgroundColor = "green";
     }else if(color == 3){
       colorsPalet[color].style.backgroundColor = "blue";
     }
@@ -14,3 +14,21 @@ function inicializeColor() {
 }
 
 inicializeColor();
+
+function createBorad(height, width) {
+  const theBorad = document.querySelector("#pixel-board");
+
+  for (let boardHeight = 0; boardHeight < height; boardHeight++) {
+    let lineBoard = document.createElement("tr");
+    theBorad.appendChild(lineBoard);
+    for (let boardWidth = 0; boardWidth < width; boardWidth++) {
+      let lineToAdd = theBorad.lastElementChild;
+      let mkPixel = document.createElement("td");
+      mkPixel.className = "pixels";
+      mkPixel.style.background = "white"
+      lineToAdd.appendChild(mkPixel);
+    }
+  }
+}
+
+createBorad(5,5);
