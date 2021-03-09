d007c95afc56c334899f75552e727a43c8696922
Jessie <miss_sc_jessi@hotmail.com>
2021-03-09 17:49:19 -0300

    requisto 8

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const tabelaCores = document.querySelectorAll('.color');
+const pixel = document.getElementsByClassName('pixel');
 
-
-  function clicaCor() {
-    for (const cor of tabelaCores) {
-      cor.addEventListener('click', function (event) {
-        for (let index = 0; index < tabelaCores.length; index += 1) {
-          tabelaCores[index].classList.remove('selected');
-          event.target.classList.add('selected');
-          tabelaCores[index] = event.target;
-        }
-      });
-    }
+function clicaCor() {
+  for (let index = 0; index < tabelaCores.length; index += 1) {
+    tabelaCores[index].addEventListener('click', function (event) {
+      for (let index = 0; index < tabelaCores.length; index += 1) {
+        tabelaCores[index].classList.remove('selected');
+        event.target.classList.add('selected');
+        tabelaCores[index] = event.target;
+      }
+    });
   }
+}
 
-  clicaCor();
+clicaCor();
+
+function colorirPixel() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', function (event) {
+      let corSelecionada = window.getComputedStyle(
+        document.querySelector('.selected')
+      ).backgroundColor;
+      event.target.style.backgroundColor = corSelecionada;
+    });
+  }
+}
+
+colorirPixel();
