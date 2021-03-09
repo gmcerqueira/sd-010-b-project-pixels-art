ab46bb644253caeaf4aeafe23b7ef412d221e17e
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-09 20:19:58 -0300

    O requisito 4 foi completo porem de forma erronea apenas o criei para verificar como deve ser o for para roda-lo de forma dinamica

---
+++ script.js
@@ -0,0 +1,12 @@
+let pixelBoarder= document.getElementById("pixel-board");
+
+//recuperando a tabela para criar linhas e colunas
+
+for( let i = 0; i>5 ; i += 1){
+    pixelBoarder = document.createElement('tr');
+    pixelBoarder.appendChild('pixelBoarder');
+                    
+    }
+
+
+}
