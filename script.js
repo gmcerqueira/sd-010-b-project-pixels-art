b03dd682feabd4f0b3da899d6eae0ab79027bbed
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-09 15:44:06 -0300

    adicioando informaçoes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// criar div com classe color
 function criarDiv() {
   const novaDiv = document.createElement('div');
   novaDiv.className = 'color';
   return novaDiv;
 }
 
+// definir as divs a paleta
 function atribuirPaleta(divs) {
   const selecionarPaleta = document.querySelector('#color-palette');
   selecionarPaleta.appendChild(divs);
 }
+
+// definir numero de paletas de cores
 
 for (let index = 0; index < 4; index += 1) {
   atribuirPaleta(criarDiv());
