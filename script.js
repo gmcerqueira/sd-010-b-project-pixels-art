d6e85d86859ad16601fdd263c86e151ee073451d
legalvao1 <legalvao1@gmail.com>
2021-03-09 18:12:37 -0300

    Tableda dos pixels que serao coloridos

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ let corBLue = document.createElement('div');
 corBLue.className = 'color'
 corBLue.style.backgroundColor = 'blue';
 paleta.appendChild(corBLue);
+
+// table Row - Criando as cinco linhas
+
+let pixelBoard = document.querySelector('#pixel-board')
+for(let line = 0; line < 5; line += 1){
+    let box = document.createElement('tr');
+    pixelBoard.appendChild(box);
+}
+// celulas da tabela
+
+let linhas = document.querySelectorAll('#pixel-board, tr'); // recupero todos os elem tr criados da id #pixel-board
+for (let line = 1; line < linhas.length; line += 1) {
+    for (let colum = 1; colum < linhas.length; colum +=1){
+        let box = document.createElement('td')
+        box.className = 'pixel';
+        linhas[colum].appendChild(box);
+    }
+}
