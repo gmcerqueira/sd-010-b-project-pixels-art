8ab2c00551cb420dc440ac1265c3ff50c3404694
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-09 20:09:33 -0300

    Empacado no projeto.

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 window.onload = function () {
-
 }
-
-// //Cria pixels na matriz.
-// function createPixel(className) {
-//   let pixel = document.createElement("div");
-//   pixel.className = className;
-//   return pixel;
-// }
-
-
