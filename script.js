5b175afa5e6a068b6fe667e23da94de744468a0b
renatogsouza <renatomacedo1995@gmail.com>
2021-03-09 20:28:07 -0300

    alterando os ids

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-function colorPixels() {
 
-}
 function criaPixels() {
   let secao = document.querySelector('#secao');
-  let quadro =document.createElement('div');
-  quadro.className = 'pixel-border' ;
+  let novaLinha = document.createElement('div');
   for (let j = 0; j < 5; j++) {
-    let novaLinha = document.createElement('div');
+    let quadro =document.createElement('div');
+    quadro.className = 'pixel-border' ;
     for (let i = 0; i < 5; i++) {
       let divNova = document.createElement("div");
       divNova.className = 'pixel';
-      novaLinha.appendChild(divNova);
+      quadro.appendChild(divNova);
     }
-    document.pixel-border.appendChild(novaLinha);
+    document.body.appendChild(quadro);
   }
   document.body.appendChild(quadro);
 }
-// function pegaCor() {
-//   let cor = document.getElementsByClassName('color');  
-//   var theCSSprop = document.getComputedStyle(item, null).getPropertyValue("background-color");
-//   // document.getElementById("demo").innerHTML = theCSSprop;
-//   localStorage.setItem('cor', theCSSprop);
-// }
-//   document.querySelectorAll('.color').forEach(item => {
-//   item.addEventListener('click', pegaCor)// => {
-
-//   });
-// })
+criaPixels();
