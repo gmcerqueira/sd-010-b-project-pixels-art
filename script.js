9b172b6bb0384c1dafe85fb3dbd70ce9eb501cdf
renanpbraga <renanpbraga@gmail.com>
2021-03-09 19:33:31 -0300

    adiciona requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let paletteContainer = document.getElementById('color-palette');
+let pixelBoardContainer = document.getElementById('pixel-board');
 
 function criaDiv() {
     let paleta = [1,2,3,4];
@@ -11,6 +12,15 @@ function criaDiv() {
 }
 criaDiv();
 
+function criaTela() {;
+    for (j = 0; j < 25; j += 1) {
+        let criaPixel = document.createElement('div');
+        criaPixel.className = 'pixel';
+        pixelBoardContainer.appendChild(criaPixel);
+    }
+    return pixelBoardContainer;
+}
+criaTela();
 
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
