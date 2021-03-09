bf29669e01b5d89fea5f3e23dc7af568524a08dd
Paulo Xavier <pxavierf@gmail.com>
2021-03-09 15:58:44 -0300

    criando quadro de pixels

---
+++ script.js
@@ -0,0 +1,16 @@
+function createPixelBoard (numberPixels) {
+  const pixelBoard = document.getElementById('pixel-board');  
+  for (let column = 0; column < numberPixels; column += 1) {
+    const createColumn = document.createElement('div');
+    createColumn.className = 'tr';   
+    pixelBoard.appendChild(createColumn)
+    for (let row = 0; row < numberPixels; row += 1) {
+      const createRow = document.createElement('div');
+      createRow.className = 'pixel td';
+      createColumn.appendChild(createRow)
+    }
+  }  
+}
+
+createPixelBoard(5)
+
