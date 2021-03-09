9d853008c2bdb909173df0d6e3e58b190344fba5
Paulo Xavier <pxavierf@gmail.com>
2021-03-09 16:11:49 -0300

    corrigindo lint

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createPixelBoard(numberPixels) {
   const pixelBoard = document.getElementById('pixel-board');
   for (let column = 0; column < numberPixels; column += 1) {
     const createColumn = document.createElement('div');
-    createColumn.className = 'tr';   
+    createColumn.className = 'tr';
     pixelBoard.appendChild(createColumn);
     for (let row = 0; row < numberPixels; row += 1) {
       const createRow = document.createElement('div');
@@ -10,5 +10,5 @@ function createPixelBoard(numberPixels) {
       createColumn.appendChild(createRow);
     }
   }
-};
+}
 createPixelBoard(5);
