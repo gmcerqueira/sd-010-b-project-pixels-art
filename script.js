9357b0cef14d7154e54bab3d4ed12afddd1f1af5
bidaffonso <bidaffondo@gmail.com>
2021-03-09 16:16:29 -0400

    Commit com teste do requisito 2 e 3 finalizado

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-let colorPalette = document.createElement('table');
+const colorPalette = document.createElement('table');
 colorPalette.id = 'color-palette';
 document.body.appendChild(colorPalette);
 
-let sizePalette = 5;
-
-
+const sizePalette = 4;
 
 function createPalette() {
-  for(let index = 0; index < sizePalette; index += 1){
-    let square = document.createElement('td');
+  for (let index = 0; index < sizePalette; index += 1) {
+    const square = document.createElement('td');
     square.className = 'color';
     colorPalette.appendChild(square);
   }
-}
+};
 
 function colorSquare() {
-  let square = document.querySelectorAll('.color');
+  const square = document.querySelectorAll('.color');
   square[0].style.backgroundColor = 'black';
   square[1].style.backgroundColor = 'blue';
   square[2].style.backgroundColor = 'green';
   square[3].style.backgroundColor = 'red';
-  square[4].style.backgroundColor = 'yellow';
 };
-
-
 
 createPalette();
 colorSquare();
