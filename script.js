626b11f2dc3922f0af8761c0c3d66a84cc0c1cb7
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2021-03-09 18:59:33 -0300

    requisito 5 altura e lagura de pixels

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-function generatorPixels(qttPixel) { 
-  let pixelBoard = document.getElementById("pixel-board");
-  for(let i = 1; i <= qttPixel; i += 1) {
-    let divTr = document.createElement("div");
-    divTr.className = "tr";
-    creatDivTd(divTr, qttPixel);
-    pixelBoard.appendChild(divTr);
+function creatDivTd(divTr, qttPixel) {
+  for (let i = 1; i <= qttPixel; i += 1) {
+    const divTd = document.createElement('div');
+    divTd.className = 'pixel';
+    divTr.appendChild(divTd);
   }
 }
 
-function creatDivTd(divTr, qttPixel) {
-  for(let i = 1; i <= qttPixel; i += 1) {
-    let divTd = document.createElement("div");
-    divTd.className = "pixel";
-    divTr.appendChild(divTd);
+function generatorPixels(qttPixel) {
+  const pixelBoard = document.getElementById('pixel-board');
+  for (let i = 1; i <= qttPixel; i += 1) {
+    const divTr = document.createElement('div');
+    divTr.className = 'tr';
+    creatDivTd(divTr, qttPixel);
+    pixelBoard.appendChild(divTr);
   }
-
-  
 }
 
 generatorPixels(5);
