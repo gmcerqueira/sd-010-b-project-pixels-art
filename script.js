ec35741f47ab0f947a52c5d56ccab14520492b85
esionascimento <esio.392@protonmail.com>
2021-03-09 17:00:24 -0300

    7-somente uma cor paleta

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let corSelected = document.querySelectorAll('.pixel');
-function clickSelected() {
-
-  if(divSelect.className == 'task') {
-    divSelect.className = 'task selected';
+let corDivBoard = document.querySelector('#color-palette');
+let cor = document.querySelectorAll('.color');
+function clickSelected(eventoDeOrigem) {
+  let aux = eventoDeOrigem.target;
+  cor[0].className = 'color';
+  cor[1].className = 'color';
+  cor[2].className = 'color';
+  cor[3].className = 'color';
+  if(aux.className == 'color') {
+    aux.className = 'color selected';
   }else {
-    divSelect.className = 'task';
+    console.log('selecionado');
+    aux.className = 'color';
   }
 }
-// corSelected.addEventListener('click', clickSelected);
+corDivBoard.addEventListener('click', clickSelected);
