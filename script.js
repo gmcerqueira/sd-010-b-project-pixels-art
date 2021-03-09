d28682d6909ce26994a783dade1b9a120db67daa
Mariana Mohr <marianamohr@gmail.com>
2021-03-09 16:24:42 -0300

    Requisito 6 - criada classa selected

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-
-
-
 function creatPixelBoard() {
   const pai = document.getElementById('pixel-board');
   for (let i = 0; i < 5; i += 1) {
     for (let index = 0; index < 5; index += 1) {
-    const div = document.createElement('div');
-    div.className = 'pixel';
-    pai.appendChild(div);
+      const div = document.createElement('div');
+      div.className = 'pixel';
+      pai.appendChild(div);
+    }
   }
-
 }
-}
-
 window.onload = creatPixelBoard();
