8243cbd3d1c44f66c799bb43170de3190602c563
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-09 17:03:33 -0300

    Requisito 2 realizado

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const elementMain = document.querySelector('main');
-let title = document.createElement('h1');
-title.id = 'title';
-title.innerText = "Paleta de Cores";
+//cria uma div com a classe color
+function createDiv(idElement) {
+  let paletteDiv = document.createElement('div');
+  paletteDiv.className = 'color'
+  return paletteDiv
+}
+//recebe uma variavel e injeta na main
+function injectMain(element) {
+  const elementMain = document.querySelector('main');
+  elementMain.appendChild(element);
+  return elementMain;
+}
 
-elementMain.appendChild(title);
+// adiciona titulo
+const title = document.createElement('h1');
+title.id = 'title';
+title.innerText = 'Paleta de Cores';
+injectMain(title);
+
+//cria section para paleta de cores
+let colorPalette = document.createElement('section');
+colorPalette.id = 'color-palette';
+
+//cria divs e injeta em section e injeta section na main
+for (let index = 0; index < 4; index += 1) {
+  colorPalette.appendChild(createDiv());
+}
+injectMain(colorPalette);
+
+let firstDiv = document.querySelector('#color-palette').firstChild;
+firstDiv.id = 'black';
+let secondDiv = document.querySelector('#color-palette').firstChild.nextSibling;
+secondDiv.id = 'blue';
+let thirdDiv = document.querySelector('#color-palette').firstChild.nextSibling.nextSibling;
+thirdDiv.id = 'red';
+let fourthDiv = document.querySelector('#color-palette').lastChild;
+fourthDiv.id = 'yellow';
+
