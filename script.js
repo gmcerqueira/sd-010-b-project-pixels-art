6821f48c34fb21686b94bcd3f77931e94d5f9384
Paulo Xavier <pxavierf@gmail.com>
2021-03-09 17:53:29 -0300

    colocando cor nos pixels

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 createPixelBoard(5);
 selected();
+colorPixels();
 
 
 function createPixelBoard(numberPixels) {
@@ -24,7 +25,8 @@ function selected() {
         colorBox[i].classList.remove('selected');
       }
       colorBox[index].classList.add('selected');
-      changeColor();
+      changeColor();     
+      colorPixels();
     })
   }
 }
@@ -37,6 +39,20 @@ function changeColor() {
     const classListColor = colorBox[index].classList;
     if (classListColor[2] === 'selected') {      
       selected.style.backgroundColor = `${classListColor[1]}`;       
+    }
+    else if (classListColor.length <= 2){
+      colorBox[index].style.backgroundColor = classListColor[index];
     }      
+  }
+}
+
+function colorPixels() {  
+  const box = document.querySelectorAll('.td');
+  const selected = document.querySelector('.selected');
+
+  for (let index = 0; index < box.length; index += 1) {
+    box[index].addEventListener('click', () => {
+      box[index].style.backgroundColor = selected.style.backgroundColor
+    })
   }
 }
