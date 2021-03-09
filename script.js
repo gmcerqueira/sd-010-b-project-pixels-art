f0b95e7799ba10f1cb769872c3ef3d2369069316
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-09 16:49:36 -0300

    corrigindo alguns erros da 4 e 5

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-
+//exercicio 4
     let encotraQuadro = document.getElementById("pixel-board");
     for(let colona = 0;colona < 5;colona++){
    for(let linha = 0;linha < 5;linha++){
     let criarDiv = document.createElement('div');
         criarDiv.classList.add("pixel");
+        criarDiv.style.backgroundColor = "white"
         encotraQuadro.appendChild(criarDiv);
     }
     let criaBr = document.createElement("br");
     encotraQuadro.appendChild(criaBr);
 }
+window.onload = pintarQuadrados;
 
+function pintarQuadrados(){
+    console.log("teste")
+}
