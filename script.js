440e44e9874f4099ab261e25ba3b1380ab427a57
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-09 15:06:49 -0300

    Criado o header e o titulo via java script

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  const elementHeader = document.createElement('header');
+  document.body.appendChild(elementHeader);
+
+  const elementTitulo = document.createElement('h1');
+  elementTitulo.id = 'title';
+  elementTitulo.innerText = 'Paleta de Cores';
+  elementHeader.appendChild(elementTitulo);
+
+  console.log(document.body);
+};
