81d8660d0f8f29342121389a7b35e22012cf02df
Camila Arruda <camilaam1802@gmail.com>
2021-03-09 17:28:50 -0300

    Organizando O CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+let select = document.querySelectorAll(".color");
+for (let index = 0; index < select.length; index +=1){
+    select.addEventListener("click",selectColor); 
+}
+
+
+function selectColor(event){
+   let elementSelecionado = document.querySelector('.selected'); 
+   elementSelecionado.classList.remove('selected');
+   event.target.classList.add('selected');
+}
+
