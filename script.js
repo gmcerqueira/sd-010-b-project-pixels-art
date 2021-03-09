6d3c03c805ce5b4f7713a3a6386acc44d0daa362
DanielTrybe <darkdanielr@gmail.com>
2021-03-09 18:56:33 -0300

    4 de novo

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function clicarCor() {
-  let button = document.getElementById("black");
+  let button = document.getElementById('#black');
 
-  button.addEventListener("click", function () {
-    if (document.getElementsByClassName("color").classList.contains('cor')) {
-      document.getElementsByClassName("cor").classList.add("preto");
-      document.getElementsByClassName("preto").classList.remove("cor");
+  button.addEventListener('click', function () {
+    if (document.getElementsByClassName('.pixel').classList.contains('.cor')) {
+      document.getElementsByClassName('.cor').classList.add('.preto');
+      document.getElementsByClassName('.preto').classList.remove('.cor');
     } else {
-        document.getElementsByClassName("preto").classList.add("cor");
-        document.getElementsByClassName("cor").classList.remove("preto");
+        document.getElementsByClassName('.preto').classList.add('.cor');
+        document.getElementsByClassName('.cor').classList.remove('.preto');
     }
   })
 }
