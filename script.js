a707d77805f4f276cb201aa2500dcdf2786c5054
Matheus Bodra <matheusbodra@gmail.com>
2021-03-09 08:55:01 -0300

    requisito 3

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (i=0; i<4; i++) {
 }
 
 let coresDaPaleta = document.getElementsByClassName("color");
-coresDaPaleta[0].style.backgroundColor = "brown";
+coresDaPaleta[0].style.backgroundColor = "black";
 coresDaPaleta[1].style.backgroundColor = "purple";
 coresDaPaleta[2].style.backgroundColor = "pink";
 coresDaPaleta[3].style.backgroundColor = "magenta"
