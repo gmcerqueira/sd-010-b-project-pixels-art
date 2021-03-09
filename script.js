840b5f992f48271ef3c962411492099c10503ee0
lucasportella <lucasportellaagu@gmail.com>
2021-03-09 15:15:38 -0300

    black palette

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let paletaRed = window.document.getElementsByClassName('color')[0];
-paletaRed.style.background = 'red';
+paletaRed.style.background = 'black';
 let paletaBlue = window.document.getElementsByClassName('color')[1];
-paletaBlue.style.background = 'blue';
+paletaBlue.style.background = 'red';
 let paletaGreen = window.document.getElementsByClassName('color')[2];
-paletaGreen.style.background = 'green';
+paletaGreen.style.background = 'blue';
 let paletaPurple = window.document.getElementsByClassName('color')[3];
-paletaPurple.style.background = 'purple';
+paletaPurple.style.background = 'green';
