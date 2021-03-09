0f3142f48e52a60cbca5a8cab7758a67214cc3bb
lucasportella <lucasportellaagu@gmail.com>
2021-03-09 15:32:05 -0300

    create table

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ let paletaGreen = window.document.getElementsByClassName('color')[2];
 paletaGreen.style.background = 'blue';
 let paletaPurple = window.document.getElementsByClassName('color')[3];
 paletaPurple.style.background = 'green';
+
+let pixelBoard = window.document.getElementById('pixel-board');
+for (lines = 0; lines < 5; lines += 1) {
+    let tableRow = window.document.createElement('tr');
+    pixelBoard.appendChild(tableRow)
+    for (columns = 0; columns < 5; columns += 1) {
+        let tableCell = window.document.createElement('th')
+        tableCell.innerText = 'testcell'
+        tableRow.appendChild(tableCell)
+    }
+}
