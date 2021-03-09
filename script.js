67d27b4781150b6643380fb117f19426cd95a758
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-09 15:52:00 -0300

    requesito 4 feito

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let paleta = document.getElementsByClassName('color');
+const paleta = document.getElementsByClassName('color');
 
-let paletaRed = paleta[0].style.backgroundColor = 'red';
+paleta[0].style.backgroundColor = 'black';
 paleta[1].style.backgroundColor = 'blue';
-paleta[2].style.backgroundColor = 'orange';
+paleta[2].style.backgroundColor = 'red';
 paleta[3].style.backgroundColor = 'green';
+
+let ul = document.querySelector('#pixel-board');
+for (let index = 0; index < 25; index += 1) {
+  let li = document.createElement('li');
+  li.className = 'pixel';
+  ul.appendChild(li);
+}
