000f6af841d47b0adc1e76a8c3d529876d088b4c
esionascimento <esio.392@protonmail.com>
2021-03-09 16:28:16 -0300

    4-cor nao funfa, arrumado

---
+++ script.js
@@ -0,0 +1,10 @@
+let corSelected = document.querySelectorAll('.pixel');
+function clickSelected() {
+
+  if(divSelect.className == 'task') {
+    divSelect.className = 'task selected';
+  }else {
+    divSelect.className = 'task';
+  }
+}
+// corSelected.addEventListener('click', clickSelected);
