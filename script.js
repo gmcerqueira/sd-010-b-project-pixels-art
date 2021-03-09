759761a23cd8bc168349c9c9b0d084c1064aab8c
lucasportella <lucasportellaagu@gmail.com>
2021-03-09 15:08:55 -0300

    paleta

---
+++ script.js
@@ -0,0 +1,8 @@
+let paletaRed = window.document.getElementsByClassName('color')[0];
+paletaRed.style.background = 'red';
+let paletaBlue = window.document.getElementsByClassName('color')[1];
+paletaBlue.style.background = 'blue';
+let paletaGreen = window.document.getElementsByClassName('color')[2];
+paletaGreen.style.background = 'green';
+let paletaPurple = window.document.getElementsByClassName('color')[3];
+paletaPurple.style.background = 'purple';
