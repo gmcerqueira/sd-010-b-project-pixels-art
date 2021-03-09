1d4b5101b6a1bf89f64298d35adbb7eb21e693ac
Joao Vanelli <vanellijoao@gmail.com>
2021-03-09 19:21:30 -0300

    Finalizando requisito 10

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 // ---------------------------------------------------
-// CREATE BOARD
+// CREATE NEW BOARD
+
 // Criação da div .row
+const pixelBoard = document.getElementById('pixel-board');
 
-function createRows(n) {
-  const pixelBoard = document.getElementById('pixel-board');
-
-  for (let i = 0; i < n; i += 1) {
-    const rowDiv = document.createElement('div');
-    rowDiv.className = 'row';
-    pixelBoard.appendChild(rowDiv);
-  }
+for (let i = 0; i < 5; i += 1) {
+  const rowDiv = document.createElement('div');
+  rowDiv.className = 'row';
+  pixelBoard.appendChild(rowDiv);
 }
 // Criação da div .pixel
-function createPixels() {
-  const rowDiv = document.getElementsByClassName('row');
+const rowDiv = document.getElementsByClassName('row');
 
-  for (let i = 0; i < rowDiv.length; i += 1) {
-    for (let j = 0; j < rowDiv.length; j += 1) {
-      const pixelDiv = document.createElement('div');
-      pixelDiv.className = 'pixel';
-      rowDiv[i].appendChild(pixelDiv);
-    }
+for (let i = 0; i < rowDiv.length; i += 1) {
+  for (let j = 0; j < rowDiv.length; j += 1) {
+    const pixelDiv = document.createElement('div');
+    pixelDiv.className = 'pixel';
+    rowDiv[i].appendChild(pixelDiv);
   }
 }
 
-function createBoard(n) {
-  createRows(n);
-  createPixels();
-}
-
-createBoard(8);
+let pixels = document.querySelectorAll('.pixel');
 
 // ---------------------------------------------------
 // Mudança da Classe .selected
@@ -56,8 +47,6 @@ fourth.addEventListener('click', classChange);
 // Utilizando a classe .selected para pintar os pixels
 // Referência: https://zellwk.com/blog/css-values-in-js/
 
-const pixels = document.querySelectorAll('.pixel');
-
 function paintPixel() {
   const style = getComputedStyle(selectedColor[0]);
   const color = style.backgroundColor;
@@ -67,7 +56,6 @@ function paintPixel() {
 for (let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', paintPixel);
 }
-
 // ---------------------------------------------------
 // Botão para limpar o board
 
@@ -82,3 +70,56 @@ function clearBoard() {
 }
 
 btn.addEventListener('click', clearBoard);
+
+// ---------------------------------------------------
+// CREATE NEW BOARD
+
+// Criação da div .row
+function createRows(n) {
+  const newPixelBoard = document.getElementById('pixel-board');
+
+  for (let i = 0; i < n; i += 1) {
+    const newRowDiv = document.createElement('div');
+    newRowDiv.className = 'row';
+    newPixelBoard.appendChild(newRowDiv);
+  }
+}
+// Criação da div .pixel
+function createPixels() {
+  const newRowDiv = document.getElementsByClassName('row');
+
+  for (let i = 0; i < newRowDiv.length; i += 1) {
+    for (let j = 0; j < newRowDiv.length; j += 1) {
+      const pixelDiv = document.createElement('div');
+      pixelDiv.className = 'pixel';
+      newRowDiv[i].appendChild(pixelDiv);
+    }
+  }
+}
+
+// Remoção do board anterior
+function removeBoard() {
+  document.getElementById('pixel-board').innerHTML = '';
+}
+
+// Criação do board
+function createBoard(n) {
+  removeBoard();
+  createRows(n);
+  createPixels();
+  pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', paintPixel);
+  }
+}
+
+// Ao clicar no botão, pegar o número no input e imprimir na função createBoard
+let boardSize = document.getElementById('board-size').value;
+const button = document.getElementById('generate-board');
+
+function board() {
+  boardSize = document.getElementById('board-size').value;
+  createBoard(boardSize);
+}
+
+button.addEventListener('click', board);
