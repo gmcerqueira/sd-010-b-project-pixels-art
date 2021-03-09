b920a04fa44f7d613e4a6a0eec820923fc84cfc5
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-09 17:49:06 -0300

    Faz o quadro de pixels ter seu tamanho definido pela pessoa usuária

--- script.js
+++ script.js
@@ -9,52 +9,77 @@ firstBoxColor.nextElementSibling.nextElementSibling.nextElementSibling.style.bac
 // Pegando os elementos de depois
 
 //Criando um quadro de pixels dinamicamente
+const inputNumber = document.querySelector("#board-size")
 
-divTable = document.createElement('div');
-divTable.id = 'pixel-board';
-lines = 5;
-columns = 5;
+function buildBoard(){
+   
 
-document.body.appendChild(divTable);
-
-for(let index = 0; index < lines; index +=1){
-    divTr = document.createElement('div');
-    divTr.className = 'tr';
-    divTable.appendChild(divTr);
-    for(let index = 0; index < columns; index +=1){
-        divBox = document.createElement('div');
-        divBox.className = 'pixel';
-        divBox.style.backgroundColor = 'rgb(255, 255, 255)';
-        divTr.appendChild(divBox);
+    if(inputNumber.value == ''){
+        alert("Board inválido!")
+        console.log(inputNumber.value);
     }
-}
-
-
-
-let colorArray = document.querySelectorAll(".color")
-for (let i = 0; i < colorArray.length; i += 1) {
-    colorArray[i].addEventListener("click", function(event) {
-        for (let j = 0; j < colorArray.length; j += 1){
-            colorArray[j].className = 'color';
+    else{
+        if(document.querySelector('#pixel-board') !== null){
+            document.querySelector('#pixel-board').remove()
+            lines = inputNumber.value;
+            columns = inputNumber.value;
+            divTable = document.createElement('div');
+            divTable.id = 'pixel-board';
+        }    
+        else{
+            lines = inputNumber.value;
+            columns = inputNumber.value;
+            divTable = document.createElement('div');
+            divTable.id = 'pixel-board';
         }
-        event.target.className = 'color selected';
-    }) 
-}
+    }
+
+    document.body.appendChild(divTable);
+
+    for(let index = 0; index < lines; index +=1){
+        divTr = document.createElement('div');
+        divTr.className = 'tr';
+        divTable.appendChild(divTr);
+        for(let index = 0; index < columns; index +=1){
+            divBox = document.createElement('div');
+            divBox.className = 'pixel';
+            divBox.style.backgroundColor = 'rgb(255, 255, 255)';
+            divTr.appendChild(divBox);
+        }
+    }
+
+    let colorArray = document.querySelectorAll(".color")
+    for (let i = 0; i < colorArray.length; i += 1) {
+        colorArray[i].addEventListener("click", function(event) {
+            for (let j = 0; j < colorArray.length; j += 1){
+                colorArray[j].className = 'color';
+            }
+            event.target.className = 'color selected';
+        }) 
+    }
 
 
-// console.log(document.querySelector('.color').className)
-let pixelArray = document.querySelectorAll(".pixel");
-for (let i = 0; i < pixelArray.length; i += 1) {
-    pixelArray[i].addEventListener("click", function(event) {
-        let selectedColor = document.querySelector(".color.selected")
-        event.target.style.backgroundColor = selectedColor.style.backgroundColor;
-    }) 
-}
+    // console.log(document.querySelector('.color').className)
+    let pixelArray = document.querySelectorAll(".pixel");
 
-let clearButton = document.querySelector("#clear-board");
-clearButton.addEventListener("click", function(event) {
     for (let i = 0; i < pixelArray.length; i += 1) {
-        pixelArray[i].style.backgroundColor = 'white';
-        
-    } 
-})
+        console.log('aqui1');
+        pixelArray[i].addEventListener("click", function(event) {
+            let selectedColor = document.querySelector(".color.selected")
+            event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+            console.log('aqui');
+        }) 
+    }
+
+    let clearButton = document.querySelector("#clear-board");
+    clearButton.addEventListener("click", function(event) {
+        for (let i = 0; i < pixelArray.length; i += 1) {
+            pixelArray[i].style.backgroundColor = 'white';
+            
+        } 
+    })
+
+}
+
+let generateButton = document.querySelector("#generate-board");
+generateButton.addEventListener("click", buildBoard);
