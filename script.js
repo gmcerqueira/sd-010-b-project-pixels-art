d21ecee9e52d0c6a6635c6dd9ba1dd0a48c66881
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-09 16:30:15 -0300

    fix

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 window.onload = function() {
   let divPixel = document.querySelector('#pixel-board');
-  function addPixelTable(){
+
+  function addPixelTable() {
     for (let index = 1; index <= 5; index += 1 ){
       let lineDiv = document.createElement('div');
-        for (let k = 1; k <= 5; k += 1){
+        for (let k = 1; k <= 5; k += 1) {
           let pixel = document.createElement('div');
             pixel.className = 'pixel';
-            lineDiv.appendChild(pixel)
+            lineDiv.appendChild(pixel);
       }
-      divPixel.appendChild(lineDiv)
+      divPixel.appendChild(lineDiv);
     }
   }
 
   addPixelTable();
 
-  function addEvents(){
+  function addEvents() {
     let divsPixels = document.querySelectorAll('.pixel');
-    let elementoBlack = document.querySelector('.black');
-    elementoBlack.className += ' selected'
+    let blackElement = document.querySelector('.black');
+    blackElement.className += 'selected';
 
-    for (let index = 0; index < divsPixels.length; index += 1){
-        divsPixels[index].addEventListener('click', function(){
+    for (let index = 0; index < divsPixels.length; index += 1) {
+        divsPixels[index].addEventListener('click', function() {
             divsPixels[index].className += ' pixEv';
         });
     }
   }
   addEvents();
 
-  
+
 }
