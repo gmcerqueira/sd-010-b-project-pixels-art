1007b9c7dcf6652169b8f6c902c27ce1e8742527
Mariana Mohr <marianamohr@gmail.com>
2021-03-09 16:11:47 -0300

    Requisito 5 - cada quadro tem 40pixels e possui borda preta de 1px

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-window.onload = creatPixelBoard();
+
 
 
 function creatPixelBoard() {
-  let pai = document.getElementById('pixel-board');
+  const pai = document.getElementById('pixel-board');
   for (let i = 0; i < 5; i += 1) {
-
     for (let index = 0; index < 5; index += 1) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.className = 'pixel';
     pai.appendChild(div);
   }
 
 }
 }
+
+window.onload = creatPixelBoard();
