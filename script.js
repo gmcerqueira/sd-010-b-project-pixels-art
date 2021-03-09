92a97f84754ac392d435b0c83e468b39bb5c4401
edersonrodara <edersonrodara@gmail.com>
2021-03-09 19:16:23 -0300

    colocando o tamanho e fazendo a grade de pixel

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 //   colorPalet.appendChild(criarDiv);
 // }
 
-function crindoDivs() {
+function palletCollor() {
   let pallet = [];
   for (let index = 0; index < 4; index += 1) {
     let criarDiv = document.createElement('div');
@@ -15,7 +15,21 @@ function crindoDivs() {
     pallet = document.getElementById('color-palette').appendChild(criarDiv);
   }
 }
-crindoDivs();
+palletCollor();
+
+function pallets () {
+  let pallet = [];
+  for (let indexHor = 0; indexHor < 5; indexHor += 1) {
+    let criarDiv = document.createElement('div');
+    pallet[indexHor] = document.getElementById('pixel-board').appendChild(criarDiv);
+    for (let indexVer = 0; indexVer < 5; indexVer += 1) {
+      let criarDiv = document.createElement('div');
+      criarDiv.className = 'pixel';
+      pallet[indexHor] = document.getElementById('pixel-board').appendChild(criarDiv);
+    }
+  }
+}
+pallets();
 
 function colorPallet() {
   getColor = document.getElementsByClassName('color');
