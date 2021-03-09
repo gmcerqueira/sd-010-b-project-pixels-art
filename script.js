8b57a947a7f1d8546491298d4d747b47ee756852
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-09 17:36:07 -0300

    Exercicio 2

---
+++ script.js
@@ -0,0 +1,19 @@
+let containerPalette = document.querySelector('#color-palette');
+let tamanhoPalette = [1, 2, 3, 4];
+
+function createPalette() {
+  for (let index = 0; index < tamanhoPalette.length; index += 1) {
+    let selectors = document.createElement('div');
+    selectors.className = 'color';
+    containerPalette.appendChild(selectors);
+  }
+  return containerPalette;
+}
+
+createPalette();
+
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'yellow';
+
