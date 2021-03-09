564f8ef5ce49c803b08fe805855b863ca03c99c9
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-09 17:37:48 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let paletaDeCores = document.getElementById("color-palette")
-for (let i= 0; i < 4; i++) {
-let criaPaleta = document.createElement("div");
-criaPaleta.className= "color";
-paletaDeCores.appendChild(criaPaleta);
+//Cria divs color
+const paletaDeCores = document.getElementById('color-palette');
+for (let i = 0; i < 4; i += 1) {
+  const criaPaleta = document.createElement("div");
+  criaPaleta.className = 'color';
+  paletaDeCores.appendChild(criaPaleta);
 }
 
-let cores = document.getElementsByClassName("color");
-let black = cores[0];
-let red = cores[1];
-let green = cores[2];
-let yellow = cores [3];
-
+//adicionar cor as divs color
+const cores = document.getElementsByClassName('color');
+const black = cores[0];
+const red = cores[1];
+const green = cores[2];
+const yellow = cores [3];
 black.style.background = 'black';
 red.style.background = 'red';
 green.style.background = 'green';
 yellow.style.background = 'yellow';
+
+// cria tabela dos pixels a serem coloridos
+const tableCreate = document.getElementById('pixel-board');
+for (let i = 0; i < 5; i += 1) {
+  const cells = document.createElement('tr');
+  tableCreate.appendChild(cells);
+}
+const tableCreateCells = document.querySelectorAll('pixel-board, tr');
+for (let i = 0; i < tableCreateCells.length; i += 1) {
+    for (let n = 0; n < tableCreateCells.length; n += 1) {
+  const lines = document.createElement('td');
+  lines.className = 'pixel';
+  tableCreateCells[i].appendChild(lines);
+}
+}
