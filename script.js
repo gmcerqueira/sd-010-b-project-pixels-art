9fd66928f06f745f8bbab02622191804261d31de
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-09 16:23:01 -0300

    adicionando cores

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function atribuirPaleta(divs) {
 for (let index = 0; index < 4; index += 1) {
   atribuirPaleta(criarDiv());
 }
+
+function adicionarBgcolor() {
+  const getDiv = document.getElementsByClassName('color');
+  for (let index = 0; index < getDiv.length; index += 1) {
+    const randomizer = Math.round(Math.random() * 255);
+    const randomizer2 = Math.round(Math.random() * 255);
+    const randomizer3 = Math.round(Math.random() * 255);
+    const randomRgb = `rgb(${randomizer}, ${randomizer2}, ${randomizer3})`;
+    getDiv[index].style.backgroundColor = randomRgb;
+    getDiv[index].id = randomRgb;
+  }
+  return getDiv;
+}
+
+window.onload = function () {
+  adicionarBgcolor();
+}
