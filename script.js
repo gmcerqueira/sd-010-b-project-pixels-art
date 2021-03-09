bcd74a60806a7729ba65062f4d4a1f0143b1068d
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-09 16:19:02 -0300

    add pixel 40/40

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 window.onload = function() {
+  let firstDiv = document.querySelector("#pixel-board")
   
+  function addPixelTable() {
+    for (let i = 0; i <= 5; i += 1){
+      let linePixel = document.createElement('div');
+      for (let columPixel = 1 columPixel <= 5; columPixel += 1) {
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        linePixel.appendChild(pixel);
+      }
+      firstDiv.appendChild(linePixel);
+    }
+  }
+  addPixelTable();
 }
