f10d613925d0222e415e259e80735dfa7f5d1f02
renatogsouza <renatomacedo1995@gmail.com>
2021-03-09 19:17:36 -0300

    criando os pixels

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ function colorPixels() {
 
 }
 function criaPixels() {
-  let divNova = document.createElement("div")//.id = 'divn';
-  document.body.appendChild(divNova);
-  divNova.className = 'divn';
+  let secao = document.querySelector('#secao');
+  let quadro =document.createElement('div');
+  quadro.className = 'pixel-border' ;
+  for (let j = 0; j < 5; j++) {
+    let novaLinha = document.createElement('div');
+    for (let i = 0; i < 5; i++) {
+      let divNova = document.createElement("div");
+      divNova.className = 'pixel';
+      novaLinha.appendChild(divNova);
+    }
+    document.body.appendChild(novaLinha);
+  }
+  document.body.appendChild(quadro);
 }
-function pegaCor() {
-  // let cor = document.getElementsByClassName();
-  var elem = document.getElementById("test");
-  var theCSSprop = window.getComputedStyle(item, null).getPropertyValue("background-color");
-  // document.getElementById("demo").innerHTML = theCSSprop;
-  localStorage.setItem('cor', theCSSprop);
-}
-  document.querySelectorAll('.color').forEach(item => {
-  item.addEventListener('click', pegaCor)// => {
-   
-  });
+// function pegaCor() {
+//   let cor = document.getElementsByClassName('color');  
+//   var theCSSprop = document.getComputedStyle(item, null).getPropertyValue("background-color");
+//   // document.getElementById("demo").innerHTML = theCSSprop;
+//   localStorage.setItem('cor', theCSSprop);
+// }
+//   document.querySelectorAll('.color').forEach(item => {
+//   item.addEventListener('click', pegaCor)// => {
+
+//   });
 // })
