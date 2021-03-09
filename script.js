9049d3fe0b91e03eb2c455492dfacb76820880dd
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-09 18:52:32 -0300

    Exercicio 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const containerPalette = document.querySelector('#color-palette');
 const tamanhoPalette = [1, 2, 3, 4];
+const containerQuadro = document.querySelector('#pixels-board');
 
 function createPalette() {
   for (let index = 0; index < tamanhoPalette.length; index += 1) {
@@ -9,7 +10,19 @@ function createPalette() {
   }
   return containerPalette;
 }
+
 createPalette();
+
+function createBox() {
+  for (let index = 0; index < 50; index += 1) {
+    let createDiv = document.createElement('div');
+    createDiv.className = 'pixels';
+    containerQuadro.appendChild(createDiv);
+  }
+}
+
+createBox();
+
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
