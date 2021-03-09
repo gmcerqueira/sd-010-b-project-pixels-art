fe54b72e6004a670c6acf3b33905da9fe816d959
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-09 16:35:06 -0300

    lint

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-  let divPixel = document.querySelector('#pixel-board');
+  const divPixel = document.querySelector('#pixel-board');
 
   function addPixelTable() {
-    for (let index = 1; index <= 5; index += 1 ){
+    for (let index = 1; index <= 5; index += 1 ) {  
       let lineDiv = document.createElement('div');
-        for (let k = 1; k <= 5; k += 1) {
-          let pixel = document.createElement('div');
-            pixel.className = 'pixel';
-            lineDiv.appendChild(pixel);
+      for (let k = 1; k <= 5; k += 1) {
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        lineDiv.appendChild(pixel);
       }
       divPixel.appendChild(lineDiv);
     }
@@ -16,17 +16,16 @@ window.onload = function() {
   addPixelTable();
 
   function addEvents() {
-    let divsPixels = document.querySelectorAll('.pixel');
-    let blackElement = document.querySelector('.black');
+    const divsPixels = document.querySelectorAll('.pixel');
+    const blackElement = document.querySelector('.black');
     blackElement.className += 'selected';
 
     for (let index = 0; index < divsPixels.length; index += 1) {
-        divsPixels[index].addEventListener('click', function() {
-            divsPixels[index].className += ' pixEv';
-        });
+      divsPixels[index].addEventListener('click', function() {
+        divsPixels[index].className += ' pixEv';
+      });
     }
   }
   addEvents();
-
 
 }
