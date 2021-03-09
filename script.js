5634db33d2df197769ca0f088e44c77e8bdb1f43
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-09 17:52:34 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function selectBlack(event) {
   const colorRed = document.getElementById('vermelho');
   const colorBlue = document.getElementById('azul');
   const colorGreen = document.getElementById('verde');
+
   colorRed.className = 'color';
   colorBlue.className = 'color';
   colorGreen.className = 'color';
@@ -12,30 +13,33 @@ function selectRed(event) {
   const corPreto = document.getElementById('preto');
   const corAzul = document.getElementById('azul');
   const corVerde = document.getElementById('verde');
+
   corPreto.className = 'color';
   corAzul.className = 'color';
   corVerde.className = 'color';
-  event.target.className = 'color selected';
+  event.target.className = 'color selected'
 }
 
 function selectBlue(event) {
   const corPreto = document.getElementById('preto');
   const corVermelho = document.getElementById('vermelho');
   const corVerde = document.getElementById('verde');
+  
   corPreto.className = 'color';
   corVermelho.className = 'color';
   corVerde.className = 'color';
-  event.target.className = 'color selected';
+  event.target.className = 'color selected'
 }
 
 function selectGreen(event) {
   const corPreto = document.getElementById('preto');
   const corVermelho = document.getElementById('vermelho');
   const corAzul = document.getElementById('azul');
+
   corPreto.className = 'color';
   corVermelho.className = 'color';
   corAzul.className = 'color';
-  event.target.className = 'color selected';
+  event.target.className = 'color selected'
 }
 
 const blackButton = document.getElementById('preto');
@@ -51,10 +55,20 @@ const greenButton = document.getElementById('verde');
 greenButton.addEventListener('click', selectGreen);
 
 function pintaPixel(event) {
-  let selecionaCor = document.querySelector('.selected').id;
   let pixel = event.target;
+  let selecionaCor = document.querySelector('.selected').style.backgroundColor;
   pixel.style.backgroundColor = selecionaCor;
 }
 
-const colore = document.querySelector('.pixel');
+let colore = document.querySelector('.pixel');
 colore.addEventListener('click', pintaPixel);
+
+function limpaGrid() {
+  const pixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+      pixel[index].style.backgroundColor = 'white';
+  }
+}
+
+const buttonClean = document.getElementById('clear-board');
+buttonClean.addEventListener('click', limpaGrid);
