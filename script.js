c5a65504e6b2b7d9246560bd755efdae37f122d2
esionascimento <esio.392@protonmail.com>
2021-03-09 18:19:11 -0300

    arrumando problemas com lint

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 let corSalva = 'black';
 let aux;
-let corDivBoard = document.querySelector('#color-palette');
-let cor = document.querySelectorAll('.color');
-//cores nas paletas
+const corDivBoard = document.querySelector('#color-palette');
+const cor = document.querySelectorAll('.color');
+/* cores nas paletas */
+
 cor[0].style.backgroundColor = 'black';
 cor[1].style.backgroundColor = 'red';
 cor[2].style.backgroundColor = 'orange';
 cor[3].style.backgroundColor = 'green';
-//Paleta
+/* Paleta */
 function clickPalette(eventoDeOrigem) {
   aux = eventoDeOrigem.target;
   cor[0].className = 'color';
   cor[1].className = 'color';
   cor[2].className = 'color';
   cor[3].className = 'color';
-  
-  if(aux.className == 'color') {
+  if (aux.className === 'color') {
     aux.className = 'color selected';
     corSalva = aux.style.backgroundColor;
-  }else {
+  } else {
     aux.className = 'color';
   }
 }
 corDivBoard.addEventListener('click', clickPalette);
-//Tabela
-let corDivPixel = document.querySelector('#pixel-board');
-let corPixel = document.querySelectorAll('.pixel');
+/* Tabela */
+const corDivPixel = document.querySelector('#pixel-board');
+const corPixel = document.querySelectorAll('.pixel');
 function clickPixel(eventoDeOrigem) {
   aux = eventoDeOrigem.target;
-  if (corPixel.className == 'color') {
+  if (corPixel.className === 'color') {
     aux.style.backgroundColor = corSalva;
   } else {
     aux.style.backgroundColor = corSalva;
   }
 }
 corDivPixel.addEventListener('click', clickPixel);
-//Criando button
-let buttonBoard = document.querySelector('#clear-board');
+/* Criando button */
+const buttonBoard = document.querySelector('#clear-board');
 function limparBoard() {
-  for (let i = 0; i < 25; i += 1){
+  for (let i = 0; i < 25; i += 1) {
     corPixel[i].style.backgroundColor = 'white';
   }
 }
