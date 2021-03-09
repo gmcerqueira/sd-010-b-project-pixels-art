3143e1be23ca3eb2de1c941708d12c903f453207
RafaMaths <rmathscoach@gmail.com>
2021-03-09 20:14:38 -0300

    inicia requisito 6

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'yellow';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
+document.getElementsByClassName('color')[0].className += ' selected';
 
 function createBox() {
   for (let i = 0; i < 25; i += 1) {
