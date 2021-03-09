181ebf539802dcaee3e7a1734da05070781b0bfd
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-09 17:38:57 -0300

    Corrigindo erro lint exercicio 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let containerPalette = document.querySelector('#color-palette');
-let tamanhoPalette = [1, 2, 3, 4];
+const containerPalette = document.querySelector('#color-palette');
+const tamanhoPalette = [1, 2, 3, 4];
 
 function createPalette() {
   for (let index = 0; index < tamanhoPalette.length; index += 1) {
@@ -9,9 +9,7 @@ function createPalette() {
   }
   return containerPalette;
 }
-
 createPalette();
-
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
