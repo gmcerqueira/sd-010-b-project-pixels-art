92f5dd40db84807fbbfb9b02b4c9206aae891007
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-09 17:03:37 -0300

    2nd and 3rt exercise 'Color Palette' done

---
+++ script.js
@@ -0,0 +1,27 @@
+function createBoxTable(numOfItems, itemClass, parentId, numOfLines = 1) {
+  const paletteBox = document.getElementById(parentId);
+  const boxTable = document.createElement('table');
+
+  paletteBox.appendChild(boxTable);
+  for (let line = 1; line <= numOfLines; line += 1) {
+    const boxLine = document.createElement('tr');
+    boxTable.appendChild(boxLine);
+    for (let item = 1; item <= numOfItems; item += 1) {
+      const boxItem = document.createElement('td');
+      boxLine.appendChild(boxItem);
+      boxItem.classList.add(itemClass);
+    }
+  }
+}
+
+function addClassToPaletteBox() {
+  const paletteBox = document.getElementById('color-palette');
+  const boxes = paletteBox.children[0].children[0].children;
+  console.log(boxes);
+  for (let index = 0; index < boxes.length; index += 1) {
+    boxes[index].classList.add(`color${index + 1}`);
+  }
+}
+
+createBoxTable(4, 'color', 'color-palette');
+addClassToPaletteBox();
