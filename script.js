3961f3b607701166ea3e9f0c6ee8e1a524c81eda
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-09 19:08:19 -0300

    Adicionando requisito 4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const array = [1, 2, 3, 4];
 
 function createDivs() {
   for (let index = 0; index < array.length; index += 1) {
-    let createColors = document.createElement('div');
+    const createColors = document.createElement('div');
     createColors.className = 'color';
     paletteBox.appendChild(createColors);
   }
@@ -16,3 +16,16 @@ document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'orange';
+
+const pixelBox = document.getElementById('pixel-board');
+
+function boardPixel() {
+  for (let index = 0; index < 25; index += 1) {
+      const creatPixels = document.createElement('div');
+      creatPixels.className = 'pixel';
+      pixelBox.appendChild(creatPixels);
+    }
+  return pixelBox;
+}
+
+boardPixel();
