4a4d912c4f87734a8d381bc10eb061265a149a87
Carlos <cemargato@gmail.com>
2021-03-09 17:53:02 -0300

    Ajustes exercicio 5

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// informacao
+
+let pixelBoard = document.querySelectorAll('lines');
+let columns = 5
+let lines = document.createElement('tr');
+for (let index = 1; index < columns; index += 1) {
+
+pixelBoard.appendChild(lines)  
+}
+
