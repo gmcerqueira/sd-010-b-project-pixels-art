52ac9460b0dbf475d416ccab5deecae324bca289
renanpbraga <renanpbraga@gmail.com>
2021-03-09 19:41:49 -0300

    corrige requisito 2

--- script.js
+++ script.js
@@ -25,4 +25,4 @@ criaTela();
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
-document.getElementsByClassName('color')[3].style.backgroundColor = 'white';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'yellow';
