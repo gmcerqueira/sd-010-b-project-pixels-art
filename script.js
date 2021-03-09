4a4e12a2cc30df9b158bfd146e181b6d48933d3e
Caio <thehaseol@gmail.com>
2021-03-09 20:47:14 -0300

    reboot, preenchimento dos pixels

--- script.js
+++ script.js
@@ -1,33 +1,165 @@
-// Cria divs color
-const paletaDeCores = document.getElementById('color-palette');
-for (let i = 0; i < 4; i += 1) {
-  const criaPaleta = document.createElement("div");
-  criaPaleta.className = 'color';
-  paletaDeCores.appendChild(criaPaleta);
+window.onload = function () {
+  criarQuadro();
+};
+
+const { body } = document;
+
+const paletteBox = document.createElement('div');
+
+paletteBox.id = 'color-palette';
+paletteBox.style.marginTop = '50px';
+
+body.appendChild(paletteBox);
+
+let N = 5;
+
+// Cria as cores dinâmicamente */
+let colors = ['black'];
+
+for (let nColor = 1; nColor < 4; nColor += 1) {
+  let colorInt = Math.random() * 4095;
+
+  for (let n = 0; n < nColor; n += 1) {
+    if (colorInt == parseInt(colors[n])) {
+      while (colorInt === parseInt(colors[n])) {
+        colorInt = Math.random() * 4095;
+      }
+    }
+  }
+
+  if (colorInt == 4095) colorInt -= 1;
+
+  colorInt = Math.floor(colorInt);
+  let colorHex = Number(colorInt).toString(16);
+  if (colorHex.length < 3) colorHex += '0';
+  colorHex = `#${colorHex}`;
+
+  colors.push(colorHex);
+}
+console.log(colors);
+/* ----------------------Paleta de Cores---------------------------------- */
+
+for (let color = 0; color < 4; color += 1) {
+  const colorElement = document.createElement('div');
+
+  colorElement.className = color == 0 ? 'color selected' : 'color';
+
+  colorElement.style.backgroundColor = colors[color];
+  console.log(colors[color]);
+  paletteBox.appendChild(colorElement);
 }
 
-// adicionar cor as divs color
-const colors = document.getElementsByClassName('color');
-const black = colors[0];
-const red = colors[1];
-const green = colors[2];
-const yellow = colors[3];
-black.style.background = 'black';
-red.style.background = 'red';
-green.style.background = 'green';
-yellow.style.background = 'yellow';
-
-// cria tabela dos pixels a serem coloridos
-const tableCreate = document.getElementById('pixel-board');
-for (let i = 0; i < 5; i += 1) {
-  const cells = document.createElement('tr');
-  tableCreate.appendChild(cells);
+function selected(event) {
+  // limpar os selecionados
+  const elements = document.querySelectorAll('.color');
+  for (let index = 0; index < elements.length; index += 1) {
+    elements[index].className = 'color';
+  }
+  event.target.className = 'color selected';
 }
-const tableCreateCells = document.querySelectorAll('pixel-board, tr');
-for (let i = 0; i < tableCreateCells.length; i += 1) {
-  for (let n = 0; n < tableCreateCells.length; n += 1) {
-    const lines = document.createElement('td');
-    lines.className = 'pixel';
-    tableCreateCells[i].appendChild(lines);
+
+paletteBox.addEventListener('click', selected, false);
+
+/* Criando o Input */
+
+const containerHeader = document.createElement('div');
+body.appendChild(containerHeader);
+
+const btn = document.createElement('div');
+btn.innerHTML = 'Limpar';
+btn.id = 'clear-board';
+
+containerHeader.appendChild(btn);
+
+const entry = document.createElement('div');
+
+containerHeader.appendChild(entry);
+
+const value = document.createElement('input');
+
+value.type = 'number';
+value.min = '1';
+value.max = '50';
+value.id = 'board-size';
+
+entry.appendChild(value);
+entry.id = 'form';
+
+const submit = document.createElement('button');
+
+entry.appendChild(submit);
+submit.innerHTML = 'VQV';
+submit.id = 'generate-board';
+
+submit.addEventListener('click', () => {
+  if (value.value == '') {
+    alert('Board inválido!');
+    return;
+  }
+  killQuadro();
+
+  if (value.value > 50) {
+    N = 50;
+    value.value = N;
+  }
+  else if (value.value < 5) {
+    N = 5;
+    value.value = N;
+  } else {
+    N = value.value;
+    value.value = N;
+  }
+  criarQuadro();
+});
+
+// Criar um botão
+
+btn.addEventListener('click', () => {
+  const pixels = document.getElementsByClassName('pixel');
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+});
+
+let pixelBoard = document.createElement('div');
+pixelBoard.id = 'pixel-board';
+pixelBoard.className = 'display-table';
+
+function killQuadro() {
+  while (pixelBoard.lastChild != null) {
+    pixelBoard.removeChild(pixelBoard.lastElementChild);
   }
 }
+
+// Criando o Quadro de Pixels
+function criarQuadro() {
+  body.appendChild(pixelBoard);
+  for (let linhas = 0; linhas < N; linhas += 1) {
+    const line = document.createElement('div');
+    line.className = 'line';
+    pixelBoard.appendChild(line);
+
+    for (let colunas = 0; colunas < N; colunas += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.style.backgroundColor = 'white';
+
+      pixel.style.width = '40px';
+      pixel.style.height = '40px';
+
+      line.appendChild(pixel);
+    }
+  }
+
+}
+
+// Preenchimento dos pixels
+
+function fillColor(event) {
+  // Procurar o Selecionado
+  const color = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = color;
+}
+
+pixelBoard.addEventListener('click', fillColor, false);
