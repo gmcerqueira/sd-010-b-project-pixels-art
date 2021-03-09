3732b71c425f6d9a5def3b21eeeb80e0871ae9c1
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-09 15:26:27 -0300

    Parte 2 finalizada

---
+++ script.js
@@ -0,0 +1,6 @@
+let paleta = document.getElementsByClassName('color');
+
+let paletaRed = paleta[0].style.backgroundColor = 'red';
+paleta[1].style.backgroundColor = 'blue';
+paleta[2].style.backgroundColor = 'orange';
+paleta[3].style.backgroundColor = 'green';
