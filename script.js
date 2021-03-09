f92ac827749d1b5d64e4e0aeeac1afc86ed72491
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-09 17:15:06 -0300

    Requisitos iniciais

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
 function selectBlack(event) {
-  let colorRed = document.getElementById('vermelho');
-  let colorBlue = document.getElementById('azul');
-  let colorGreen = document.getElementById('verde');
-
+  const colorRed = document.getElementById('vermelho');
+  const colorBlue = document.getElementById('azul');
+  const colorGreen = document.getElementById('verde');
   colorRed.className = 'color';
   colorBlue.className = 'color';
   colorGreen.className = 'color';
-  event.target.className = 'color selected'
+  event.target.className = 'color selected';
 }
 
 function selectRed(event) {
-  let corPreto = document.getElementById('preto');
-  let corAzul = document.getElementById('azul');
-  let corVerde = document.getElementById('verde');
-
+  const corPreto = document.getElementById('preto');
+  const corAzul = document.getElementById('azul');
+  const corVerde = document.getElementById('verde');
   corPreto.className = 'color';
   corAzul.className = 'color';
   corVerde.className = 'color';
-  event.target.className = 'color selected'
+  event.target.className = 'color selected';
 }
 
 function selectBlue(event) {
-  let corPreto = document.getElementById('preto');
-  let corVermelho = document.getElementById('vermelho');
-  let corVerde = document.getElementById('verde');
-  
+  const corPreto = document.getElementById('preto');
+  const corVermelho = document.getElementById('vermelho');
+  const corVerde = document.getElementById('verde');
   corPreto.className = 'color';
   corVermelho.className = 'color';
   corVerde.className = 'color';
-  event.target.className = 'color selected'
+  event.target.className = 'color selected';
 }
 
 function selectGreen(event) {
-  let corPreto = document.getElementById('preto');
-  let corVermelho = document.getElementById('vermelho');
-  let corAzul = document.getElementById('azul');
-
+  const corPreto = document.getElementById('preto');
+  const corVermelho = document.getElementById('vermelho');
+  const corAzul = document.getElementById('azul');
   corPreto.className = 'color';
   corVermelho.className = 'color';
   corAzul.className = 'color';
-  event.target.className = 'color selected'
+  event.target.className = 'color selected';
 }
 
-let blackButton = document.getElementById('preto');
+const blackButton = document.getElementById('preto');
 blackButton.addEventListener('click', selectBlack);
 
-let redButton = document.getElementById('vermelho');
+const redButton = document.getElementById('vermelho');
 redButton.addEventListener('click', selectRed);
 
-let blueButton = document.getElementById('azul');
+const blueButton = document.getElementById('azul');
 blueButton.addEventListener('click', selectBlue);
 
-let greenButton = document.getElementById('verde');
+const greenButton = document.getElementById('verde');
 greenButton.addEventListener('click', selectGreen);
 
 function pintaPixel(event) {
   let selecionaCor = document.querySelector('.selected').id;
   let pixel = event.target;
-  pixel.style.backgroundColor = selecionaCor.style.backgroundColor;
+  pixel.style.backgroundColor = selecionaCor;
 }
 
-let colore = document.querySelector('.pixel');
+const colore = document.querySelector('.pixel');
 colore.addEventListener('click', pintaPixel);
