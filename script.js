12e12cef5a160e46cb98b2b1edce1c3d520a9541
Paulo Xavier <pxavierf@gmail.com>
2021-03-09 17:28:32 -0300

    selecionando cor de bcg

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 createPixelBoard(5);
 selected();
 
+
 function createPixelBoard(numberPixels) {
   const pixelBoard = document.getElementById('pixel-board');
   for (let column = 0; column < numberPixels; column += 1) {
@@ -16,15 +17,26 @@ function createPixelBoard(numberPixels) {
 }
 
 function selected() {
-  const colorBox = document.querySelectorAll('.color');
-  const pixelBoard = document.getElementById('pixel-board');
-  
+  const colorBox = document.querySelectorAll('.color');  
   for (let index = 0; index < colorBox.length; index += 1) {
     colorBox[index].addEventListener('click', () => {
       for (let i = 0; i < colorBox.length; i += 1) {
         colorBox[i].classList.remove('selected');
       }
       colorBox[index].classList.add('selected');
+      changeColor();
     })
+  }
+}
+
+function changeColor() {
+  const colorBox = document.querySelectorAll('.color');
+  const selected = document.querySelector('.selected');
+  
+  for (let index = 0; index < colorBox.length; index += 1) {   
+    const classListColor = colorBox[index].classList;
+    if (classListColor[2] === 'selected') {      
+      selected.style.backgroundColor = `${classListColor[1]}`;       
+    }      
   }
 }
