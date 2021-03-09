a1ee1dd6775a946716233ea506d9c81c8f184d38
Daniel <dsouzaborini@gmail.com>
2021-03-09 17:38:06 -0300

    add selecao de cores

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let lado = 5;
 let numeroPixels = lado * lado;
 
 const pixelBoard = document.getElementById("pixel-board");
+const paleta1 = document.getElementById("paleta1");
+const paleta2 = document.getElementById("paleta2");
+const paleta3 = document.getElementById("paleta3");
+const paleta4 = document.getElementById("paleta4");
 
 for (let index = 1; index <= lado; index++) {
     createLines("linha" + index);
@@ -17,8 +21,45 @@ for (let index = 1; index <= lado; index++) {
     }
 }
 
+paleta1.addEventListener("click", function() {
+    selectColor("paleta1");
+})
 
+paleta2.addEventListener("click", function() {
+    selectColor("paleta2");
+})
 
+paleta3.addEventListener("click", function() {
+    selectColor("paleta3");
+})
+
+paleta4.addEventListener("click", function() {
+    selectColor("paleta4");
+})
+
+function selectColor(corPaleta) {
+    if (corPaleta == "paleta1") {
+        paleta1.className = "color selected";
+        paleta2.className = "color";
+        paleta3.className = "color";
+        paleta4.className = "color";
+    } else if (corPaleta == "paleta2") {
+        paleta1.className = "color";
+        paleta2.className = "color selected";
+        paleta3.className = "color";
+        paleta4.className = "color";
+    } else if (corPaleta == "paleta3") {
+        paleta1.className = "color";
+        paleta2.className = "color";
+        paleta3.className = "color selected";
+        paleta4.className = "color";
+    } else if (corPaleta == "paleta4") {
+        paleta1.className = "color";
+        paleta2.className = "color";
+        paleta3.className = "color";
+        paleta4.className = "color selected";
+    }
+}
 
 function createLines(id) {
     let line = document.createElement("div");
