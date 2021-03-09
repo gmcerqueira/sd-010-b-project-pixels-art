de74a7c663cead69a3b7be6855e29cea510809b3
Jessie <miss_sc_jessi@hotmail.com>
2021-03-09 18:19:50 -0300

    100% obrigatorios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const tabelaCores = document.querySelectorAll('.color');
-const pixel = document.getElementsByClassName('pixel');
+const pixel = document.querySelectorAll('.pixel');
 
 function clicaCor() {
   for (let index = 0; index < tabelaCores.length; index += 1) {
     tabelaCores[index].addEventListener('click', function (event) {
-      for (let index = 0; index < tabelaCores.length; index += 1) {
-        tabelaCores[index].classList.remove('selected');
+      for (let index1 = 0; index1 < tabelaCores.length; index1 += 1) {
+        tabelaCores[index1].classList.remove('selected');
         event.target.classList.add('selected');
-        tabelaCores[index] = event.target;
+        tabelaCores[index1] = event.target;
       }
     });
   }
@@ -18,12 +18,22 @@ clicaCor();
 function colorirPixel() {
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].addEventListener('click', function (event) {
-      let corSelecionada = window.getComputedStyle(
-        document.querySelector('.selected')
-      ).backgroundColor;
+      const corSelecionada = window.getComputedStyle(
+        document.querySelector('.selected')).backgroundColor;
       event.target.style.backgroundColor = corSelecionada;
     });
   }
 }
 
 colorirPixel();
+
+function limparPixel() {
+    const botaoLimpar = document.querySelector('#clear-board');
+    botaoLimpar.addEventListener('click', function (){
+        for(let index = 0; index < pixel.length; index += 1){
+            pixel[index].style.backgroundColor = 'white';
+        }
+    })
+}
+
+limparPixel();
