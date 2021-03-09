2acc715652e069fa44bf1a7c001d1a5cd50cb0b7
Carlos <cemargato@gmail.com>
2021-03-09 17:59:06 -0300

    desecrevendo o js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 
-let pixelBoard = document.querySelectorAll('lines');
-let columns = 5
-let lines = document.createElement('tr');
-for (let index = 1; index < columns; index += 1) {
-
-pixelBoard.appendChild(lines)  
-}
 
