f672dd5e58f8d0d9517357e651059814249625c4
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-09 18:01:14 -0300

    tentando resolver questõs de 1 à 7

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function gerar_cor() {
+    let preencherCor = document.querySelectorAll(".color");
+
+    for (let cont = 1; cont < preencherCor.length; cont += 1) {
+
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+        let randomColor = `rgb(${r}, ${g} , ${b})`;
+
+        if (r == 255 && g == 255 && b == 255) {
+            r = Math.random() * 255;
+            g = Math.random() * 255;
+            b = Math.random() * 255;
+            randomColor = `rgb(${r}, ${g} , ${b})`;
+        }
+
+        preencherCor[cont].style.backgroundColor = randomColor;
+        preencherCor[0].style.backgroundColor = "rgba(0,0,0)";
+
+
+    }
+}
+gerar_cor();
+
+function geraPixel() {
+    let numeroPixels = 25;
+    let pixels = document.getElementById("pixel-board");
+
+
+    for (let cont = 0; cont < numeroPixels; cont++) {
+        let itemPixel = document.createElement('div');
+        itemPixel.className = 'pixel';
+        pixels.appendChild(itemPixel);
+    }
+}
+geraPixel();
