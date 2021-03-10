243184bfd32c1aca84ffaf97c8c3c068e10341d9
root <diegoserafim1@gmail.com>
2021-03-09 23:17:54 -0300

    D7 tentando selecionar as cores

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const title = document.querySelector('#title');
+// const title = document.querySelector('#title');
 const colorPalette = document.querySelector('#color-palette');
-const color = document.querySelector('.color');
-const pixelBoard = document.querySelector('#pixel-board');
-const pixel = document.querySelector('.pixel');
+// const color = document.querySelector('.color');
+// const pixelBoard = document.querySelector('#pixel-board');
+// const pixel = document.querySelector('.pixel');
+
+colorPalette.addEventListener('click', event => {
+    const colorSelected = document.getElementById(event.target.id);
+    const verify = colorSelected.className;
+        verify == 'color-palette' ? (
+            event.stopPropagation()
+        )
+        (
+            document.querySelector('.selected').className = 'color', colorSelected.className = 'color selected')
+}, false);
