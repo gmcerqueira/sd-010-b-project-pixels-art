b081d376683f2ef7acc3b6d7e5a9a9e5168b8794
joao966 <joaooliveira966@gmail.com>
2021-03-09 21:36:07 -0300

    teste requisito 4

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ for(index = 0; index < 4; index += 1){
     paleta.appendChild(criaColuna);
 }
 
+// criaColuna.style.backgroundColor = "black";
+
 colunaPaleta = document.getElementsByClassName("color");
 colunaPaleta[0].style.backgroundColor = "black";
 colunaPaleta[1].style.backgroundColor = "green";
@@ -16,6 +18,12 @@ colunaPaleta[3].style.backgroundColor = "yellow";
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
+// colunaPaleta2 = document.getElementsByClassName("pixel");
+// colunaPaleta2[0].style.backgroundColor = "white";
+// colunaPaleta2[1].style.backgroundColor = "white";
+// colunaPaleta2[2].style.backgroundColor = "white";
+// colunaPaleta2[3].style.backgroundColor = "white";
+
 let quadrado = document.createElement("table");
 quadrado.id = "pixel-board";
 document.body.appendChild(quadrado);
@@ -23,10 +31,10 @@ document.body.appendChild(quadrado);
 for(i = 0;i < 5;i += 1){
     let linhaTabela = document.createElement("tr");
 
-for(j = 0;j < 5; j += 1){
-    let criaColuna = document.createElement("td");
-    criaColuna.className = "pixel";
-    linhaTabela.appendChild(criaColuna);
+for(j = 0;j < 5;j += 1){
+    let criaColuna2 = document.createElement("td");
+    criaColuna2.className = "pixel";
+    linhaTabela.appendChild(criaColuna2);
     quadrado.appendChild(linhaTabela);
  }
 }
