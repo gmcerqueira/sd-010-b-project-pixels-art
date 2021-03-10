0d828638e6dcf39a46d4596d840db1b66fcf2a54
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-10 13:18:39 -0300

    Corrigindo espaçamento de linhas

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ createPalette();
 function createPixels() {
   for (let index = 0; index < 5; index += 1) {
     const line = document.createElement('div');
-    line.className = 'line';
     containerQuadro.appendChild(line);
 
     for (let index2 = 0; index2 < 5; index2 += 1) {
