2bdd0a5ac8a9dc624447753a3062b87de8804248
Cristian <cristian.bugs@gmail.com>
2021-03-09 22:58:40 -0300

    some errors fixed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let pixel = document.getElementById("pixel-board");
+const pixel = document.getElementById('pixel-board');
 
-
-function matrix () {
-for(index = 0; index < 5; index += 1) {
+function matrix(){
+for(let index = 0; index < 5; index += 1) {
     let square = document.createElement("div");
     pixel.appendChild(square);
     
-    for ( index2 = 0; index2 < 5; index2 += 1) {
+    for (let index2 = 0; index2 < 5; index2 += 1) {
         let variable = document.createElement("div");
         square.appendChild(variable);
         variable.className = "pixel"
         };
     };
 };
+
 matrix ()
 //questão resolvida com a ajuda do colega durante grupo de estudos;
