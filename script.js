5e27448d44f626fd8cf8347d4699dd64eda08cad
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-03-10 13:34:53 -0300

    mudança nas estruturas para cumprir os requisitos com funçoes js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function randomColor(opacity = 1) {
+    let color = document.querySelectorAll(".color");
+    for(let index = 0; index < color.length; index += 1) {
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+        let generatedColor = `rgb(${r}, ${g}, ${b}, ${opacity})`;
+        color[index].style.backgroundColor = generatedColor;
+    }
+    color[0].style.backgroundColor = "rgba(0, 0, 0, 1)";
+}
