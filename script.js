ee52869a23b09d121e2f743328d73ec9769643b3
renatogsouza <renatomacedo1995@gmail.com>
2021-03-10 13:16:57 -0300

    remove e adiciona a classe selected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Cria os Pixels que vão ser coloridos 
 function criaPixels() {
-    let quadro = document.getElementById('pixel-board');
+  let quadro = document.getElementById('pixel-board');
   for (let j = 0; j < 5; j++) {
     let novaLinha =document.createElement('div');    
     for (let i = 0; i < 5; i++) {
@@ -11,14 +11,17 @@ function criaPixels() {
     quadro.appendChild(novaLinha);
   }
 }
+
 // Evento para ouvir o chamado de pegar a cor
 function chamado() {
-  let corSelecionada = document.getElementsByClassName('color');
-  for(let i = 0; i < corSelecionada.length; i++) {
-  corSelecionada[i].addEventListener('click', function(){
-  this.classList.add('Selected');
-  });
-  }
+  let remove = document.getElementsByClassName('selected')[0];
+  remove.classList.remove('selected');  
+  event.target.classList.add('selected');
+}
+// adicionando funcao de click
+let chamando = document.querySelectorAll('.color');
+for (let i = 0; i < chamando.length; i++) {
+chamando[i].addEventListener("click", chamado);
 }
 // Pega a cor selecionada para preencher o pixel
 function pegaCor() {
@@ -26,17 +29,18 @@ function pegaCor() {
 }
 // Inicia com a cor preta para preenchimento dos pixels
 function corInicial() {
-  let corPreta = document.querySelector('.c1');
+  let corPreta = document.querySelector('.c4');
   corPreta.className = corPreta.className + ' selected';
 }
 function pintarPixel() {
   let pintar = document.getElementsByClassName('pixel');
   for(let i = 0; i < pintar.length; i++) {
-		pintar[i].addEventListener("click", function() {
-			// this.style.backgroundcolor = '';
-  });
+    pintar[i].addEventListener("click", function() {
+      // this.style.backgroundcolor = '';
+    });
+  }
 }
-}
+
 
 // Inicialização do site 
 corInicial();
