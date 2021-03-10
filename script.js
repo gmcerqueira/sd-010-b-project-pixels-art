8e9f49058e3244afc0118a65cca4078c1590d9c1
Flavio Andrade <foa12381@gmail.com>
2021-03-10 13:39:16 -0300

    teste2

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ cor[1].style.backgroundColor = 'purple';
 cor[2].style.backgroundColor = 'orange';
 cor[3].style.backgroundColor = 'green';
 
-let quant = 25
 function createDivpixel(){
+  let quant = 25
   let divContainer = document.querySelector('#pixel-board');
 
   for (let i = 0; i < quant; i += 1){
@@ -17,33 +17,30 @@ function createDivpixel(){
 }
 createDivpixel()
 
-let corInicial = "black"
-let selecao;
-function corSelected(corSelected){
-  selecao = addEventListener.target
-  cor[0].style.backgroundColor = 'black';
-  cor[1].style.backgroundColor = rgb();
-  cor[2].style.backgroundColor = rgb();
-  cor[3].style.backgroundColor = rgb();
-  if(selecao.style.backgroundColor === 'color'){
-    corInicial.style.background = 'color selected'
-  }else {
-    corInicial.style.background = 'color'
-  }
+// let corInicial = "black"
+// let selecao;
+// function corSelected(corSelected){
+//   selecao = addEventListener.target
+//   cor[0].style.backgroundColor = 'black';
+//   cor[1].style.backgroundColor = rgb();
+//   cor[2].style.backgroundColor = rgb();
+//   cor[3].style.backgroundColor = rgb();
+//   if(selecao.style.backgroundColor === 'color'){
+//     corInicial.style.background = 'color selected'
+//   }else {
+//     corInicial.style.background = 'color'
+//   }
+// }
+// corSelected('click')
 
 
+// function rgb(){
+//   const r = Math.ceil(Math.random() * 255);
+//   const g = Math.ceil(Math.random() * 255);
+//   const b = Math.ceil(Math.random() * 255);
 
-}
-corSelected('click')
-
-
-function rgb(){
-  const r = Math.ceil(Math.random() * 255);
-  const g = Math.ceil(Math.random() * 255);
-  const b = Math.ceil(Math.random() * 255);
-
-  return `rgb(${r}, ${g}, ${b})`;
-}
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
 
 
 
