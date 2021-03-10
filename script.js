c968507a9a43b8628c43fece2f271b0627f56242
isTorres <ismaeltorres612@gmail.com>
2021-03-10 03:27:09 -0300

    refatorando tudo - 6

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+window.onload = function init() {
+  localStorage.setItem('backgroundColor', 'black');
+};
 // Functions:
 
 function createElements(tag) {
@@ -6,17 +9,20 @@ function createElements(tag) {
 }
 
 function createColorPalette() {
+  const palettes = document.getElementById('color-palette').childNodes;
+  const colors = ['black', 'blue', 'red', 'cyan'];
+
   for (let i = 0; i < 4; i += 1) {
     const element = createElements('div');
     element.className = 'color';
     document.querySelector('#color-palette').appendChild(element);
   }
-  const colors = ['black', 'blue', 'red', 'cyan'];
-  const palettes = document.getElementById('color-palette').childNodes;
   palettes.forEach((nodes, index) => {
     const elements = nodes;
     elements.style.backgroundColor = colors[index];
   });
+
+  palettes[0].classList.add('selected');
 }
 createColorPalette();
 
@@ -32,6 +38,26 @@ function createPixels() {
 createPixels();
 
 // // Events:
+
+// function colorPicker () {
+//   const ref = document.querySelectorAll('.color');
+
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//   }
+// }
+// for (let i = 0; i < referencias.length; i++) {
+//   referencias[i].addEventListener('click', (e) => {
+//       selecionada.style.backgroundColor = e.target.style.backgroundColor
+//   })
+// }
+
+// for (let i = 0; i < pixels_em_branco.length; i++) {
+//   pixels_em_branco[i].addEventListener('click', (e) => {
+//       e.target.style.backgroundColor = selecionada.style.backgroundColor
+//   })
+// }
+
 // function clickColorPicker() {
 //   let getSelected = colorPalette[i].style.backgroundColor;
 //   const colorPalette = document.querySelector('#color-palette').children;
