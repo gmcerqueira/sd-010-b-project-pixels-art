d80c15e2ba05a8754ec1b7c4c317311aba94d159
Malbathan Lima <malbathanlima@gmail.com>
2021-03-09 22:46:21 -0300

    correçao do background white box

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 window.onload = function(){
-columns.style.backgroundColor = 'white';
-}
 
 let columns = document.getElementById('pixel-board');
 
@@ -8,13 +6,19 @@ let tableLines = function(){
 	for (let index = 0; index < 5; index +=1) {
 		line = document.createElement ('div')
 		line.className = 'pixel';
+		line.style.backgroundColor = 'white';
 		columns.appendChild(line);
 		for (let index = 0; index < 5; index +=1 ){
 			container = document.createElement('div')
 			container.className = 'pixel';
+			container.style.backgroundColor = 'white';
 			line.appendChild(container);
 		}
 	}
 }
 
 tableLines();
+
+// let boxPixel = document.getElementsByClassName('pixel')
+// 	boxPixel.style.backgroundColor = 'white';
+}
