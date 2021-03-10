b9f244fdb09fb3775c32545fb05f662795f2c866
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-10 16:06:59 -0300

    criando a paleta de cores

---
+++ script.js
@@ -0,0 +1,11 @@
+let coresPaleta = document.querySelectorAll('.color');
+let preto = coresPaleta[0];
+let vermelho = coresPaleta[1];
+let verde = coresPaleta[2];
+let azul = coresPaleta[3];
+
+preto.style.backgroundColor = 'black';
+vermelho.style.backgroundColor = 'red';
+verde.style.backgroundColor = 'green';
+azul.style.backgroundColor = 'blue';
+
