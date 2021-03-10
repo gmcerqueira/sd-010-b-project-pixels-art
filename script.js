96da9a375b1c1e707b1be7fef01a847763ccb8e4
Flavio Andrade <foa12381@gmail.com>
2021-03-10 13:31:35 -0300

    divs e rgb

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let cor = document.querySelectorAll('.color');
+const cor = document.querySelectorAll('.color');
 cor[0].style.backgroundColor = 'black';
 cor[1].style.backgroundColor = 'purple';
 cor[2].style.backgroundColor = 'orange';
 cor[3].style.backgroundColor = 'green';
-//gera paleta de cores
+
 let quant = 25
 function createDivpixel(){
   let divContainer = document.querySelector('#pixel-board');
@@ -16,5 +16,34 @@ function createDivpixel(){
   }
 }
 createDivpixel()
+
+let corInicial = "black"
+let selecao;
+function corSelected(corSelected){
+  selecao = addEventListener.target
+  cor[0].style.backgroundColor = 'black';
+  cor[1].style.backgroundColor = rgb();
+  cor[2].style.backgroundColor = rgb();
+  cor[3].style.backgroundColor = rgb();
+  if(selecao.style.backgroundColor === 'color'){
+    corInicial.style.background = 'color selected'
+  }else {
+    corInicial.style.background = 'color'
+  }
+
+
+
+}
+corSelected('click')
+
+
+function rgb(){
+  const r = Math.ceil(Math.random() * 255);
+  const g = Math.ceil(Math.random() * 255);
+  const b = Math.ceil(Math.random() * 255);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 
 
