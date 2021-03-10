a8fd8cdbf34fe9c32d9f068a87525565ac0bf60a
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-09 22:16:55 -0300

    4 - Adicione à página um quadro de pixels, com 25 pixels. DONE

--- script.js
+++ script.js
@@ -5,11 +5,38 @@ let colorsList = [
   'rgb(0, 48, 73)'
 ];
 
+let colorItems = document.querySelectorAll('.color');
+
 function buttonColor() {
-  let colorItems = document.getElementsByClassName('color');
   for (let i = 0; i < colorItems.length; i++) {
     colorItems[i].style.backgroundColor = colorsList[i];
   }
 }
 
 buttonColor();
+
+let pixelBoard = document.querySelector('#pixel-board'); // All board
+console.log(pixelBoard);
+
+let j = 1
+function createPixel(rowNumber) {
+	let pixel = document.createElement('td');
+	let row = document.querySelectorAll(`#pixel-board>tr`);
+	console.log(row);
+	pixel.className = 'pixel';
+	row[rowNumber].appendChild(pixel);
+}
+
+let i = 1;
+function createRow(rowNumber) {
+	let row = document.createElement('tr');
+	console.log(i);
+	pixelBoard.appendChild(row);
+	for (let i = 0; i < 5; i++) {
+		createPixel(rowNumber);
+	}
+}
+
+for (let i = 0; i < 5; i++) {
+	createRow(i);
+}
