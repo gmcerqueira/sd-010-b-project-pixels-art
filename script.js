67badf1e33cd1d425e970213c91c58ccd0fe978d
Malbathan Lima <malbathanlima@gmail.com>
2021-03-09 21:00:28 -0300

    Tabela de quadrados vazios

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-window.onload ;
+let columns = document.getElementById('pixel-board');
 
+let tableLines = function(){
+	for (let index = 0; index < 5; index +=1) {
+		line = document.createElement ('div')
+		line.className = 'pixel';
+		columns.appendChild(line);
+		for (let index = 0; index < 5; index +=1 ){
+			container = document.createElement('div')
+			container.classList.add ('pixel');
+			line.appendChild(container);
+		}
+	}
+}
+
+tableLines();
