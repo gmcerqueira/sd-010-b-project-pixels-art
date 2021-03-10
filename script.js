b43e727313e922209d70cde53c434f69497bae8e
André <hammel.engineer@gmail.com>
2021-03-09 22:06:53 -0300

    ->concluido item 4, 5 6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function colourSetUp(colorArray) {
-  const colors = colorArray;
+  let colors = colorArray;
   colors[0].style.backgroundColor = 'black';
   colors[1].style.backgroundColor = 'red';
   colors[2].style.backgroundColor = 'blue';
@@ -20,5 +20,32 @@ window.onload = function () {
     colourSetUp(colorsDiv);
   }
 
+  function makeBoard() {
+    const pixelBoard = document.createElement('div'); //table
+    pixelBoard.setAttribute('id', 'pixel-board')
+    document.body.appendChild(pixelBoard);
+    for (let i = 0; i < 5; i += 1) {
+      const headRow = document.createElement('div') //th
+      headRow.setAttribute('class', 'row-head');
+      pixelBoard.appendChild(headRow);
+      for (let j = 0; j < 5; j += 1) {
+        const lineElemento = document.createElement('div') //td
+        lineElemento.setAttribute('class', 'pixel')
+        headRow.appendChild(lineElemento);
+      }
+    }
+  }
+
+  function selectedBlack() {
+    let colorsDiv = document.querySelectorAll('.color');
+    for (let k = 0; k < colorsDiv.length; k += 1) {
+      if (colorsDiv[k].style.backgroundColor === 'black') {
+        colorsDiv[k].className = 'color selected';
+      }
+    }
+  }
+
   paletteColour(4);
+  makeBoard();
+  selectedBlack();
 };
