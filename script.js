f44019c71f821fe3d765dc15b4141c508642ef30
Holistic <paulofreire.si@outlook.com>
2021-03-10 08:26:43 -0300

    working towards requirement 4, saving some progress

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
- const pixelLenght = 5;
- cont pixelHeight = 5;
+ //const pixelLenght = 5;
+ //const pixelHeight = 5;
+//OLD IDEAD GOT COMMENTED FOR POSSIBLE LATER REUSE
+ //function createBox() {
+//for (let iLenght; iLenght < pixelLenght; iLenght += 1) {
+//  let box = document.createElement("div");
+//  box.className = "pixel";
+//  box.appendChild();
+//}
+//}
 
- for (let iLenght; iLenght < pixelLenght; iLenght += 1) {
-   let box = document.createElement("div");
-   box.className = "pixel";
- }
+const boxes = 5;
+const row = 5;
+
+function creatBox(boxes) {
+  let body = document.body;
+  for (let i = 0; i < boxes; i += 1) {
+    let creation = document.createElement("div")
+    creation.className = 'pixel';
+    body.appendChild(creation);
+  }
+}
+
+creatBox(boxes);
