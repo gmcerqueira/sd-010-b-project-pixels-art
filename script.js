b5040f0ffd46bca3a9f757089f3f1de634963fa4
Holistic <paulofreire.si@outlook.com>
2021-03-10 10:41:41 -0300

    redid my entire logic for requirement 4... found it easier this way

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
- //const pixelLenght = 5;
- //const pixelHeight = 5;
-//OLD IDEAD GOT COMMENTED FOR POSSIBLE LATER REUSE
- //function createBox() {
-//for (let iLenght; iLenght < pixelLenght; iLenght += 1) {
-//  let box = document.createElement("div");
-//  box.className = "pixel";
-//  box.appendChild();
-//}
-//}
-
 const boxes = 5;
-const row = 5;
+const table = document.getElementById('pixel-board');
 
-function creatBox(boxes) {
-  let body = document.body;
-  for (let i = 0; i < boxes; i += 1) {
-    let creation = document.createElement("div")
-    creation.className = 'pixel';
-    body.appendChild(creation);
+function pixelBoard() {
+    for (let i = 0; i < boxes; i += 1) {
+      const createRow = document.createElement('tr');
+      table.appendChild(createRow);
+      for (let i2 = 0; i2 < boxes; i2 += 1) {
+        const createBox = document.createElement('td');
+        table.appendChild(createBox).className = 'pixel';
+      }
+    }
   }
-}
-
-creatBox(boxes);
+  
+  pixelBoard();
