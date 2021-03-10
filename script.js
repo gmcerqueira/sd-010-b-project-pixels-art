b20c0db0e2d7a754e0230eb02bfc65ac6b35f098
Jander Liborio <jandernunes14@gmail.com>
2021-03-10 10:00:21 -0400

    verificando os commits

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let color = ['black', 'red', 'blue', 'green'];
-let colorPalette = document.querySelector('#color-palette');
-for (let index = 0; index < color.length; index += 1) {
-  let colorGround = color[index];
-  let paletteItem = document.createElement('div');
-  paletteItem.className = 'color';
-  paletteItem.style.backgroundColor = colorGround;
-  colorPalette.appendChild(paletteItem);
+function paleta() {
+  let color = ['black', 'red', 'blue', 'green'];
+  const colorPalette = document.querySelector('#color-palette');
+  for (let index = 0; index < color.length; index += 1) {
+    let colorGround = color[index];
+    let paletteItem = document.createElement('div');
+    paletteItem.className = 'color';
+    paletteItem.style.backgroundColor = colorGround;
+    colorPalette.appendChild(paletteItem);
+  }
 }
+paleta();
+const quadro = document.querySelectorAll(".quadro");
+function quadrado() {
+  for (let index = 0; index < quadro.length; index += 1) {
+    for (let indexQ = 1; indexQ == 5; indexQ += 1) {
+      let pixelCreado = document.createElement('div');
+      pixelCreado.className = 'pixel';
+      quadro[index].appendChild(pixelCreado);
+    }
+  }
+}
+quadrado();
