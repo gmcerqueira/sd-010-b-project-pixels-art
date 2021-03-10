f1bdf777559bc372d72fa18c3c84a635239bfd6d
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-10 14:34:52 -0300

    foi criado 1 div com id pixel-board e outras divs filhas com a class pixel (item 4)

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const boxBlack = colorPalette.children[0].children[0];
 const box1 = colorPalette.children[0].children[1];
 const box2 = colorPalette.children[0].children[2];
 const box3 = colorPalette.children[0].children[3];
+const main = document.querySelector('.main');
 
 // código retirado do forum https://stackoverflow.com/questions/1484506/random-color-generator
-// código gera cores aleatórias
+// função que gera cores aleatórias para cada box da paleta de cor
 function getRancomColor() {
   let color = '#';
   const letters = '0123456789ABCDEF';
@@ -15,7 +16,25 @@ function getRancomColor() {
   return color;
 }
 
+// Cor de cada quadro da paleta
 boxBlack.style.backgroundColor = 'black';
 box1.style.backgroundColor = getRancomColor();
 box2.style.backgroundColor = getRancomColor();
 box3.style.backgroundColor = getRancomColor();
+
+// criando box central
+const divPixelBoard = document.getElementById('pixel-board');
+
+for (let i = 1; i <= 5; i += 1) {
+  const divPixelLine = document.createElement('div');
+  divPixelLine.className = 'pixel tr';
+  divPixelLine.style.backgroundColor = 'white';
+  divPixelBoard.appendChild(divPixelLine);
+
+  for (let i2 = 1; i2 <= 5; i2 += 1) {
+    const divPixelCol = document.createElement('div');
+    divPixelCol.className = 'pixel td';
+    divPixelCol.style.backgroundColor = 'white';
+    divPixelLine.appendChild(divPixelCol);
+  }
+}
