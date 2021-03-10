295c340e576c9bd08f2958e849ca755ef3bb7d68
Misael Bueno <misa.hbueno@gmail.com>
2021-03-10 08:28:30 -0300

    refatorando requisito 4

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let pixelBoard = document.getElementById("pixel-board");
+
+for (let cont = 1; cont <= 5; cont+=1) {
+  let div = document.createElement("div");
+  div.classList.add("tr")
+  for (let cont2 = 1; cont2 <= 5; cont2+=1){
+    let div2 = document.createElement("div");
+    div2.classList.add("pixel");
+    div.appendChild(div2);
+  }  
+  pixelBoard.appendChild(div);
+}
+
+
 let selected = document.querySelectorAll(".color");
 
 for (let index = 0; index < selected.length; index+=1) {
