d40aae3e2294d3479871ae55e82f61b6f9080e71
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-10 20:38:20 -0300

    Refatorei o html e o script. Provavelmente perdi algum dos requisitos

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-window.onload = function () {
+//Paleta de Cores
+let colorPalette = ['black', 'green', 'red', 'blue'];
+let nonSelected = document.getElementsByClassName('color');
+let heldColor = '';
+// let selectedPalette = document.querySelector('.selected');
+
+//Botões da paleta de cores
+let getPaletteList = document.querySelector('#color-palette');
+
+for (index = 0; index < 4; index += 1) {
+  let paletteBox = document.createElement('div');
+  paletteBox.id = colorPalette[index];
+  paletteBox.className = 'color';
+  getPaletteList.appendChild(paletteBox);
+  paletteBox.addEventListener('click', function () {
+    paletteBox.classList.add('selected');
+    paletteBox.classList.remove('color');
+  });
 }
+
+//Número de Pixels
+let pixelsList = [];
+for (index = 0; index < 25; index += 1) {
+  pixelsList.push(index);
+};
+
+//Criando a tabela: 
+let getPixelsList = document.querySelector('#pixel-row');
+
+for (let row = 0; row < 5; row += 1) {
+  let pixelRow = document.createElement('tr');
+  getPixelsList.appendChild(pixelRow);
+  for (let index = 0; index < 5; index += 1) {
+    let getPixelsBoxes = getPixelsList.lastChild;
+    let pixelBox = document.createElement('td');
+    pixelBox.className = 'pixel';
+    getPixelsBoxes.appendChild(pixelBox);
+    pixelBox.addEventListener('click', function () {
+      pixelBox.style.backgroundColor = heldColor;     
+    });
+  };
+};
+
+// function paintPixel () {
+//   // let getColorFromPalette = document.querySelector('.selected');
+
+//   console.log('click no pixel');
+// }
