c9089e9cebc0a15778038d006d2de6a3fcec0ae5
thosijulio <thosijulio@gmail.com>
2021-03-09 23:39:39 -0300

    correcao exercicios 4 e 5

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function createPixelBoard () {
-  let pixelBoard = document.getElementById('pixel-board');
+function createPixelBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let pixelLine = document.createElement('div');
+    const pixelLine = document.createElement('div');
     pixelLine.id = 'pixelLine';
     pixelLine.style.display = 'inline-block';
     pixelLine.style.marginTop = '70px';
     pixelBoard.appendChild(pixelLine);
 
     for (let index2 = 0; index2 < 5; index2 += 1) {
-      let pixel = document.createElement('div');
-      pixel.id = 'pixel';
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
       pixel.style.border = '1px solid black';
       pixel.style.width = '40px';
       pixel.style.height = '40px';
