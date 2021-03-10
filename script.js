043e46ff10ee9899da79e9fdc26a0b08ace76ea6
Jander Liborio <jandernunes14@gmail.com>
2021-03-09 20:07:28 -0400

    2 requisito

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// const color = [black, red, blue, green];
-const colorPalette = document.querySelector('#color-palette');
-for (let index = 0; index < colorPalette.length; index += 1) {
-  let colorGround = color[index];
+// let color = ["black", "red", "blue", "green"];
+// let colorPalette = document.querySelector("#color-palette");
+// for (let index = 0; index < cores.length; index += 1) {
+//   let colorGround = color[index];
+//   let paletteItem = document.createElement('div');
+//   paletteItem.className = 'color';
+//   paletteItem.style.backgroundColor = colorGround;
+//   paletteItem.appendChild(paletteItem);
+// }
+let color = ['black', 'red', 'blue', 'green'];
+let colorPalette = document.querySelector('#color-palette');
+for(let i=0; i<cores.length; i++){
+  let cor = cores[i];
+  let criarCor = document.createElement("div");
+  criarCor.className = "color";
+  criarCor.style.backgroundColor = cor;
+  colorPalette.appendChild(criarCor);
 }
