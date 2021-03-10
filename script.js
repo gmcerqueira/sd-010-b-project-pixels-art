573b8554d063e6d3bfd7bf7826eec676e8e91a9e
aline <aline.melryaa@gmail.com>
2021-03-10 17:59:06 -0400

    troca de cor

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let paletaCores = document.getElementById("color-palette")
-function trocaCor(){
-let cor = document.getElementById("red").value;
-document.getElementById("black").style.background = "black"
+ListeningStateChangedEvent
+function trocadiv(){
+
+
 }
 
 
