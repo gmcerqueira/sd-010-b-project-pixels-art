ec35a02e4e9ece88d79ecb5575b216190f33078a
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-10 13:10:12 -0300

    Tirando cor aleatoria

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const containerPalette = document.querySelector('#color-palette');
 const containerQuadro = document.querySelector('#pixel-board');
-//criando div para plateas de cores
+
 function createPalette() {
   for (let index = 0; index < 4; index += 1) {
     const selectors = document.createElement('div');
@@ -14,13 +14,13 @@ function createPalette() {
 }
 
 createPalette();
-//gerando div para ser linhas para o quadrado
+
 function createPixels() {
   for (let index = 0; index < 5; index += 1) {
     const line = document.createElement('div');
     line.className = 'line';
     containerQuadro.appendChild(line);
-//gerando div para ser colunas para o quadrado
+
     for (let index2 = 0; index2 < 5; index2 += 1) {
       const column = document.createElement('div');
       column.className = 'pixel';
@@ -31,18 +31,3 @@ function createPixels() {
 }
 
 createPixels();
-//função para gerar cores aleatorias
-function corAleatoria() {
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
-
-  //setando o primeiro quadrado com a cor black
-  document.getElementsByClassName('color')[0].style.backgroundColor = 'rgb(0,0,0)';
-  //gerando cores aleatorias para os outros quadrados
-  document.getElementsByClassName('color')[1].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-  document.getElementsByClassName('color')[2].style.backgroundColor = 'rgb(' + g + ',' + r + ',' + b + ')';
-  document.getElementsByClassName('color')[3].style.backgroundColor = 'rgb(' + b + ',' + g + ',' + r + ')';
-
-}
-corAleatoria();
