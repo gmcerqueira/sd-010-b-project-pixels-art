74f4ceafb6759ee002cd3723a4e84ccab1c5b052
Camila Arruda <camilaam1802@gmail.com>
2021-03-10 11:18:33 -0300

    Tentando resolver a 7 novamente

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+let box1 = document.getElementById("color1");
+let box2 = document.getElementById("color2");
+let box3 = document.getElementById("color3");
+let box4 = document.getElementById("color4");
 
-let select = document.querySelectorAll(".color");
+box2.addEventListener("click",selectColor);
+box3.addEventListener("click",selectColor);
+box4.addEventListener("click",selectColor);
+box5.addEventListener("click",selectColor);
+
+
+function selectColor(event){
+    let elementSelecionado = document.querySelector('.selected');
+    elementSelecionado.classList.remove('selected');
+    event.target.classList.add('selected');
+}
+
+
+
+
+/* let select = document.querySelectorAll(".color");
 for (let index = 0; index < select.length; index +=1){
     select.addEventListener("click",selectColor); 
 }
 
 
 function selectColor(event){
-    let elementSelecionado = document.querySelector('.color .selected'); 
-    elementSelecionado.classList.remove('color selected');
-    event.target.classList.add('color selected');
+    for(let index = 0; index < select.length; index += 1){
+        if(select[index].className() === "color selected"){
+            let elementSelecionado = document.querySelector('.selected'); 
+            elementSelecionado.classList.remove('selected');
+            event.target.classList.add('selected');
+        }
 }
+} 
 
+ */
