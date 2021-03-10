60c3e1c022b1108439f8c326e7eb7ebddf95fae4
Camila Arruda <camilaam1802@gmail.com>
2021-03-10 14:40:54 -0300

    Adicionando requisito 8

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let box1 = document.getElementById("color1");
 let box2 = document.getElementById("color2");
 let box3 = document.getElementById("color3");
 let box4 = document.getElementById("color4");
-
+//document.getElementById("color-palette").addEventListener("click",selectColor); 
 box2.addEventListener("click",selectColor);
 box3.addEventListener("click",selectColor);
 box4.addEventListener("click",selectColor);
@@ -15,4 +15,14 @@ function selectColor(event){
 }
 
 
-
+//Função para pintar os Pixels
+function colorPixel () {
+    let pixel = document.getElementsByClassName('pixel')
+    for (let index = 0; index < pixel.length; index += 1) {
+      pixel[index].addEventListener('click',function() {
+        let selectedColor = document.querySelector('.selected')
+          pixel[index].style.backgroundColor = window.getComputedStyle(selectedColor).getPropertyValue('background-color');
+      })
+    }
+  }
+  colorPixel(); 
