997e4ce62285c23f5bd81e3942d5874a791a9a59
Holistic <paulofreire.si@outlook.com>
2021-03-09 22:38:19 -0300

    starting to do something with my JS file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function loadedPage () {
-    
-}
+ const pixelLenght = 5;
+ cont pixelHeight = 5;
+
+ for (let iLenght; iLenght < pixelLenght; iLenght += 1) {
+   let box = document.createElement("div");
+   box.className = "pixel";
+ }
