7032935fe8320bbfa90617029c285c86fbee20c3
Carlos <cemargato@gmail.com>
2021-03-10 13:08:39 -0300

    transformando a table em dinamica

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+window.onload = function () {
 
 
+
+  const tabela = document.querySelector("#pixel-board")//chama tabela
+  let tamanho = 5
+
+  // linha.appendChild(pixel);
+  //   tabela.appendChild(linha)
+  for (let index = 0; index < tamanho; index += 1) {
+    let linha = document.createElement('tr');//da linha
+    for (let index2 = 0; index2 < tamanho; index2 += 1) {
+      let pixel = document.createElement('td');// da celula
+      pixel.className = 'pixel';
+      linha.appendChild(pixel)// cria a celula
+    }
+    linha.className = 'line';
+    tabela.appendChild(linha)// era pra criar as linhas
+  }
+
+}
+document.prepend
