e2712af4271ed1baa361a50b2821a8bd32823d0b
Italo Matos <italomatos764@gmail.com>
2021-03-10 10:24:20 -0400

    Adiciona cor preta como padrao

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ for(let j = 0; j < 5; j += 1) {
     for(let k = 0; k < 5; k += 1) {
       let criaPixel = document.createElement("div");
       criaPixel.className = "pixel";
-      criaPixel.backgroundColor = "white";
+      criaPixel.backgroundColor = "rgb(255, 255, 255)";
       pixelize[j].appendChild(criaPixel);
     }
 }
 
+let corPadrao = document.querySelectorAll('.color')[0];
+let criaPadrao = corPadrao.classList;
+criaPadrao.add('selected');
