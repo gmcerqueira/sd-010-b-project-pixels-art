776f2ca3b488ae7f1e398630e8c3c3d4f4f4751c
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-10 13:38:47 -0300

    feito a tarefa 10, colocando o input para o tamanho da tabela

--- script.js
+++ script.js
@@ -20,55 +20,79 @@ function criaPaleta() {
 }
 criaPaleta();
 
-function criaQuadro() {
-  for (let index = 0; index < 5; index += 1) {
+function criaQuadro(tamanho) {
+  for (let index = 0; index < tamanho; index += 1) {
     const quadro = document.querySelector('#pixel-board');
     const criaRow = document.createElement('div');
     criaRow.className = 'pixel-row';
     quadro.appendChild(criaRow);
   }
   const row = document.querySelectorAll('.pixel-row');
-  for (let index = 0; index < 5; index += 1) {
-    for (let indexDois = 0; indexDois < 5; indexDois += 1) {
+  for (let index = 0; index < tamanho; index += 1) {
+    for (let indexDois = 0; indexDois < tamanho; indexDois += 1) {
       const criaPixel = document.createElement('div');
       criaPixel.className = 'pixel';
       row[indexDois].appendChild(criaPixel);
     }
   }
 }
+let tamanhoQuadro = 5;
+criaQuadro(tamanhoQuadro);
 
-criaQuadro();
-
-function seleciona() {
+function selecionaCorPaleta() {
   const cores = document.getElementsByClassName('color');
   cores[0].classList.add('selected');
 }
+selecionaCorPaleta();
 
-seleciona();
-
-function trocaCores(cores) {
+function trocaCoresPaleta(cores) {
   const corSelecionada = document.querySelector('.selected');
   const novaCorSelecionada = cores.target;
   corSelecionada.classList.remove('selected');
   novaCorSelecionada.classList.add('selected');
 }
 const paleta = document.querySelector('#color-palette');
-paleta.addEventListener('click', trocaCores);
+paleta.addEventListener('click', trocaCoresPaleta);
 
 function pintaPixel(pixel) {
   const pixelSelecionado = pixel.target;
+  console.log(pixelSelecionado);
   const corSelecionada = document.querySelector('.selected');
   pixelSelecionado.style.backgroundColor = corSelecionada.style.backgroundColor;
 }
-const pixels = document.getElementsByClassName('pixel');
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', pintaPixel);
+
+function selecionaPixel() {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', pintaPixel);
+  }
 }
+selecionaPixel();
 
 function apagaTudo() {
+  const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'white';
   }
 }
 const botao = document.querySelector('#clear-board');
 botao.addEventListener('click', apagaTudo);
+
+function mudaTamanhoQuadro() {
+  const inputQuadro = document.querySelector('#board-size').value;
+  tamanhoQuadro = inputQuadro;
+  const quadro = document.querySelector('#pixel-board');
+  const quadrofilho = document.querySelectorAll('.pixel-row');
+  if (inputQuadro === '') {
+    alert('Board inválido!');
+  } else if (inputQuadro >= 5 && inputQuadro <= 50) {
+    for (let index = 0; index < quadrofilho.length; index += 1) {
+      quadro.removeChild(quadrofilho[index]);
+    }
+
+    criaQuadro(tamanhoQuadro);
+    selecionaPixel();
+  }
+}
+const botaoQuadro = document.querySelector('#generate-board');
+botaoQuadro.addEventListener('click', mudaTamanhoQuadro);
