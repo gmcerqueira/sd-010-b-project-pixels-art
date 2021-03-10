eb4c3d101a18709d7d5dc71fdaf157dd2cd277a0
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-09 22:07:26 -0300

    Corrigindo linter

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-const table = document.getElementById("color-palette");
-const pixel = document.getElementById("pixel-board");
+const table = document.getElementById('color-palette');
+const pixel = document.getElementById('pixel-board');
 
-function structureTable () {
-  let line1 = document.createElement("div");
-    line1.className = "palette"
+function structureTable() {
+  let line1 = document.createElement('div');
+    line1.className = 'palette'
     table.appendChild(line1);
   
   for (let columns = 0; columns < 4; columns += 1) {
-    let column = document.createElement("div");
-    column.className = "color";
+    const column = document.createElement('div');
+    column.className = 'color';
     line1.appendChild(column);
   } 
 } structureTable();
 
-let column = document.querySelectorAll(".color");
-function styleTable () {
+const column = document.querySelectorAll('.color');
+function styleTable() {
   for (let index = 0; index < column.length; index += 1){
-    column[0].style.backgroundColor = "black";
-    column[1].style.backgroundColor = "green";
-    column[2].style.backgroundColor = "red";
-    column[3].style.backgroundColor = "blue";
+    column[0].style.backgroundColor = 'black';
+    column[1].style.backgroundColor = 'green';
+    column[2].style.backgroundColor = 'red';
+    column[3].style.backgroundColor = 'blue';
   } 
 } 
 styleTable();
 
-function pixelStructure () {
+function pixelStructure() {
   for (let lines = 0; lines < 5; lines += 1) {  
-    let pixelLine = document.createElement("div");
-    pixelLine.className = "pixelLine"
+    let pixelLine = document.createElement('div');
+    pixelLine.className = 'pixelLine';
     pixel.appendChild(pixelLine);
     
     for (let columns = 0; columns < 5; columns += 1) {
-      let column1 = document.createElement("div");
+      let column1 = document.createElement('div');
       column1.className = "pixel";
       pixelLine.appendChild(column1);
     }
   }
 } pixelStructure();
 
-function transSelect () {
+function transSelect() {
   for (let index = 0; index < column.length; index += 1) {
-    column[index].classList.remove("color-selected");
+    column[index].classList.remove('color-selected');
   }
   //column[?].classList.add("color-selected");
 }
 
-window.onload = function () {
+window.onload = function() {
   column[0].classList.add("color-selected");
 }
