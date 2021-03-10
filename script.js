49a41c3f47370d88095e63a9acb6ad077aac79a1
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-10 10:46:58 -0300

    updates and script cleaning

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
-// Comentários inseridos para minha própria orientação e possíveis alterações futuras
-
+// função generalista para criação de elementos
 function createElement(tagName) {
-  // CreateElement function: https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-  const element = document.createElement(tagName); // função generalista para criação de elementos
+  const element = document.createElement(tagName);
   return element;
 }
 
+//função para criar paletas de cores
+//fontes usadas: 
+// classList: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+// forEach: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// childNodes: https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes
+// Arrow Functions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 function setPaletteColor() {
   const colorPalette = '#color-palette';
   for (let index = 0; index < 4; index += 1) {
-    const elem = createElement('span'); // cria uma span, no caso, 4
-    elem.classList.add('color'); // adiciona elementos pela id no css, fonte: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-    document.querySelector(colorPalette).appendChild(elem); // cria parentesco com colorPallete (que está recebendo a classe collor-pallete)
+    const elem = createElement('span');
+    elem.classList.add('color');
+    document.querySelector(colorPalette).appendChild(elem);
   }
-  const cores = ['black', 'red', 'green', 'blue']; // define as cores manualmente. *Nota*: Mais tarde posso inserir Math.random para deixar aleatório, mas ainda não sei como aplicá-la
-  const array = document.getElementById('color-palette').childNodes; // recebe os filhos de color-palette
-  array.forEach((element, index) => { // for loop para definir as cores usando "arrow function"
+  const cores = ['black', 'red', 'green', 'blue'];
+  const array = document.getElementById('color-palette').childNodes;
+  array.forEach((element, index) => {
     const elem = element; elem.style.backgroundColor = cores[index];
   });
-  const colors = document.getElementsByClassName('color'); // pega as cores pela classe
-  colors[0].classList.add('selected'); // define a cor selecionada ao clique
+  const colors = document.getElementsByClassName('color');
+  colors[0].classList.add('selected');
 }
 
-function setPixelBoard() { // função para o preenchimento dos pixels
+//função para preenchimento dos pixels
+function setPixelBoard() {
   for (let index = 0; index < 5; index += 1) {
     for (let i = 0; i < 5; i += 1) {
-      const elem = createElement('span'); // cria um span
-      elem.classList.add('pixel'); // adiciona o pixel
-      document.getElementById('pixel-board').appendChild(elem); // adiciona cor
+      const elem = createElement('span');
+      elem.classList.add('pixel');
+      document.getElementById('pixel-board').appendChild(elem);
     }
   }
 }
 
-function getSelected(event) { // função para trocar a cor selecionada
-  const elem = event.target; // fonte: https://developer.mozilla.org/en-US/docs/Web/API/Event/target
-  if (!elem.id) { // if abreviado para especificar algo "se for diferente", '!' significa 'não'
+// função para trocar a cor selecionada
+// event.target: https://developer.mozilla.org/en-US/docs/Web/API/Event/target
+function getSelected(event) {
+  const elem = event.target;
+  if (!elem.id) { // if abreviado para especificar algo "se for diferente", onde '!' significa 'não'
     const colors = document.querySelectorAll('.color');
-    colors.forEach((element) => element.classList.remove('selected')); // para remover a cor anteriormente selecionada, caso contrário há conflito de cores
-    elem.classList.add('selected'); // para adicionar nova cor
+    colors.forEach((element) => element.classList.remove('selected'));
+    elem.classList.add('selected');
   }
   sessionStorage.color = elem.style.backgroundColor; // define a nova cor
 }
 
-function paintPixel(event) { // função para alterar a cor de fundo dos pixels
+// função para alterar a cor de fundo dos pixels
+function paintPixel(event) {
   const elem = event.target;
   if (!elem.id) {
     const colorToPaint = sessionStorage.color;
-    elem.style.backgroundColor = colorToPaint; // troca a cor em elem para colorToPaint, que seria a nova cor
+    elem.style.backgroundColor = colorToPaint;
   }
 }
 
-function clearBoard() { // função para limpar a board dos pixels
+// função para limpar a board dos pixels
+function clearBoard() {
   const pixels = document.querySelectorAll('.pixel');
-  pixels.forEach((e) => { e.style.backgroundColor = ''; }); // troca o que tiver no pixel por ''
+  pixels.forEach((index) => { index.style.backgroundColor = ''; }); // troca o que tiver no pixel por ''
 }
-window.onload = function init() { // fonte: https://www.w3schools.com/jsref/event_onload.asp
+
+// window.load para carregar as funções na página
+window.onload = function init() {
   sessionStorage.setItem('color', 'black'); // para definir a cor padrão
   setPaletteColor(); // para chamar a função das paletas de cores
   setPixelBoard(); //  para chamar a função da board dos pixels
   document.getElementById('color-palette').addEventListener('click', getSelected); // adiciona o evento "clique do mouse" à paleta de cores
   document.getElementById('pixel-board').addEventListener('click', paintPixel); // adiciona o evento "clique do mouse" à board de pixels
-  document.getElementById('clear-board').addEventListener('click', clearBoard); // adiciona o evento "clique do mouse" ao botão Limpar
+  document.getElementById('clear-button').addEventListener('click', clearBoard); // adiciona o evento "clique do mouse" ao botão Limpar
 };
