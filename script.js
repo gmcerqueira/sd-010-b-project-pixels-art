735021af86d77fc2a0f6142ef2dd254886a3693c
aythan azevedo <aythan16@gmail.com>
2021-03-10 18:53:44 -0300

    recuperar quest 7

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ window.onload = function trocaCor() {
   let preto = document.getElementById("preto");
   
 
+function remove(even){
+  let selectebox = document.querySelector(".selected")
+  selectebox.classList.remove("selected")
+    even.target.classList.add("selected")
+
+}
+function add (){
+  let elemento = document.querySelector(".color")
+  for (let index = 0; index < elemento.length; index++) {
+   elemento[index].addEventListener("click", remove(even))
+  }
+}
 
 
+
+//chaaama
+add ()
