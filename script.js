b9e2188bd38c2255df0d8239f78193f1bd72dcee
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-10 20:04:45 -0300

    botao limpar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ 
 // Cria divs color
 const paletaDeCores = document.getElementById('color-palette');
 for (let i = 0; i < 4; i += 1) {
@@ -8,8 +9,8 @@ for (let i = 0; i < 4; i += 1) {
 
 // adicionar cor as divs color
 const cores = document.getElementsByClassName('color');
- // pesquisa realizada no site https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList sobre o classList
- cores[0].classList.add('selected') 
+// pesquisa realizada no site https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList sobre o classList
+cores[0].classList.add('selected');
 cores[0].style.background = 'black';
 cores[1].style.background = 'red';
 cores[2].style.background = 'green';
@@ -26,8 +27,29 @@ for (let i = 0; i < tableCreateCells.length; i += 1) {
   for (let n = 0; n < tableCreateCells.length; n += 1) {
     const lines = document.createElement('td');
     lines.className = 'pixel';
-  tableCreateCells[i].appendChild(lines);
+    tableCreateCells[i].appendChild(lines);
   }
 
- }
+function changeColorClass () {
+  const change = document.querySelector('selected');
+  change.classList.remove('selected');
+  this.classList.add('selected');
+}
 
+
+  const clean =document.getElementById('clear-board');
+  clean.addEventListener('click', clear);
+  function clear() {
+const pixel = document.getElementsByClassName('pixel')
+    for (let i = 0; i < pixel.length; i += 1){
+      pixel[i].style.background = 'white';
+    }
+    }
+    }
+  
+//const changeColorClass = document.querySelector('color-palette');
+//changeColorClass.addEventListener('click', (change) => {
+//        onload.querySelector('selected').classList.remove('selected');
+//        change.target.classList.add('selected')
+        
+ //   })
