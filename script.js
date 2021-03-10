ff47e88e7c9b982a19c13b376ba3108807a463e1
Daniel Ceci <danielceci96@gmail.com>
2021-03-10 10:46:52 -0300

    ainda na 4

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let n=5
 
 for(let indexLine=0;indexLine<n;indexLine+=1){
     let line= document.createElement('tr')
-    line.className='pixel'
-    getTable.appendChild(line)
     for(let indexColumn=0;indexColumn<n;indexColumn+=1){
         let column=document.createElement('td')
         column.className='pixel'
         line.appendChild(column)
+        getTable.appendChild(line)
     }
 }
