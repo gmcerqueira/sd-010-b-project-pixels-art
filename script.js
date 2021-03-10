c08da7c844bc9c8bdff1106160c713c8ae1e5895
root <diegoserafim1@gmail.com>
2021-03-09 22:13:39 -0300

    D7 criando as variaveis .js

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = document.querySelector('#title');
+const colorPalette = document.querySelector('#color-palette');
+const color = document.querySelector('.color');
+const pixelBoard = document.querySelector('#pixel-board');
+const pixel = document.querySelector('.pixel');
+
+
+document.addEventListenergit('#color-palette'){
+
+}
