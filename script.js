ec48cd4784b717477f9737055f7ae9863fe3606a
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-10 06:39:21 -0300

    Adicionando requisito 6

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'orange';
+document.getElementsByClassName('color')[0].className += ' selected';
 
 const pixelBox = document.getElementById('pixel-board');
 
