d67a5cf19874619a235fbb63ef9c13cdc2085f31
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-09 23:50:08 -0300

    function SetPaletteColor added - tests

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function createElement(tagName) {
   const element = document.createElement(tagName);
   return element;
 }
+
+function setPaletteColor() {
+  const colorPalette = '#color-palette';
+  for (let index = 0; index < 4; index += 1) {
+    const elem = createElement('span');
+    elem.classList.add('color');
+    document.querySelector(colorPalette).appendChild(elem);
+  }
+  const cores = ['black', 'red', 'green', 'blue'];
+  const arr = document.getElementById('color-palette').childNodes;
+  arr.forEach((element, index) => {
+    const elem = element; elem.style.backgroundColor = cores[index];
+  });
+  const colors = document.getElementsByClassName('color');
+  colors[0].classList.add('selected');
+}
