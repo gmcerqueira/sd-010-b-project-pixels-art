ffe215d02b5ef3ce94edb11a24301357794a41ce
aline <aline.melryaa@gmail.com>
2021-03-10 16:09:13 -0400

    atualizando formatacao style

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 let paletaCores = document.getElementById("color-palette")
 let pixel = document.getElementById("pixel-board");
 let quadrado = document.getElementById("pixel")
-function tabela(){
-for (index = 0; index <5; index +=1){
-    let quadrado = document.createElement("div");
-    pixel.appendChild(quadrado);
+function cores(){
+    let pc=
+}
 
-    for(index2 = 0; index2 < 5;index2 +=1){
-        let tabel = document.createElement("div");
-        quadrado.appendChild(tabel);
-        tabel.className = " pixel";
-    }
-};
-};
-tabela()
+
+
 
