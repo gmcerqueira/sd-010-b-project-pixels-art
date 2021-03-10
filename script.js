d516c366116f3a7469cdd0d94c3f9701bc902f99
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-09 23:46:59 -0300

    Tenta corrigir os requisitos 2 e 3

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const getFirstColor = document.querySelector('#firstColor');
 const getSecondColor = document.querySelector('#secondColor');
 const getThirdColor = document.querySelector('#thirdColor');
 const getFourthColor = document.querySelector('#fourthColor');
+const getFifthColor = document.querySelector('#fifthColor');
 const firstColor = window.getComputedStyle(getFirstColor).backgroundColor;
 const secondColor = window.getComputedStyle(getSecondColor).backgroundColor;
 const thirdColor = window.getComputedStyle(getThirdColor).backgroundColor;
 const fourthColor = window.getComputedStyle(getFourthColor).backgroundColor;
+const fifthColor = window.getComputedStyle(getFifthColor).backgroundColor;
 const pixelList = document.querySelectorAll('.pixel');
 //
 window.onload = function () {
@@ -23,6 +25,9 @@ getThirdColor.addEventListener('click', function () {
 });
 getFourthColor.addEventListener('click', function () {
   colorChange = fourthColor;
+});
+getFifthColor.addEventListener('click', function () {
+  colorChange = fifthColor;
 });
 pixelList.forEach(function (pixel) {
   pixel.addEventListener('click', function (element) {
