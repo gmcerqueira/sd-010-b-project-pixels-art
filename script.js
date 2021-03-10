83d636f644a8d85e25342f59917a49b484dbf3aa
Jander Liborio <jandernunes14@gmail.com>
2021-03-09 20:27:53 -0400

    problema resolvido do script e feito o 3

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// let color = ["black", "red", "blue", "green"];
-// let colorPalette = document.querySelector("#color-palette");
-// for (let index = 0; index < cores.length; index += 1) {
-//   let colorGround = color[index];
-//   let paletteItem = document.createElement('div');
-//   paletteItem.className = 'color';
-//   paletteItem.style.backgroundColor = colorGround;
-//   paletteItem.appendChild(paletteItem);
-// }
 let color = ['black', 'red', 'blue', 'green'];
 let colorPalette = document.querySelector('#color-palette');
-for(let i=0; i<cores.length; i++){
-  let cor = cores[i];
-  let criarCor = document.createElement("div");
-  criarCor.className = "color";
-  criarCor.style.backgroundColor = cor;
-  colorPalette.appendChild(criarCor);
+for (let index = 0; index < color.length; index += 1) {
+  let colorGround = color[index];
+  let paletteItem = document.createElement('div');
+  paletteItem.className = 'color';
+  paletteItem.style.backgroundColor = colorGround;
+  colorPalette.appendChild(paletteItem);
 }
