2be7ef9059b091eb3a4487042cb069edfdfc540f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-10 13:43:39 -0300

    Começando a criar todos os elememtos atravez da DOM

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let titulo = document.createElement('h1');
+titulo.id = 'title';
+document.body.appendChild(titulo);
+
+let textoTitulo = document.
+
+
+
+
+
+
+
 const paletaCores = document.querySelectorAll('.color'); // Estou capturando a classe color.
 // Selecionando o indice da classe color.
 const preto = paletaCores[0];
@@ -9,3 +21,16 @@ preto.style.backgroundColor = 'black';
 vermelho.style.backgroundColor = 'red';
 verde.style.backgroundColor = 'green';
 azul.style.backgroundColor = 'blue';
+
+let divTabela = document.createElement('div');
+document.body.appendChild(divTabela);
+let quadroPixels = document.createElement('table');
+quadroPixels.id = 'pixel-board';
+document.divTabela.appendChild(quadroPixels);
+
+for (let index = 0; index 
+    
+let < array.length; index++) {
+    const element = array[index];
+    
+}
