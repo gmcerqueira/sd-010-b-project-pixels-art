9a959721dec4d7ca56f6f9e515369350ccdb65ab
Dieghonm <dieghonm@gmail.com>
2021-03-10 17:44:50 -0300

    Quesito 10 meio 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let pixel = document.querySelectorAll('.pixel');
 // **************Escolhe a cor****************
 let black = document.querySelector('#black');
 let blue = document.querySelector('#blue');
@@ -12,15 +13,6 @@ black.addEventListener('click', escolheACor);
 blue.addEventListener('click', escolheACor);
 yellow.addEventListener('click', escolheACor);
 red.addEventListener('click', escolheACor);
-// **************Pinta****************
-let pixel = document.querySelectorAll('.pixel');
-function pinta () {
-  let cor = document.querySelector('.selected').id;//nao era para ser cm o id rs
-  this.style.backgroundColor = cor;
-}
-for (let cont = 0; cont < pixel.length; cont += 1) {
-  pixel[cont].addEventListener('click', pinta);
-}
 // **************Limpa****************
 let button = document.querySelector('#clear-board');
 button.addEventListener('click', function () {
@@ -28,3 +20,58 @@ button.addEventListener('click', function () {
     pixel[cont].style.backgroundColor = 'white';
   }
 });
+// **************Tamanho da Pixel Art****************
+let tamanho = 5
+
+let indicaTamanho = document.querySelector('#valor-atual')
+indicaTamanho.innerHTML = 'Tamanho atual = '+ tamanho +' blocos.'
+
+let tamanhoCoordenadas = document.querySelector('#board-size')
+tamanhoCoordenadas.addEventListener("keyup", guardaTamanho);
+
+function guardaTamanho () {
+tamanho = tamanhoCoordenadas.value
+indicaTamanho.innerHTML = 'Tamanho atual = '+ tamanho +' blocos.'
+//criaQuadro ()
+}
+
+let buttonTamanho = document.querySelector('#generate-board');
+let taman = document.querySelector
+buttonTamanho.addEventListener('click', apaga)
+
+function apaga () {
+  let quadro = document.querySelector('#pixel-board')
+  let apagaDiv = document.querySelector('#pixel-board').childNodes
+  for (let i = 0; i != apagaDiv.length; i){
+    quadro.lastChild.remove()
+  }
+
+  criaQuadro()
+}
+
+criaQuadro ()
+function criaQuadro () {
+let quadro = document.querySelector('#pixel-board')
+for (let index = 0; index < tamanho; index++) {
+  let div = document.createElement('div')
+  div.className = 'linha'
+  for (let cont = 0; cont < tamanho; cont++) {
+      let span = document.createElement('span')
+      let id = (index)+'-'+(cont)
+      span.id = id
+      span.className = 'pixel'
+      div.appendChild(span)
+    }
+  quadro.appendChild(div)
+  }
+  pixel = document.querySelectorAll('.pixel');
+}
+
+// **************Pinta****************
+function pinta () {
+  let cor = document.querySelector('.selected').id;//nao era para ser cm o id rs
+  this.style.backgroundColor = cor;
+}
+for (let cont = 0; cont < pixel.length; cont += 1) {
+  pixel[cont].addEventListener('click', pinta);
+}
