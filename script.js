736b985da4d76095d29a8d2cbcf364c2be4a014b
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-09 22:47:43 -0300

    7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-/* Pegado os inputs: */
-console.log('Pegando inputs:');
-let inpSecond = null;
-inpSecond = document.getElementById('input-second');
-console.log(inpSecond);
-
-let inpThird = null;
-inpThird = document.getElementById('input-third');
-console.log(inpThird);
-
-let inpFourth = null;
-inpFourth = document.getElementById('input-fourth');
-console.log(inpFourth);
-
-/* Pegado as cores dos inputs: */
-console.log('Cores dos inputs:');
-let inpSecondColor = null;
-inpSecondColor = document.getElementById('input-second').value;
-console.log(inpSecondColor);
-
-let inpThirdColor = null;
-inpThirdColor = document.getElementById('input-third').value;
-console.log(inpThirdColor);
-
-let inpFourthColor = null;
-inpFourthColor = document.getElementById('input-fourth').value;
-console.log(inpFourthColor);
-
-console.log('SPANS');
-/* Pegando as SPANS COLORS: */
-
-let spanSecondColor = null;
-spanSecondColor = document.getElementById('second-color').style.backgroundColor;
-spanSecondColor = '#ed4e23';
-console.log(spanSecondColor);
-
-let spanThirdColor = null;
-spanThirdColor = document.getElementById('third-color').style.backgroundColor;
-spanThirdColor = '#3970fe';
-console.log(spanThirdColor);
-
-let spanFouthColor = document.getElementById('fourth-color').style.backgroundColor;
-spanFouthColor = '#438104';
-console.log(spanFouthColor);
-
-/* Pegando as tags SPANS */
-/* Pegando as SPANS COLORS: */
-
 let spanSecond = null;
 spanSecond = document.getElementById('second-color');
 
@@ -67,9 +19,45 @@ function changeFourthColor(element) {
   spanFouth.style.backgroundColor = element.target.value;
 }
 
+
+let inpSecond = null;
+inpSecond = document.getElementById('input-second');
+
+let inpThird = null;
+inpThird = document.getElementById('input-third');
+
+let inpFourth = null;
+inpFourth = document.getElementById('input-fourth');
+
 inpSecond.addEventListener('change', changeSecondColor);
 inpThird.addEventListener('change', changeThirdColor);
 inpFourth.addEventListener('change', changeFourthColor);
+
+
+/* Pegado as cores dos inputs: */
+let inpSecondColor = null;
+inpSecondColor = document.getElementById('input-second').value;
+
+let inpThirdColor = null;
+inpThirdColor = document.getElementById('input-third').value;
+
+let inpFourthColor = null;
+inpFourthColor = document.getElementById('input-fourth').value;
+
+let spanSecondColor = null;
+spanSecondColor = document.getElementById('second-color').style.backgroundColor;
+spanSecondColor = '#ed4e23';
+
+let spanThirdColor = null;
+spanThirdColor = document.getElementById('third-color').style.backgroundColor;
+spanThirdColor = '#3970fe';
+
+let spanFouthColor = document.getElementById('fourth-color').style.backgroundColor;
+spanFouthColor = '#438104';
+
+/* Pegando as tags SPANS */
+/* Pegando as SPANS COLORS: */
+
 
 /* Criando quadro de pixels dinamicamente: */
 let pixelBoard = null;
@@ -86,3 +74,27 @@ for (let index = 0; index < 25; index += 1) {
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
 }
+
+/* Fazendo com que um elemento da palheta de cores marque selected caso um deles for clicado */
+
+function getPaletteItems() {
+  return document.getElementsByClassName('color');
+}
+let paletterItems = [];
+paletterItems = getPaletteItems();
+
+function markAllWithNotSelected() {
+  for (let index = 0; index < paletterItems.length; index += 1) {
+    paletterItems[index].className = 'color';
+  }
+}
+
+function markSelected(element) {
+  markAllWithNotSelected();
+  element.target.setAttribute('class', 'color selected');
+}
+
+for (let index = 0; index < paletterItems.length; index += 1) {
+  paletterItems[index].addEventListener('click', markSelected);
+}
+console.log(getPaletteItems());
