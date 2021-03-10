945598a628ae87641444338a2308cf41bbde9a6b
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-10 00:54:17 -0300

    Adiciona uma classe para as lnhas do boarder

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.onload = function() {
     let pixelPalette = 4;
-    let pixelBoardLength = window.prompt("Enter the number of elements you want for the Pixel-board");
+    // let pixelBoardLength = window.prompt("Enter the number of elements you want for the Pixel-board");
+    let pixelBoardLength = 5;
     let pixelBoardBase = 5;
     let colorPalette = document.querySelector("#color-palette");
+
     let firstPaletteColor = document.querySelector(".color");
         firstPaletteColor.style.backgroundColor = "black";
-    let lines = document.querySelector("#pixel-board");
     let secondPaletteColor = document.querySelectorAll(".color")[1];
         secondPaletteColor.style.backgroundColor = "red";
     let thirdPaletteColor = document.querySelectorAll(".color")[2];
@@ -13,8 +14,11 @@ window.onload = function() {
     let fourthPaletteColor = document.querySelectorAll(".color")[3];
         fourthPaletteColor.style.backgroundColor = "yellow"; 
 
-    createBoardLines(pixelBoardLength);
-    // repeatLines(pixelBoardLength);
+
+    let lines = document.querySelectorAll(".lines");
+    fillBoard(lines);
+
+    // repeatLines(lines);
 
     // function createPalette(pixelPalette) {        
     //     let pixelColorPalette = document.createElement("div");
@@ -22,31 +26,31 @@ window.onload = function() {
     //         colorPalette.appendChild(pixelColorPalette)[index];
     //         colorPalette[index].className = "color";
     //     }
-    // }  
+    // } 
+
+    function fillBoard(lines)  {
+        for (let i = 0; i < pixelBoardLength; i += 1) {
+            createBoardLines(lines[i]);            
+        }
+    }
 
     function createBoardPixel(className) {
         let pixel = document.createElement("div");
         pixel.className = className;
         return pixel;
-        }
+        }   
 
-    function createBoardLines(pixelBoardLength) {
+    function createBoardLines(divLine) {
         for (let n = 0; n < pixelBoardLength; n += 1) {
             if (pixelBoardLength >= 0 && pixelBoardLength <= 50) {
                 let pixel = createBoardPixel("pixel");
-                lines.appendChild(pixel);
+                divLine.appendChild(pixel);
             } else {
                 alert ("Board inválido");
                 break;
             }
         }
-    }
-
-    // function repeatLines(pixelBoardLength)  {
-    //     for (let i = 0; i < pixelBoardLength; i += 1) {
-    //         createBoardLines([i]);
-    //     }
-    // }
+    }   
 }
    
 
