2341e1d838d2e20048c70a040dbe8cc34f4ffe73
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-10 16:03:15 -0300

    Corrige alguns erros nos requsitos 4 e 5

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let quadros = document.getElementById('pixel-board');
 let n = 5;
 
-for(let indexLinhas = 0; indexLinhas < n; indexLinhas += 1) {
-    let linha = document.createElement('tr');
-    for (let indexColunas = 0; indexColunas < n;indexColunas += 1) {
-        let coluna = document.createElement('td')
-        coluna.className = 'pixel';
-        linha.appendChild(coluna);
-        quadros.appendChild(linha);
-    }
+for (let indexLinhas = 0; indexLinhas < n; indexLinhas += 1) {
+  let linha = document.createElement('tr');
+  for (let indexColunas = 0; indexColunas < n;indexColunas += 1) {
+    let coluna = document.createElement('td');
+    coluna.className = 'pixel';
+    linha.appendChild(coluna);
+    quadros.appendChild(linha);
+  }
 }
