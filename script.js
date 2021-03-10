0e7841a9bee38c2881d92836f643cff6d529ea7e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-03-10 02:17:06 -0300

    Em index.html: adiciona link para arquivo script.js; adiciona section com id "pixel-board". Em style.css: para id "pixel-board", retirada de border e padding, centralização e auto-ajuste ao conteúdo; para classe "pixel-row", retirada de margin, border, padding, display flex, centralização e auto-ajuste ao conteúdo; para classe "pixel", retirada de padding, ajuste de width e height para 40px, alteração de background-color para white, display inline-block e border de 1px solid black. Em script.js, a function createPixel cria e retorna uma div com classe "pixel"; a function generateBoard recebe width e height e constrói um board de pixels; a function window.onload executa uma function anônima para definir valores iniciais para boardSize, width, height e gerar o board de pixels.

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+   Essa função cria um element div, altera sua classe para 'pixel' e o retorna.
+  */
+ function createPixel() {
+  const pixel = document.createElement('div'); // cria um element div e armazena em pixel
+  pixel.className = 'pixel'; // altera a classe do element para 'pixel'
+  return pixel; // retorna o element criado
+}
+
+/*
+   Essa função recebe width e height e cria um board contendo width colunas e height linhas preenchidos com elementos da classe 'pixel'.
+  */
+function generateBoard(width, height) {
+  const board = document.getElementById('pixel-board'); // board armazena o elemento html do id pixel-board
+  for (let line = 0; line < height; line += 1) { // para cada linha até height
+    const pixelLine = document.createElement('div'); // cria um element div e armazena em pixelLine
+    pixelLine.className = 'pixel-row'; // altera a classe do element para 'pixel-row'
+
+    for (let column = 0; column < width; column += 1) { // para cada coluna até width
+      pixelLine.appendChild(createPixel()); // o pixel criado é adicionado como filho em pixelLine
+    }
+
+    board.appendChild(pixelLine); // a linha de pixels é adicionada como filha em board
+  }
+}
+
+window.onload = function() { // define uma função anômina para carregar valores e gerar board
+  let boardSize = 5; // define o tamanho do board
+  let width = boardSize; // define width a partir de boardSize
+  let height = boardSize; // define height a partir de boardSize
+
+  generateBoard(width, height); // gera o board de dimensão width X height
+};
