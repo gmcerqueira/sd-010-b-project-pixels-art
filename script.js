fdd8efe71d4a0096cfbd0e0bf68d85c57d6c814e
isTorres <ismaeltorres612@gmail.com>
2021-03-10 01:52:02 -0300

    refatorando tudo - 1, 2

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-// let colors = document.querySelectorAll('.color');
+// Functions:
 
-// let pixels = document.querySelectorAll('.pixel');
-// const line = document.querySelector('.line');
-const pixelBoard = document.getElementById('pixel-board');
+function createElements(tag) {
+  const element = document.createElement(tag);
+  return element;
+}
 
-const boardSize = 5;
+function createColorPalette() {
+  for (let i = 0; i < 4; i += 1) {
+    const element = createElements('div');
+    element.className = 'color';
+    document.querySelector('#color-palette').appendChild(element);
+  }
+}
+createColorPalette();
 
-window.onload = function () {
+// function createPixels() {
+//   const array = pixelBoard.childNodes;
+//   for (let x = 0; x < array.length; x += 1) {
+//     for (let i = 0; i < array.length; i += 1) {
+//       const createPixel = document.createElement('div');
+//       createPixel.className = 'pixel td';
+//       array[x].appendChild(createPixel);
+//     }
+//   }
+// }
+// createLinePixel();
+// createPixels();
+
+// // Events:
+// function clickColorPicker() {
+//   let getSelected = colorPalette[i].style.backgroundColor;
+//   const colorPalette = document.querySelector('#color-palette').children;
+//   const pixelBoard = document.getElementById('pixel-board')
+
+//   for (let i = 0; i < colorPalette.length; i += 1) {
+//     colorPalette[i].addEventListener('click', (event) => {
+      
+//     });
+//   }
+// }
+
+window.onload = () => {
+  const defaultColorPixels = document.querySelectorAll('.pixel');
+  const pixelColorStorage = localStorage.setItem('backgroundColor', 'white');
+  defaultColorPixels.style.backgroundColor = pixelColorStorage;
+
   const color1 = document.getElementById('color1');
   const colorStorage = localStorage.setItem('backgroundColor', 'black');
   color1.style.backgroundColor = colorStorage;
+
+  const colorSelected = localStorage.setItem('class', 'selected');
+  color1.className = colorSelected;
 };
-// Functions:
-
-function createLinePixel() {
-  for (let i = 0; i < boardSize; i += 1) {
-    const createLine = document.createElement('div');
-    createLine.className = 'line';
-    pixelBoard.appendChild(createLine);
-  }
-}
-
-function createPixels() {
-  const array = pixelBoard.childNodes;
-  for (let x = 0; x < array.length; x += 1) {
-    for (let i = 0; i < array.length; i += 1) {
-      const createPixel = document.createElement('div');
-      createPixel.className = 'pixel td';
-      array[x].appendChild(createPixel);
-    }
-  }
-}
-createLinePixel();
-createPixels();
-
-
-
-// Events:
-let colorPalette = document.querySelector('#color-palette').children;
-for (let i = 0; i < colorPalette.length; i += 1) {
-  colorPalette[i].addEventListener('click', function() {
-    
-  });
-}
