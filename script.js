cb2f1dca5cfdab026578eab89abaf707c314a1ab
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-10 00:26:54 -0400

    Reposicioamento da class Selected

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
 
-// setBoxColor
-
-
 // function setBackgroundColor(color) {
 //   let content = document.querySelector(".content")
 //   content.style.backgroundColor = color
@@ -25,41 +22,41 @@
 //       let backgroundColor = localStorage.getItem("backgroundColor")
 //         if (backgroundColor) setBackgroundColor(backgroundColor)
 
-let blackBox = document.getElementById('blackBox');
-let blueBox = document.getElementById('blueBox');
-let redBox = document.getElementById('redBox');
-let greenBox = document.getElementById('greenBox');
+// let blackBox = document.getElementById('blackBox');
+// let blueBox = document.getElementById('blueBox');
+// let redBox = document.getElementById('redBox');
+// let greenBox = document.getElementById('greenBox');
 
-function selectedBlackBox() {
-  blackBox.className = 'color selected';
-  blueBox.className = 'color';
-  redBox.className = 'color';
-  greenBox.className = 'color';
-}
+// function selectedBlackBox() {
+//   blackBox.className = 'color selected';
+//   blueBox.className = 'color';
+//   redBox.className = 'color';
+//   greenBox.className = 'color';
+// }
 
-function selectedBlueBox() {
-  blackBox.className = 'color';
-  blueBox.className = 'color selected';
-  redBox.className = 'color';
-  greenBox.className = 'color';
-}
-function selectedRedBox() {
-  blackBox.className = 'color';
-  blueBox.className = 'color';
-  redBox.className = 'color selected';
-  greenBox.className = 'color';
-}
-function selectedGreenBox() {
-  blackBox.className = 'color';
-  blueBox.className = 'color';
-  redBox.className = 'color';
-  greenBox.className = 'color selected';
-}
+// function selectedBlueBox() {
+//   blackBox.className = 'color';
+//   blueBox.className = 'color selected';
+//   redBox.className = 'color';
+//   greenBox.className = 'color';
+// }
+// function selectedRedBox() {
+//   blackBox.className = 'color';
+//   blueBox.className = 'color';
+//   redBox.className = 'color selected';
+//   greenBox.className = 'color';
+// }
+// function selectedGreenBox() {
+//   blackBox.className = 'color';
+//   blueBox.className = 'color';
+//   redBox.className = 'color';
+//   greenBox.className = 'color selected';
+// }
 
-blackBox.addEventListener('click', selectedBlackBox);
-blueBox.addEventListener('click', selectedBlueBox);
-redBox.addEventListener('click', selectedRedBox);
-greenBox.addEventListener('click', selectedGreenBox);
+// blackBox.addEventListener('click', selectedBlackBox);
+// blueBox.addEventListener('click', selectedBlueBox);
+// redBox.addEventListener('click', selectedRedBox);
+// greenBox.addEventListener('click', selectedGreenBox);
 
 
   // let selected = document.querySelector('.color');
@@ -72,3 +69,27 @@ greenBox.addEventListener('click', selectedGreenBox);
   //   boxSeleted.classList.remove(' selected');
   //   event.target.classList.add(' selected');
   // }
+
+let classSelected = document.querySelectorAll(".color");
+
+for (i = 0; i < classSelected.length; i+=1) {
+  classSelected[i].addEventListener("click", changeSelected)
+} 
+
+function changeSelected(event) {
+  let selectBox = document.querySelector(".selected")
+  selectBox.classList.remove("selected")
+  event.target.classList.add("selected")
+}
+
+let pixelBox = document.querySelectorAll(".pixel");
+for (i = 0; i < pixelBox.length; i += 1) {
+  pixelBox[i].addEventListener("click", changeColor);
+}
+
+
+function changeColor(event) {
+  let selectPixel = document.querySelector(".selected")
+  let color = window.getComputedStyle(selectPixel).backgroundColor
+  event.target.style.backgroundColor = color    
+}
