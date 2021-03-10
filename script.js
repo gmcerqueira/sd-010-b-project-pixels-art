074d7a6a7d180d16624646f66b4768d607ec8e32
DanielTrybe <darkdanielr@gmail.com>
2021-03-10 20:41:13 -0300

    linter

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 let palet = document.getElementsByClassName('color');
-for (let index = 0; index < palet.length; index +=1) {
-  palet[index].addEventListener('click', function(event) {
+for (let index = 0; index < palet.length; index += 1) {
+  palet[index].addEventListener('click', function (event) {
     document.getElementsByClassName('selected')[0].classList.remove('selected');
     event.target.classList.add('selected');
   })
 }
 let quadro = document.getElementsByClassName('pixel');
-for (let quadroIndex = 0; quadroIndex < quadro.length; quadroIndex += 1){
-  quadro[quadroIndex].addEventListener('click', function(event){
+for (let quadroIndex = 0; quadroIndex < quadro.length; quadroIndex += 1) {
+  quadro[quadroIndex].addEventListener('click', function (event) {
     if (document.getElementsByClassName('color')[0].classList.contains('selected')) {
       event.target.style.background = 'black';
-    } if (document.getElementsByClassName('color')[1].classList.contains('selected')) {
+    }
+    if (document.getElementsByClassName('color')[1].classList.contains('selected')) {
       event.target.style.background = 'red';
-    } if (document.getElementsByClassName('color')[2].classList.contains('selected')) {
+    }
+    if (document.getElementsByClassName('color')[2].classList.contains('selected')) {
       event.target.style.background = 'blue';
-    } if (document.getElementsByClassName('color')[3].classList.contains('selected')) {
+    }
+    if (document.getElementsByClassName('color')[3].classList.contains('selected')) {
       event.target.style.background = 'green';
     }
   })
 }
 
 document.getElementById('clear-board').addEventListener('click', limpar);
-function limpar () {
-let branco = document.getElementsByClassName('pixel');
-for (let key = 0; key < branco.length; key +=1 ){
-  branco[key].style.background = 'white';
-}
+
+function limpar() {
+  let branco = document.getElementsByClassName('pixel');
+  for (let key = 0; key < branco.length; key += 1) {
+    branco[key].style.background = 'white';
+  }
 }
 
 //Agradeço aos plantões, Raphael Gumieri e Pedro Henrique.
