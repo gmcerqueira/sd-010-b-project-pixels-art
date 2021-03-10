d069861d0623fdb59accf0be2400e241f26d2376
Cristian <cristian.bugs@gmail.com>
2021-03-09 22:42:57 -0300

    fourth task

---
+++ script.js
@@ -0,0 +1,17 @@
+let pixel = document.getElementById("pixel-board");
+
+
+function matrix () {
+for(index = 0; index < 5; index += 1) {
+    let square = document.createElement("div");
+    pixel.appendChild(square);
+    
+    for ( index2 = 0; index2 < 5; index2 += 1) {
+        let variable = document.createElement("div");
+        square.appendChild(variable);
+        variable.className = "pixel"
+
+        };
+};
+};
+matrix ()
