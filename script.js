cac3d468969b2609054cd5aae2891ec3cb7a56f8
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-10 12:10:03 -0300

    Corrigindo erro lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const containerQuadro = document.querySelector('#pixel-board');
 
 function createPalette() {
   for (let index = 0; index < 4; index += 1) {
-    let selectors = document.createElement('div');
+    const selectors = document.createElement('div');
     selectors.className = 'color';
     containerPalette.appendChild(selectors);
     if (index === 0) {
@@ -26,13 +26,11 @@ function createPixels() {
       column.className = 'pixel';
       line.appendChild(column);
     }
-  };
+  }
 }
 createPixels();
-
 
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'green';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'yellow';
-
