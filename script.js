804f928b9a5867e39bfb0b3aeed560f3638ab92b
Igor Machado <rf59imft@gmail.com>
2021-03-10 17:18:00 -0300

    desafio 7

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function setColorClass() {
   const colorSelected = document.querySelectorAll('.color');
+  const changeClassBlack = document.querySelector('.color');
+  changeClassBlack.className = 'color black';
   for (let item = 0; item < colorSelected.length; item += 1) {
     colorSelected[item];
     colorSelected[item].addEventListener('click', function (event) {
       console.log(event.target);
       console.log(colorSelected[item].style);
-
-      if (colorSelected[item].classList.contains('selected')) {
-        event.target.classList.remove('selected');
-      } else {
-        event.target.classList.add('selected');
+      // procurar tirar o elemeto que esta selecionado previamente
+      // esta selecionado pela classe
+      if (colorSelected[item].classList.toggle('selected')) {
+        event.target.classList.add('selected') &&
+          event.target.classList.toggle('selected');
+      } else if (event.target.classList.contains('selected')) {
+        event.target.classList.remove('selected') &&
+          event.target.classList.toggle('selected');
       }
     });
   }
