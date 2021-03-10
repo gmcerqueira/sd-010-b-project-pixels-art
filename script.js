dba560f304cde66681cb53715ec7a99f26b240f7
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-10 19:32:14 -0300

    limpando o codigo do desafio 8, criado variaveis & diminuindo linhas de comentarios

--- script.js
+++ script.js
@@ -22,17 +22,6 @@ for(let index = 0; index < getColor.length; index+=1) {
 }
 
 
-function teste (event) {
-
-} 
-  //let teste2 = document.getElementsByClassName('selected')[0].style.black;
-  
-//teste 1 
-// let getPixel = document.getElementsByClassName('pixel');
-//  for(let index2 = 0; index2 < getPixel.length; index2+=1) {
-//   getPixel[index2].addEventListener('click', function);
-//  }
-
 let getPixel = document.getElementsByClassName('pixel');
 let selectedColor = document.getElementsByClassName('color');
 
@@ -48,23 +37,9 @@ for(let pixelIndex = 0; pixelIndex < getPixel.length; pixelIndex += 1){
 
     } else if (selectedColor[2].classList.contains('selected')) {
       event.target.style.background = 'blue';
-      
+
     } else if (selectedColor[3].classList.contains('selected')) {
       event.target.style.background = 'green';
     }
 })
 }
-// let quadro = document.getElementsByClassName('pixel');
-// for (let quadroIndex = 0; quadroIndex < quadro.length; quadroIndex += 1){
-//   quadro[quadroIndex].addEventListener('click', function(event){
-//     if (document.getElementsByClassName('color')[0].classList.contains('selected')) {
-//       event.target.style.background = 'black';
-//     } if (document.getElementsByClassName('color')[1].classList.contains('selected')) {
-//       event.target.style.background = 'red';
-//     } if (document.getElementsByClassName('color')[2].classList.contains('selected')) {
-//       event.target.style.background = 'blue';
-//     } if (document.getElementsByClassName('color')[3].classList.contains('selected')) {
-//       event.target.style.background = 'green';
-//     }
-//   })
-// }
