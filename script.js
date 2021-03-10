0445bd28aec8bcce70de402e89e8f55949c35797
root <diegoserafim1@gmail.com>
2021-03-10 13:09:52 -0300

    D7/8 finalizando

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const colorPalette = document.querySelector('#color-palette');
 // const color = document.querySelector('.color');
 const pixelBoard = document.querySelector('#pixel-board');
 // const pixel = document.querySelector('.pixel');
-
+// D7
 colorPalette.addEventListener('click', event => { // https://www.w3schools.com/js/js_htmldom_eventlistener.asp
   const colorPaletteSelected = document.getElementById(event.target.id);
   const verify = colorPaletteSelected.className;
-  verify === 'color-palette' ? (event.stopPropagation()): 
-  (document.querySelector('.selected').className = 'color',
-  colorPaletteSelected.className = 'color selected')
+  verify === 'color-palette' ? (event.stopPropagation()
+  ) : (document.querySelector('.selected').className = 'color',
+    colorPaletteSelected.className = 'color selected');
 });
-
+// D8
 pixelBoard.addEventListener('click', event => {
   const selectedPixel = event.target;
   const selectedColor = document.querySelector('.selected');
-  const selectedColorRgb = window.getComputedStyle(selectedColor, 
+  const selectedColorRgb = window.getComputedStyle(selectedColor,
     null).getPropertyValue('background-color');
   selectedPixel.style.backgroundColor = selectedColorRgb;
 });
