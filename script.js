62969b8647e7ec03bfbdbfcb97f63dd77dbc71b7
Italo Matos <italomatos764@gmail.com>
2021-03-10 10:51:16 -0400

    Ajustando funcao de criar quadro

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-let cores = ['black', 'red', 'blue', 'green'];
-let colorPalette = document.querySelector('#color-palette');
+function criarPaleta() {
+  let cores = ['black', 'red', 'blue', 'green'];
+  let colorPalette = document.querySelector('#color-palette');
 
-for(let i=0; i<cores.length; i++){
-  let cor = cores[i];
-  let criarCor = document.createElement("div");
-  criarCor.className = "color";
-  criarCor.style.backgroundColor = cor;
-  colorPalette.appendChild(criarCor);
+  for(let i = 0; i<cores.length; i += 1) {
+    let cor = cores[i];
+    let criarCor = document.createElement("div");
+    criarCor.className = "color";
+    criarCor.style.backgroundColor = cor;
+    colorPalette.appendChild(criarCor);
+  }
 }
+criarPaleta();
 
-let pixelize = document.querySelectorAll('.line');
-for(let j = 0; j < 5; j += 1) {
-  pixelize[j];
-    for(let k = 0; k < 5; k += 1) {
-      let criaPixel = document.createElement("div");
-      criaPixel.className = "pixel";
-      criaPixel.backgroundColor = "rgb(255, 255, 255)";
-      pixelize[j].appendChild(criaPixel);
-    }
+function pixelizar() {
+  let pixelize = document.querySelector('#pixel-board');
+  for(let j = 0; j < 25; j += 1) {
+    let criaPixel = document.createElement("div");
+    criaPixel.className = "pixel";
+    pixelize.appendChild(criaPixel);
+  }
 }
+pixelizar();
 
-let corPadrao = document.querySelectorAll('.color')[0];
-let criaPadrao = corPadrao.classList;
-criaPadrao.add('selected');
+function criaPadrao() {
+  let corPadrao = document.querySelectorAll('.color')[0];
+  let criaPadrao = corPadrao.classList;
+  criaPadrao.add('selected');
+}
+criaPadrao();
