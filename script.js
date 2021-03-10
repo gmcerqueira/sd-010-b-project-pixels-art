abb3bcbf48a5847b1398a5615206abc2a461e3bb
aline <aline.melryaa@gmail.com>
2021-03-10 18:56:37 -0400

    ajuste do pixel css

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let paletaCores = document.getElementById("color-palette")
-ListeningStateChangedEvent
-function trocadiv(){
-
+function  criarDivs(){
+    let pixel = document.getElementById("pixel-board");
+    for (index = 0; index < 25; index +=1){
+        let cd= document.createElement("div");
+        cd.className = 'pixel';
+        pixel.appendChild(cd);
+    }
 
 }
-
+window.onload = criarDivs();
 
 
 
