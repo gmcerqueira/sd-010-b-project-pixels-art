ec3b60e25c544465507f4ebae7b14b09d2e57ca2
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-10 16:42:39 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function addpixel(){
 
-    let quadroPixels = document.getElementById('pixel-board');
+    let pixelTable = document.getElementById('pixel-board');
     //caminho para criar o append child
     
 
     for(let l = 0; l < 5; l += 1){
         let td = document.createElement('td');
-        quadroPixels.appendChild(td);
+            pixelTable.appendChild(td);
         // lines tem o td e table lines tem o caminho, sendo assim
         // o caminho recebe as linhas    
         td.className = 'pixel';                        
@@ -16,21 +16,58 @@ function addpixel(){
 }
 
 function stepLine(){
-    let quadroPixels = document.getElementById('pixel-board');
+    let pixelTable = document.getElementById('pixel-board');
     // caminho para criar o table-row
 
     let td = document.createElement('tr');
-        quadroPixels.appendChild(td);
+     pixelTable.appendChild(td);
         // lines tem o td e table lines tem o caminho, sendo assim
         // o caminho recebe as linhas   
          
 
 }
-
+//criando rodando os table-row com as os pixels
 for(let matrix = 0; matrix < 5; matrix += 1){
     addpixel();
     stepLine();
 }
+window.onload = function(){
+    // Botões da paleta
+    let buttonBlack =document.querySelector('#preto');
+    let buttonRed =document.querySelector('#vermelho');
+    let buttonBlue =document.querySelector('#azul');
+    let buttonGreen =document.querySelector('#verde');
+
+    //1. Aguardar o click na paleta com o preto selecionado
+    buttonBlack.addEventListener('click',blackPaint());
+    buttonRed.addEventListener('click',redPaint);
+    buttonBlue.addEventListener('click',bluePaint);
+    buttonGreen.addEventListener('click',greenPaint);
+    
+}
+
+function blackPaint(){
+    //estou escutando
+    console.log('Black');
+    let captureBlack = 'black';
+}
+function redPaint(){
+    //estou escutando
+    console.log('red');
+    let captureRed = 'red';
+}
+function bluePaint(){
+    //estou escutando
+    console.log('blue');
+    let captureBlue = 'blue';
+}
+function greenPaint(){
+    //estou escutando
+    console.log('green')
+    let captureGreen = green
+}
+
+
 
   
   
