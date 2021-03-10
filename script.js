da07e255b465b8484400361a0c201de39ee44d1d
Thiago Leite <leite.tjs@gmail.com>
2021-03-09 21:57:27 -0300

    challenge 5

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-window.onload
-
-let colorPallete = document.getElementById('color-palette');
-let divPallete = document.createElement('div')
+const colorPallete = document.getElementById('color-palette');
+let divPallete = document.createElement('div');
 
 for (let divs = 0; divs < 4; divs += 1) {
-  divPallete = document.createElement('div')
+  divPallete = document.createElement('div');
   divPallete.className = 'color';
-  colorPallete.appendChild(divPallete)
+  colorPallete.appendChild(divPallete);
 }
 
-let boardSquares = [];
+const boardSquares = [];
 for (let i = 0; i < 5; i += 1) {
   boardSquares[i] = i;
 }
 
-let bigSquare = document.getElementById('pixel-board');
+const bigSquare = document.getElementById('pixel-board');
 let createLines = document.createElement('tr');
 let createCol = document.createElement('td');
 
@@ -23,14 +21,10 @@ for (let j = 0; j < boardSquares.length; j += 1) {
   createLines.className = 'table-lines';
   bigSquare.appendChild(createLines);
   for (let k = 0; k < boardSquares.length; k += 1) {
-  let lineSquare = document.querySelectorAll('.table-lines');
-  lineSquare = document.querySelectorAll('.table-lines');
-  createCol = document.createElement('td');
-  createCol.className = 'pixel';
-  lineSquare[j].appendChild(createCol);
+    let lineSquare = document.querySelectorAll('.table-lines');
+    lineSquare = document.querySelectorAll('.table-lines');
+    createCol = document.createElement('td');
+    createCol.className = 'pixel';
+    lineSquare[j].appendChild(createCol);
   }
 }
-
-
-
-
