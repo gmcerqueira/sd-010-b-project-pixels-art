a04b1640de7a51c475896decb2795ab4f4753bad
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-10 17:22:07 -0300

    Tenta corrigir o requisito 7

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const thirdColor = window.getComputedStyle(getThirdColor).backgroundColor;
 const fourthColor = window.getComputedStyle(getFourthColor).backgroundColor;
 const fifthColor = window.getComputedStyle(getFifthColor).backgroundColor;
 const pixelList = document.querySelectorAll('.pixel');
+const selected = document.getElementsByClassName('selected');
 //
 window.onload = function () {
   getFirstColor.className = 'selected';
@@ -16,18 +17,39 @@ window.onload = function () {
 let colorChange = firstColor;
 getFirstColor.addEventListener('click', function () {
   colorChange = firstColor;
+
+  if (getFirstColor.className === 'selected' || getSecondColor.className === 'selected' || getThirdColor.className === 'selected' || getFourthColor.className === 'selected' || getFifthColor.className === 'selected') {
+    selected[0].className = "color";
+  }
+  getFirstColor.className = 'selected';
 });
 getSecondColor.addEventListener('click', function () {
   colorChange = secondColor;
+  if (getFirstColor.className === 'selected' || getSecondColor.className === 'selected' || getThirdColor.className === 'selected' || getFourthColor.className === 'selected' || getFifthColor.className === 'selected') {
+    selected[0].className = "color";
+  }
+  getSecondColor.className = 'selected';
 });
 getThirdColor.addEventListener('click', function () {
   colorChange = thirdColor;
+  if (getFirstColor.className === 'selected' || getSecondColor.className === 'selected' || getThirdColor.className === 'selected' || getFourthColor.className === 'selected' || getFifthColor.className === 'selected') {
+    selected[0].className = "color";
+  }
+  getThirdColor.className = "selected";
 });
 getFourthColor.addEventListener('click', function () {
   colorChange = fourthColor;
+  if (getFirstColor.className === 'selected' || getSecondColor.className === 'selected' || getThirdColor.className === 'selected' || getFourthColor.className === 'selected' || getFifthColor.className === 'selected') {
+    selected[0].className = "color";
+  }
+  getFourthColor.className = "selected";
 });
 getFifthColor.addEventListener('click', function () {
   colorChange = fifthColor;
+  if (getFirstColor.className === 'selected' || getSecondColor.className === 'selected' || getThirdColor.className === 'selected' || getFourthColor.className === 'selected' || getFifthColor.className === 'selected') {
+    selected[0].className = "color";
+  }
+  getFifthColor.className = 'selected';
 });
 pixelList.forEach(function (pixel) {
   pixel.addEventListener('click', function (element) {
