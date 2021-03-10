05a12a16f43f9833cbeb4998bdc6d2b38e93903f
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-09 23:51:55 -0300

    Exercicio 4 correção

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function createPalette() {
     let selectors = document.createElement('div');
     selectors.className = 'color';
     containerPalette.appendChild(selectors);
+    if (index === 0) {
+      selectors.className += ' selected';
+    }
   }
   return containerPalette;
 }
@@ -13,14 +16,17 @@ function createPalette() {
 createPalette();
 
 function createPixels() {
-  for (let index = 0; index < 25; index += 1) {
-    let createDiv = document.createElement('div');
-    createDiv.className = 'pixel';
-    containerQuadro.appendChild(createDiv);
-  }
-  return containerQuadro;
-}
+  for (let index = 0; index < 5; index += 1) {
+    const line = document.createElement('div');
+    containerQuadro.appendChild(line);
 
+    for (let index2 = 0; index2 < 5; index2 += 1) {
+      const colun = document.createElement('div');
+      colun.className = 'pixel';
+      line.appendChild(colun);
+    }
+  };
+}
 createPixels();
 
 document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
