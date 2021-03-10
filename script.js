62fc69000571df1dd0e4bf4a21c0b6d1683ea2f9
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-09 21:05:27 -0300

    verificando se js atrapalho o corretor

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let tr = document.createElement('tr'); // lines
-let td = document.createElement('td'); // collumns
-
-//recuperando a tabela para criar linhas e colunas
-
-td.className('pixel');
-
-
 
