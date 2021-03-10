35b4fd220352a82d1cd9a5913e32689ac57f08aa
Dieghonm <dieghonm@gmail.com>
2021-03-10 03:01:55 -0300

    Quesito 8 teste 1

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ let blue = document.querySelector('#blue');
 let yellow = document.querySelector('#yellow');
 let red = document.querySelector('#red');
 function escolheACor () {
-  let escolhida = document.querySelector('.selected'); 
+  let escolhida = document.querySelector('.selected');
   escolhida.classList.remove('selected');
   this.classList.add('selected');
-};
-black.addEventListener('click',escolheACor);
-blue.addEventListener('click',escolheACor);
-yellow.addEventListener('click',escolheACor);
-red.addEventListener('click',escolheACor);
+}
+black.addEventListener('click', escolheACor);
+blue.addEventListener('click', escolheACor);
+yellow.addEventListener('click', escolheACor);
+red.addEventListener('click', escolheACor);
+// **************Pinta****************
+let pixel = document.querySelectorAll('.pixel');
+  for (let cont = 0; cont < pixel.length; cont += 1) {
+  pixel[cont].addEventListener('click', pinta);
+}
+
+function pinta () {
+ let cor = document.querySelector('.selected').id
+this.style.backgroundColor = cor
+} 
+
