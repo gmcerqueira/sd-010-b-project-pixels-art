165a1aa45367998f86640012aa6a9a5781e35bde
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-09 21:13:31 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ document.addEventListener('click', (event) => {
     selectedColor = event.target.id;
     event.target.classList.add('selected');
   } else if (event.target.classList.contains('pixel')) {
-      event.target.style.backgroundColor = selectedColor;
+    event.target.style.backgroundColor = selectedColor;
   }
 });
+
+document.getElementById('clear-board').onclick = function () {
+  let board = document.getElementsByClassName('pixel');
+  for (let i = 0; i < board.length; i += 1) {
+    board[i].style.backgroundColor = 'white';
+  }
+}
