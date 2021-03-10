aebd202811c84956c603942f1ac13320f3b27e12
Cristian <cristian.bugs@gmail.com>
2021-03-10 17:43:29 -0300

    new try on task seven

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ matrix();
 // questão resolvida com a ajuda do colega durante grupo de estudos;
 
 function SelectColor() {
-  const firstColorSelected = document.querySelector('.color selected');
+  const firstColorSelected = document.getElementsByClassName('selected');
 
   const Colors = document.querySelectorAll('.color');
 
@@ -25,7 +25,7 @@ function SelectColor() {
       firstColorSelected.className = 'color';
     });
   }
-  const SelectedColor = document.querySelector('.color selected');
+  const SelectedColor = document.getElementsByClassName('selected');
   return SelectedColor;
 }
 SelectColor();
