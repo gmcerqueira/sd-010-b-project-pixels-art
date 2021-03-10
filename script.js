08a46ea9165bc7faa29127faa605157bae85df69
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-10 14:49:11 -0300

    fazendo a 8 e final

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ window.onload = selecionaPreto;
 function selecionaPreto(){
     let encontraPreto = document.getElementsByClassName("color")[0];
     encontraPreto.classList.add("selected");
+    pintarOQuadro("black");
 }
 function pintarQuadrado(cor){
     let encontraSelected = document.getElementsByClassName("selected")[0];
@@ -22,17 +23,30 @@ function pintarQuadrado(cor){
     if(cor === "blue") {
         let encontraDiv2 = document.getElementsByClassName("div2")[0];
         encontraDiv2.classList.add("selected");
+        pintarOQuadro(cor);
     }
     else if(cor === "black") {
         let encontraDiv1 = document.getElementsByClassName("div1")[0];
         encontraDiv1.classList.add("selected");
+        pintarOQuadro(cor);
     }
     else if(cor === "brown") {
         let encontraDiv3 = document.getElementsByClassName("div3")[0];
         encontraDiv3.classList.add("selected");
+        pintarOQuadro(cor);
     }
     else if(cor === "cadetblue") {
         let encontraDiv4 = document.getElementsByClassName("div4")[0];
         encontraDiv4.classList.add("selected");
+        pintarOQuadro(cor);
     }
+}
+function pintarOQuadro(cor){
+    let encontraPixel = document.querySelectorAll('.pixel');
+    encontraPixel.forEach((element) => {
+        element.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = cor
+    })
+    })
+    
 }
