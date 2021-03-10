c4858b61cb43aa6f4d757ff620c9207ba10f4dfe
aline <aline.melryaa@gmail.com>
2021-03-10 15:02:18 -0400

    adicionado lado a lado paleta

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let paletasCores = document.getElementById("color-palette");
-function cores(){
-    var corPreta= document.getElementsByClassName(".background-color:black");
-    corPreta.style
-}
+let paletaCores = document.getElementById("color-palette")
+let pixel = document.getElementById("pixel-board");
+let quadrado = document.getElementById("pixel")
+function tabela(){
+for (index = 0; index <5; index +=1){
+    let quadrado = document.createElement("div");
+    pixel.appendChild(quadrado);
+
+    for(index2 = 0; index2 < 5;index2 +=1){
+        let tabel = document.createElement("div");
+        quadrado.appendChild(tabel);
+        tabel.className = " pixel";
+    }
+};
+};
+tabela()
+
