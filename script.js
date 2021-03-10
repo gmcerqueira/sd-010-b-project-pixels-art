6c6b279a8042f679a49157ba561903a3405e0a4a
root <diegoserafim1@gmail.com>
2021-03-09 23:50:33 -0300

    D7 section color-palette

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // const title = document.querySelector('#title');
 const colorPalette = document.querySelector('#color-palette');
 // const color = document.querySelector('.color');
-// const pixelBoard = document.querySelector('#pixel-board');
+//const pixelBoard = document.querySelector('#pixel-board');
 // const pixel = document.querySelector('.pixel');
 
-colorPalette.addEventListener('click', event => {
+colorPalette.addEventListener('click', event => { // https://www.w3schools.com/js/js_htmldom_eventlistener.asp
     const colorSelected = document.getElementById(event.target.id);
     const verify = colorSelected.className;
         verify == 'color-palette' ? (
-            event.stopPropagation()
-        )
-        (
+            event.stopPropagation();
+        ):(
             document.querySelector('.selected').className = 'color', colorSelected.className = 'color selected')
 }, false);
