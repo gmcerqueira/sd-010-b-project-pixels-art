8464f240ab099e68e48422762bc6902d707cea6a
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-10 15:23:11 -0300

    desafio4

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function mudandoacorpreto() {
-  let preto = document.getElementsByClassName("color")[0]
-  preto.style.backgroundColor = "black"
+  let preto = document.getElementsByClassName("color")[0];
+  preto.style.backgroundColor = "black";
 }
 mudandoacorpreto();
 
 function mudandoacorosa() {
-  let rosa = document.getElementsByClassName("color")[1]
-  rosa.style.backgroundColor = "#F47CC4"
+  let rosa = document.getElementsByClassName("color")[1];
+  rosa.style.backgroundColor = "#F47CC4";
 }
 mudandoacorosa();
 
 function mudandoacorverdea() {
-  let verdea = document.getElementsByClassName("color")[2]
-  verdea.style.backgroundColor = "#6fffe9"
+  let verdea = document.getElementsByClassName("color")[2];
+  verdea.style.backgroundColor = "#6fffe9";
 }
 mudandoacorverdea();
 
 function mudandoacoroxa() {
-  let roxo = document.getElementsByClassName("color")[3]
-  roxo.style.backgroundColor = "#9B5DE5"
+  let roxo = document.getElementsByClassName("color")[3];
+  roxo.style.backgroundColor = "#9B5DE5";
 }
 mudandoacoroxa();
