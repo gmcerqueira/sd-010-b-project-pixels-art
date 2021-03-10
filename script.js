db1609086c03eca142acd763bc3d237510c8fc6c
Valeriandreoni <valeriandreoni@gmail.com>
2021-03-10 13:01:57 -0300

    desafio 1

---
+++ script.js
@@ -0,0 +1,29 @@
+function mudandoacorpreto() {
+  let preto = document.getElementsByClassName("color")[0]
+  preto.style.backgroundColor = "black"
+}
+mudandoacorpreto();
+
+function mudandoacorosa() {
+  let rosa = document.getElementsByClassName("color")[1]
+  rosa.style.backgroundColor = "#F47CC4"
+}
+mudandoacorosa();
+
+function mudandoacorazul() {
+  let azul = document.getElementsByClassName("color")[2]
+  azul.style.backgroundColor = "#00BBF9"
+}
+mudandoacorazul();
+
+function mudandoacorverdea() {
+  let verdea = document.getElementsByClassName("color")[3]
+  verdea.style.backgroundColor = "#64dfdf"
+}
+mudandoacorverdea();
+
+function mudandoacoroxa() {
+  let roxo = document.getElementsByClassName("color")[4]
+  roxo.style.backgroundColor = "#9B5DE5"
+}
+mudandoacoroxa();
