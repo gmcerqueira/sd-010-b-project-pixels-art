6095c7d2f99e6a802151a9502ff1ac8264fbb08b
root <diegoserafim1@gmail.com>
2021-03-10 00:01:33 -0300

    D7 tentando conectar a tabela id pixel-board

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // const title = document.querySelector('#title');
 const colorPalette = document.querySelector('#color-palette');
 // const color = document.querySelector('.color');
-//const pixelBoard = document.querySelector('#pixel-board');
+const pixelBoard = document.querySelector('#pixel-board');
 // const pixel = document.querySelector('.pixel');
 
 colorPalette.addEventListener('click', event => { // https://www.w3schools.com/js/js_htmldom_eventlistener.asp
     const colorSelected = document.getElementById(event.target.id);
     const verify = colorSelected.className;
         verify == 'color-palette' ? (
-            event.stopPropagation();
+            event.stopPropagation()
         ):(
             document.querySelector('.selected').className = 'color', colorSelected.className = 'color selected')
 }, false);
+
+pixelBoard.addEventListener('click', event => {
+    const selectedPixel = event.target;
+    const selectedColor = document.querySelector('.selected');
+    const selectedColorRgb = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
+        selectedPixel.style.backgroundColor = selectedColorRgb;
+});
