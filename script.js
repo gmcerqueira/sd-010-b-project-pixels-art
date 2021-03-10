d8c5a9e668eb4344235736b00ce8357087a4f2cc
aythan azevedo <aythan16@gmail.com>
2021-03-10 17:44:44 -0300

    quest 7

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ window.onload = function trocaCor() {
     preto.classList.add("selected")
   }
 
+  let preto = document.getElementById("preto");
+  
+  let paleta = document.querySelectorAll(".color")
 
-let preto = document.getElementById("preto");
+  paleta.addEventListener("click",clicado)
+
+function clicado(remover){
+  
+
+   
+for(let i in remover ){
+
+
+  remover[i].classList.remove("selected")
+}
+};
+
+
 
