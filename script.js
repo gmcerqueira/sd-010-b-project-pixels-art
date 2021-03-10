08ab4e89f073afa1f17bf9b56cd10e172ac45450
Thiago Marchini <thiago@msn.com>
2021-03-10 09:14:08 -0300

    modifica a funcao auxiliar resizeMinMax

--- script.js
+++ script.js
@@ -48,38 +48,34 @@ function removeAllChildNodes(parent) {
 
 function resizeMinMax(parameter) {
   if (parameter < 5) {
-    return 5; 
+    return 5;
   }
   if (parameter > 50) {
-    return 50; 
+    return 50;
+  } else {
+    return parameter;
   }
+
 }
 
 window.onload = function () {
-  // cria a paleta de cores
   createPalette();
-
-  // cria o grid de N por N
   createGrid(5);
-
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('color') && !event.target.classList.contains('selected')) {
       document.querySelectorAll('.selected')[0].className = 'color';
       event.target.className = 'color selected';
     }
-
     if (event.target.classList.contains('pixel')) {
       const selected = document.getElementsByClassName('selected');
       event.target.style.backgroundColor = selected[0].style.backgroundColor;
     }
-
     if (event.target.id === 'clear-board') {
       const pixelList = document.querySelectorAll('.pixel');
       for (let pixel of pixelList) {
         pixel.style.backgroundColor = 'white';
       }
     }
-
     if (event.target.id === 'generate-board') {
       const size = document.getElementById('board-size');
       if (size.value && size.value > 0) {
