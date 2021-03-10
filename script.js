5d764a9a0da7f35a9f522b58c5a4543e6d4da4e1
Igor Machado <rf59imft@gmail.com>
2021-03-10 14:21:45 -0300

    desafio 7

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 function setPixelClass() {
-  // const colorPalette = document.querySelectorAll('.color-palette');
   const colorSelected = document.querySelectorAll('.color');
-
   for (let item = 0; item < colorSelected.length; item += 1) {
     colorSelected[item];
-    // console.log(seletecColorPalette);
     colorSelected[item].addEventListener('click', function (event) {
       console.log(event.target);
       console.log(colorSelected[item].style);
-
-      if (colorSelected[item].classList !== 'selected') {
-        colorSelected[item].classList.add('selected');
-      } else if (colorSelected[item].classList) {
-        colorSelected[item].classList.remove('selected');
+      if (colorSelected[item] !== 'selected') {
+        event.target.classList.add('selected');
+      } else {
+        event.target.classList.remove('selected');
       }
-
-      // colorPalette[item].classList.remove('selected');
     });
   }
 }
