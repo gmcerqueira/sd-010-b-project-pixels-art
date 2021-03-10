d0d503d9b4fb05ef74994805ed6d4014d144c519
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-10 11:39:43 -0300

    Adiciona um início da idéa do código pra gerar as divs pais para o quadro de pixels com N elementos, e adiciona pallet com cores aleatórias

--- script.js
+++ script.js
@@ -2,36 +2,47 @@ window.onload = function() {
     let pixelPalette = 4;
     // let pixelBoardLength = window.prompt("Enter the number of elements you want for the Pixel-board");
     let pixelBoardLength = 5;
-    let pixelBoardBase = 5;
-    let colorPalette = document.querySelector("#color-palette");
-
+    let pixelBoardBase = 5;    
+    let paletteColor = document.querySelectorAll(".color")
     let firstPaletteColor = document.querySelector(".color");
         firstPaletteColor.style.backgroundColor = "black";
-    let secondPaletteColor = document.querySelectorAll(".color")[1];
-        secondPaletteColor.style.backgroundColor = "red";
-    let thirdPaletteColor = document.querySelectorAll(".color")[2];
-        thirdPaletteColor.style.backgroundColor = "green"; 
-    let fourthPaletteColor = document.querySelectorAll(".color")[3];
-        fourthPaletteColor.style.backgroundColor = "yellow"; 
 
+    randomColors();    
+    function randomColors() {
+        for (let index = 1; index < paletteColor.length; index += 1) {
+            paletteColor[index].style.backgroundColor = getRandomColor()
+        }
+    }
 
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+        // let secondPaletteColor = document.querySelectorAll(".color")[1];
+        //     secondPaletteColor.style.backgroundColor = "red";
+        // let thirdPaletteColor = document.querySelectorAll(".color")[2];
+        //     thirdPaletteColor.style.backgroundColor = "green"; 
+        // let fourthPaletteColor = document.querySelectorAll(".color")[3];
+        //     fourthPaletteColor.style.backgroundColor = "yellow"; 
+
+    let boarFathers = document.querySelector("#pixel-board")
     let lines = document.querySelectorAll(".lines");
-    fillBoard(lines);
-
-    // repeatLines(lines);
-
-    // function createPalette(pixelPalette) {        
-    //     let pixelColorPalette = document.createElement("div");
-    //     for (let index = 0; index < pixelPalette; index += 1) {
-    //         colorPalette.appendChild(pixelColorPalette)[index];
-    //         colorPalette[index].className = "color";
-    //     }
-    // } 
+    fillBoard(lines);    
 
     function fillBoard(lines)  {
         for (let i = 0; i < pixelBoardLength; i += 1) {
             createBoardLines(lines[i]);            
         }
+    }
+
+    function createBoardFathers(className){
+        let lines = createElement("div");
+        lines.className = className;
+        return lines
     }
 
     function createBoardPixel(className) {
