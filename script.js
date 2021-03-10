443aa4fb1106627e9d95820239d3834d4a5a5175
root <diegoserafim1@gmail.com>
2021-03-10 11:21:58 -0300

    D7/8 inicando, e consertando erros de espaçamentos

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const pixelBoard = document.querySelector('#pixel-board');
 // const pixel = document.querySelector('.pixel');
 
 colorPalette.addEventListener('click', event => { // https://www.w3schools.com/js/js_htmldom_eventlistener.asp
-    const colorSelected = document.getElementById(event.target.id);
-    const verify = colorSelected.className;
-        verify == 'color-palette' ? (
-            event.stopPropagation()
-        ):(
-            document.querySelector('.selected').className = 'color', colorSelected.className = 'color selected')
+  const colorSelected = document.getElementById(event.target.id);
+  const verify = colorSelected.className;
+  verify === 'color-palette' ? (
+  event.stopPropagation()
+  ):(
+  document.querySelector('.selected').className = 'color',
+  colorSelected.className = 'color selected')
 }, false);
 
 pixelBoard.addEventListener('click', event => {
-    const selectedPixel = event.target;
-    const selectedColor = document.querySelector('.selected');
-    const selectedColorRgb = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
-        selectedPixel.style.backgroundColor = selectedColorRgb;
+  const selectedPixel = event.target;
+  const selectedColor = document.querySelector('.selected');
+  const selectedColorRgb = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
+  selectedPixel.style.backgroundColor = selectedColorRgb;
 });
