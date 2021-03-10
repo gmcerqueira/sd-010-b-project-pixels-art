6a92f04a699d93c4401dd291e258087c3940a522
aythan azevedo <aythan16@gmail.com>
2021-03-10 18:36:12 -0300

    retorno a quest 6

--- script.js
+++ script.js
@@ -6,39 +6,5 @@ window.onload = function trocaCor() {
   let preto = document.getElementById("preto");
   
 
-  function add(){
-    let paleta = document.querySelectorAll(".color")
-    
-
-    for (let index = 0; index < paleta.length; index++) {
-
-     paleta[index].addEventListener("click", clicado(index));
-      
-    }
-  }
-
-  
-function clicado(i){
-
-  let e = document.querySelectorAll(".color")
-
-  let ed = document.getElementsByClassName("selected")
-
-for(let i = 0 ; i < ed.length ; i ++ ) {
-
-  ed[i].classList.remove("selected")
-
-}
-
-
-
-  e[i].classList.add("selected")
-
-};
-
-add ()
-
-
-
 
 
