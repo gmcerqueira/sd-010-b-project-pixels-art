ef40cf48b83138140d1b363a0a51d8ca4a248c73
legalvao1 <legalvao1@gmail.com>
2021-03-09 23:38:57 -0300

    Arrumando alguns erros de lint

--- script.js
+++ script.js
@@ -1,93 +1,99 @@
-//criando a paleta
+// criando a paleta
 
-let paleta = document.querySelector('#color-palette');
-for (let box = 0; box < 4; box += 1){
-    let cor = document.createElement('div')
-    cor.className = 'color'
-    paleta.appendChild(cor);
+const paleta = document.querySelector('#color-palette');
+for (let box = 0; box < 4; box += 1) {
+  const cor = document.createElement('div');
+  cor.className = 'color';
+  paleta.appendChild(cor);
 }
 // Adicionando as cores
-let cores = document.querySelectorAll('.color')
+const cores = document.querySelectorAll('.color');
 
-let corBlack = cores[0]
-corBlack.className = 'color selected'
+const corBlack = cores[0];
+corBlack.className = 'color selected';
 corBlack.style.backgroundColor = 'black';
 
-let corRed = cores[1]
+const corRed = cores[1];
 corRed.style.backgroundColor = 'red';
 
-let corGreen = cores [2]
+const corGreen = cores[2];
 corGreen.style.backgroundColor = 'green';
 
-let corBLue = cores[3]
+const corBLue = cores[3];
 corBLue.style.backgroundColor = 'blue';
 
 // table Row - Criando as cinco linhas
 
-let pixelBoard = document.querySelector('#pixel-board')
-for(let line = 0; line < 5; line += 1){
-    let box = document.createElement('tr');
-    box.className='pixel-table'
-    pixelBoard.appendChild(box);
+const pixelBoard = document.querySelector('#pixel-board');
+for (let line = 0; line < 5; line += 1) {
+  const box = document.createElement('tr');
+  box.className = 'pixel-table';
+  pixelBoard.appendChild(box);
 }
 // celulas da tabela
-let linhas = document.querySelectorAll('#pixel-board, tr'); // recupero todos os elem tr criados da id #pixel-board
+const linhas = document.querySelectorAll('#pixel-board, tr'); // recupero todos os elem tr criados da id #pixel-board
 for (let line = 1; line < linhas.length; line += 1) {
-    for (let colum = 1; colum < linhas.length; colum +=1){
-        let box = document.createElement('td')
-        box.className = 'pixel';
-        linhas[colum].appendChild(box);
-    }
+  for (let colum = 1; colum < linhas.length; colum += 1) {
+    const box = document.createElement('td');
+    box.className = 'pixel';
+    linhas[colum].appendChild(box);
+  }
 }
 
 // cor selecionada
 sessionStorage.setItem('color', corBlack.style.backgroundColor);
 
-function corSelecionada(index){
-    if (index === 0){
-        sessionStorage.setItem('color',corBlack.style.backgroundColor);
-        corBlack.className = 'color selected';
-        corRed.className = 'color';
-        corBLue.className = 'color';
-        corGreen.className = 'color';
-    } else if(index === 1){
-        sessionStorage.setItem('color',corRed.style.backgroundColor);
-        corBlack.className = 'color';
-        corRed.className = 'color selected';
-        corBLue.className = 'color';
-        corGreen.className = 'color';
-    }else if(index === 2){
-        sessionStorage.setItem('color',corBLue.style.backgroundColor);
-        corBlack.className = 'color';
-        corRed.className = 'color';
-        corBLue.className = 'color selected';
-        corGreen.className = 'color';
-    }else if(index === 3){
-        sessionStorage.setItem('color',corGreen.style.backgroundColor);
-        corBlack.className = 'color';
-        corRed.className = 'color';
-        corBLue.className = 'color ';
-        corGreen.className = 'color selected';
-    }
+function corSelecionada(index) {
+  if (index === 0) {
+    sessionStorage.setItem('color', corBlack.style.backgroundColor);
+    corBlack.className = 'color selected';
+    corRed.className = 'color';
+    corBLue.className = 'color';
+    corGreen.className = 'color';
+  } else if (index === 1) {
+    sessionStorage.setItem('color', corRed.style.backgroundColor);
+    corBlack.className = 'color';
+    corRed.className = 'color selected';
+    corBLue.className = 'color';
+    corGreen.className = 'color';
+  } else if (index === 2) {
+    sessionStorage.setItem('color', corBLue.style.backgroundColor);
+    corBlack.className = 'color';
+    corRed.className = 'color';
+    corBLue.className = 'color selected';
+    corGreen.className = 'color';
+  } else if (index === 3) {
+    sessionStorage.setItem('color', corGreen.style.backgroundColor);
+    corBlack.className = 'color';
+    corRed.className = 'color';
+    corBLue.className = 'color ';
+    corGreen.className = 'color selected';
+  }
 }
 
-corBlack.addEventListener('click',function() {corSelecionada(0)});
-corRed.addEventListener('click', function() {corSelecionada(1)});
-corBLue.addEventListener('click', function() {corSelecionada(2)});
-corGreen.addEventListener('click', function() { corSelecionada(3)});
+corBlack.addEventListener('click', function() { corSelecionada(0) });
+corRed.addEventListener('click', function() { corSelecionada(1) });
+corBLue.addEventListener('click', function() { corSelecionada(2) });
+corGreen.addEventListener('click', function() { corSelecionada(3) });
 
 // Adicionando cor ao quadrado selecionado
 
-let pixelTable = document.querySelectorAll('.pixel'); // pego todos os elem da classe pixel
+const pixelTable = document.querySelectorAll('.pixel'); // pego todos os elem da classe pixel
 
-for (let i = 0; i < pixelTable.length; i += 1){ // percorro todos os elem da classe que fora atribuidos a essa variavel
-    pintaOPixel(i); // e mando o indice para a função
+function pintaOPixel (i) {
+  pixelTable[i].addEventListener('click', function() { // adiciono o evento de click a todos os elem do array
+    pixelTable[i].style.backgroundColor = sessionStorage.color; // atribuo a cor salva no storage ao elemento que recebeu o click
+  });
 }
 
-function pintaOPixel (i){
-    pixelTable[i].addEventListener('click', function(){ // adiciono o evento de click a todos os elem do array
-        pixelTable[i].style.backgroundColor = sessionStorage.color // atribuo a cor salva no storage ao elemento que recebeu o click
-    });
+for (let i = 0; i < pixelTable.length; i += 1) { // percorro todos os elem da classe que fora atribuidos a essa variavel
+  pintaOPixel(i); // e mando o indice para a função
 }
 
+// Botão Limpa a paleta
+const body = document.querySelector('body')
+const botaoClear = document.createElement('div')
+botaoClear.id = 'clear-board'
+botaoClear.innerHTML = 'clear'
+body.appendChild(botaoClear)
 
