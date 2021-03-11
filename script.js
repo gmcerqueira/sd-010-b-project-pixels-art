765743a86b991085b590a30546adb6d2f49c9787
walterlbf <walter.lbf.f@gmail.com>
2021-03-11 17:54:27 -0400

    troquei o className de pixel por linha

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     for (let linha = 0; linha < 5; linha++) {
 
       let criaLinha = document.createElement("div")
-      criaLinha.className = "pixel";
+      criaLinha.className = "linha";
       criaLinha.id = linha;
       pixelBoard.appendChild(criaLinha);
       criaLinha.addEventListener('click', pintaPixel)
@@ -60,7 +60,7 @@
   }
 
   function limparPixels() {
-    
+
     let pixels = document.getElementsByClassName('pixel');
     for (let index = 0; index < pixels.length; index ++) {
 
