283c432336a2728ff20a134162ec46e5dcfd271f
Camila <camilafabiabh9@gmail.com>
2021-03-11 11:13:36 -0300

    Criando cores e variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Alguns colegas me ajudaram a construir meu raciocinio */
+let colorDivPallette = document.querySelector('#color-palette');
+let color = document.querySelectorAll('.color');
+let colorInicial = 'black';
+let auxiliar = 0;
+color[0].style.backgroundColor = 'black';
+color[1].style.backgroundColor = 'pink';
+color[2].style.backgroundColor = 'teal';
+color[3].style.backgroundColor = 'green';
+color[4].style.backgroundColor = 'red';
+// Function paleta de 4 cores
