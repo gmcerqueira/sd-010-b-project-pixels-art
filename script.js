2243b4ce2a38e38659beeaa64c997dbe5d29e998
aline <aline.melryaa@gmail.com>
2021-03-10 21:36:19 -0400

    criando class selected

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function  criarDivs(){
-    // chama minha div htm id pixel-bord
-    let pixel = document.getElementById("pixel-board");
-    //criação das novas divs no for
-    for (index = 0; index < 25; index +=1){
-        let cd= document.createElement("div");//variavel cd recebe as novas divs criadas
-        cd.className = 'pixel';// cd.className recebe a class pixel
-        pixel.appendChild(cd);//pixel é adicionado na variavel cd
-    }
-
+function criarDivs() {
+  // chama minha div htm id pixel-bord
+  let pixel = document.getElementById('pixel-board');
+  //criação das novas divs no for
+  for (index = 0; index < 25; index += 1) {
+    let cd = document.createElement('div'); //variavel cd recebe as novas divs criadas
+    cd.className = 'pixel'; // cd.className recebe a class pixel
+    pixel.appendChild(cd); //pixel é adicionado na variavel cd
+  }
 }
-window.onload = criarDivs();
+   
+     
 
-
-
+ window.onload = criarDivs();
