23d543e81d3fa842fc8b11773667b290a48e42cd
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-11 00:06:35 -0300

    Requisito 7 completo

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ for (let indexLinhas = 0; indexLinhas < n; indexLinhas += 1) {
   }
 }
 
-document.getElementsByClassName('color')[0].className = 'color selected';
+// document.getElementsByClassName('color')[0].className = 'color selected';
+
+//requisito 7
+let cor = document.getElementsByClassName('color');
+let colorPalette = document.getElementById('color-palette');
+function corTroca() {
+  for (let corIndex = 0; corIndex < cor.length; corIndex += 1) {
+    cor[corIndex].addEventListener('click', function () {
+      colorPalette.querySelector('.selected').classList.remove('selected');
+      this.classList.add('selected');
+    })
+  }
+}
+
+corTroca();
