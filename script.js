9bfc092396c59621aeae422d88f00db87793686b
Aladino Borges <eualadino@gmail.com>
2021-03-11 13:26:24 -0300

    Requisito 1: Adiciona o titulo Paleta de Cores usando o JavaScript e faz link dos ficheiros style.css e script.js no documento HTML.

---
+++ script.js
@@ -0,0 +1,7 @@
+const generateHeader = document.querySelector('body');
+const headerGenerator = document.createElement('h1');
+headerGenerator.id = "title";
+headerGenerator.innerText = 'Paleta de Cores';
+generateHeader.appendChild(headerGenerator);
+
+console.log(document.querySelector('body'));
