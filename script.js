995561c97ced08250d381e4c2288246a78666e2f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-11 19:10:08 -0300

    Estilizando e completando requisito 10 e 11

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const table = document.getElementById('color-palette');
 const pixel = document.getElementById('pixel-board');
 
 function structureTable() {
-  const line1 = document.createElement('div');
+  const line1 = document.createElement('tr');
   line1.className = 'palette'
   table.appendChild(line1);
   for (let columns = 0; columns < 4; columns += 1) {
-    const column = document.createElement('div');
+    const column = document.createElement('td');
     column.className = 'color';
     line1.appendChild(column);
   } 
@@ -20,19 +20,6 @@ function styleTable() {
     column[3].style.backgroundColor = 'blue';
   }
 } styleTable();
-function pixelStructure() {
-  for (let lines = 0; lines < 5; lines += 1) {
-    const pixelLine = document.createElement('div');
-    pixelLine.className = 'pixelLine';
-    pixel.appendChild(pixelLine);  
-    for (let columns = 0; columns < 5; columns += 1) {
-      const column1 = document.createElement('div');
-      column1.className = 'pixel';
-      pixelLine.appendChild(column1);
-      column1.style.backgroundColor = "white";
-    }
-  }
-} pixelStructure();
 function blackSelect () {
   column[0].classList.add('selected');
 } blackSelect();
@@ -45,14 +32,52 @@ document.addEventListener('click', function (event) {
   event.target.style.backgroundColor = document.querySelector(".selected").style.backgroundColor;
   }
 })
-// Com o auxilio de Andre de Bem e Henrique Zozimo
 const btnReset = document.querySelector("#clear-board");
-function resetPixel () {
+//Adicionando botao e input
+const generateSize = document.getElementById('board-size');
+const btnRandom = document.getElementById('generate-board');
+btnRandom.innerHTML = 'VQV';
+function generate () {
+  generateSize.value = 5;
+} generate();
+function pixelStructure(valor) {
+    for (let lines = 0; lines < valor; lines += 1) {
+      const pixelLine = document.createElement('tr');
+      pixelLine.className = 'pixelLine';
+      pixel.appendChild(pixelLine);  
+      for (let columns = 0; columns < valor; columns += 1) {
+        const column1 = document.createElement('td');
+        column1.className = 'pixel';
+        pixelLine.appendChild(column1);
+        column1.style.backgroundColor = "white";
+      }
+    }
+} pixelStructure(generateSize.value);
+// Com o auxilio de Andre de Bem e Henrique Zozimo
+function buttonReset () {
   btnReset.innerHTML = "Limpar";
-} resetPixel()
+} buttonReset()
 btnReset.addEventListener("click", () => {
   const column1 = document.querySelectorAll('.pixel');
   for (let index = 0; index < column1.length; index += 1) {
     column1[index].style.backgroundColor = "white";
   }
+})
+function resetPixel () {
+  pixel.innerHTML = "";  
+}
+
+btnRandom.addEventListener('click', function () {
+  resetPixel();
+  if (generateSize.value === "") {
+    alert('Board inválido!');
+  }
+  else if (generateSize.value < 5) {
+    generateSize.value = 5;
+  }
+  else if (generateSize.value > 50) {
+    generateSize.value = 50;
+  }
+  const valor = generateSize.value;
+  pixelStructure(valor);
 })
