a66e4a568bd94cf7636ccf477fcf5f4014cb9804
walterlbf <walter.lbf.f@gmail.com>
2021-03-11 17:14:25 -0400

    indentacao

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
   criaPixelBoard();
- 
 
-    function criaPixelBoard() {
+  function criaPixelBoard() {
     let pixelBoard = document.getElementById("pixel-board");
 
     for (let linha = 0; linha < 5; linha++) {
@@ -11,7 +10,7 @@
       criaLinha.id = linha;
       pixelBoard.appendChild(criaLinha);
       criaLinha.addEventListener('click', pintaPixel)
-      
+
 
       for (let coluna = 0; coluna < 5; coluna++) {
 
@@ -19,7 +18,7 @@
         criaColuna.className = "pixel";
         criaLinha.appendChild(criaColuna);
         criaColuna.id = linha + "." + coluna;
-        
+
       }
 
     }
@@ -28,41 +27,34 @@
 
   }
 
-  function click(){
+  function click() {
 
     document.querySelector("#black").addEventListener('click', selecionaCor);
     document.querySelector("#red").addEventListener('click', selecionaCor);
     document.querySelector("#blue").addEventListener('click', selecionaCor);
     document.querySelector("#green").addEventListener('click', selecionaCor);
 
-
-
   }
 
-  function selecionaCor(event){
+  function selecionaCor(event) {
     let boxSelecionada = event.target.id;
     let selecionandoPaleta = document.getElementsByClassName("color");
-    
+
     for (let index = 0; index < selecionandoPaleta.length; index++) {
-      selecionandoPaleta[index].classList.remove("selected");      
+      selecionandoPaleta[index].classList.remove("selected");
     }
 
     document.getElementById(boxSelecionada).classList.add("selected");
 
   }
 
-
-
-  function pintaPixel (event){
+  function pintaPixel(event) {
 
     let idPixelEmBranco = event.target.id;
     let paletaComSelected = document.querySelector(".selected");
-    let corSelecionada = window.getComputedStyle(paletaComSelected, null).backgroundColor; 
-    
+    let corSelecionada = window.getComputedStyle(paletaComSelected, null).backgroundColor;
+
     document.getElementById(idPixelEmBranco).style.backgroundColor = corSelecionada;
 
     console.log(corSelecionada);
   }
- 
-
-  
