919919a963b1fd5c8bd6f492801d1e0e4487b217
BarrisennLinux <nuncaparedelutar1984@gmail.com>
2021-03-11 12:07:50 -0300

    adição comandos no script2

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function createBoxTable(numOfItems, itemClass, parentId, numOfLines = 1) {
   const boxTable = document.createElement('table');
   paletteBox.appendChild(boxTable);
   for (let line = 1; line <= numOfLines; line += 1) {
-    const boxLine = document.createElement('tr');
-    boxTable.appendChild(boxLine);
-    for (let item = 1; item <= numOfItems; item += 1) {
-      const boxItem = document.createElement('td');
-      boxLine.appendChild(boxItem);
-      boxItem.classList.add(itemClass);
+  const boxLine = document.createElement('tr');
+  boxTable.appendChild(boxLine);
+  for (let item = 1; item <= numOfItems; item += 1) {
+  const boxItem = document.createElement('td');
+  boxLine.appendChild(boxItem);
+  boxItem.classList.add(itemClass);
     }
   }
 }
@@ -21,9 +21,30 @@ function createBoxTable(numOfItems, itemClass, parentId, numOfLines = 1) {
 function addClassToPaletteBox() {
     const boxes = document.getElementsByClassName('color');
     for (let index = 0; index < boxes.length; index += 1) {
-      boxes[index].classList.add(`color${index + 1}`);
-      if (getComputedStyle(boxes[index]).backgroundColor === 'rgb(0, 0, 0)') {
-        boxes[index].classList.add('selected');
+    boxes[index].classList.add(`color${index + 1}`);
+    if (getComputedStyle(boxes[index]).backgroundColor === 'rgb(0, 0, 0)') {
+    boxes[index].classList.add('selected');
       }
     }
+  }
+
+// Faz cada box ser clicavel
+function clickPaletteBox(event) {
+    const selectedBox = document.querySelector('.selected');
+    selectedBox.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+
+  // Adiciona o evento de click à box.
+  function addPaletteBoxClickEvent() {
+    const boxes = document.getElementsByClassName('color');
+    for (let index = 0; index < boxes.length; index += 1) {
+      boxes[index].addEventListener('click', clickPaletteBox);
+    }
+  }
+
+  // Muda a cor de fundo do elemento vinculado.
+  function changeBgColor(event, color) {
+    const element = event;
+    element.target.style.backgroundColor = color;
   }
