4f2a88d5407d1d961b89169042d528cde9cb0d5d
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-03-11 15:24:00 -0300

    correçao linter

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 function selectColor(selected) {
-  let oldColor = document.querySelector('.selected');
+  const oldColor = document.querySelector('.selected');
   oldColor.classList.remove('selected');
-  let newColor = selected.target;
+  const newColor = selected.target;
   newColor.classList.add('selected');
 }
 
 function clear() {
   const paintedPixels = document.querySelectorAll('.pixel');
   for (let index = 0; index < paintedPixels.length; index += 1) {
-    if (paintedPixels[index].classList.contains('painted')) {
     paintedPixels[index].style.backgroundColor = 'white';
     paintedPixels[index].classList.remove('painted');
   }
 }
-}
-document.getElementById("clear-board").addEventListener("click", clear);
+document.getElementById('clear-board').addEventListener('click', clear);
 
 function paintPixels() {
   const colorBlack = document.getElementById('black');
   const colorBlue = document.getElementById('blue');
   const colorRed = document.getElementById('red');
   const colorGreen = document.getElementById('green');
-  const fullClass = 'color selected'
+  const fullClass = 'color selected';
   if (colorBlack.className === fullClass) {
     this.style.backgroundColor = 'black';
   } else if (colorBlue.className === fullClass) {
@@ -32,11 +30,11 @@ function paintPixels() {
     this.style.backgroundColor = 'green';
   }
   this.classList.add('painted');
-} 
+}
 const pixelColor = document.querySelectorAll('.pixel');
 for (let index = 0; index < pixelColor.length; index += 1) {
   pixelColor[index].addEventListener('click', paintPixels);
 }
 // Para resolver esse requisito (8), dei uma conferida no Pull Request dos meus colegas Fioravante e Mateus Bodra, que me fez entender a lógica por trás desse requisito
 
-document.getElementById("color-palette").addEventListener("click", selectColor);
+document.getElementById('color-palette').addEventListener('click', selectColor);
