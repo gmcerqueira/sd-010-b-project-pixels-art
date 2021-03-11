53518ffbd77ab9885423e06fc51ac97259a3d1b6
Carlos <cemargato@gmail.com>
2021-03-10 22:45:00 -0300

    entendendo eventlistener

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-window.onload = function () {
-
-
-
-  const tabela = document.querySelector("#pixel-board")//chama tabela
-  let tamanho = 5
-
-  // linha.appendChild(pixel);
-  //   tabela.appendChild(linha)
-  for (let index = 0; index < tamanho; index += 1) {
-    let linha = document.createElement('tr');//da linha
-    for (let index2 = 0; index2 < tamanho; index2 += 1) {
-      let pixel = document.createElement('td');// da celula
+//criando a tabela com disponibilidade para alteração de tamanho
+function tableMaker() {
+  const tabela = document.querySelector("#pixel-board")//chamada tabela
+  let tamanho = 5;
+  let count = 0
+  for (let index = 1; index <= tamanho; index += 1) {
+    let linha = document.createElement('tr');
+    for (let index2 = 1; index2 <= tamanho; index2 += 1) {
+      let pixel = document.createElement('td');
+      count += 1;
       pixel.className = 'pixel';
-      linha.appendChild(pixel)// cria a celula
+      
+      linha.appendChild(pixel);
     }
     linha.className = 'line';
-    tabela.appendChild(linha)// era pra criar as linhas
+    tabela.appendChild(linha);
   }
-
 }
-document.prepend
+
+tableMaker();
+let listCollors = document.querySelectorAll('.color')
+let black = document.querySelector('.color');
+black.classList.add('selected')
+let fullColors = document.getElementsByClassName('pixel')
+let selectedColor = document.getElementsByClassName('selected')
+
+function selection(){
+  for(index=0; index < listCollors.length; index+=1){
+    listCollors[index].addEventListener('click',changeClass)
+  }
+}
+selection()
+function changeClass(styleClass) {
+}
