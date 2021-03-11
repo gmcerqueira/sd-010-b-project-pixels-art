6f43251e6a1397f23540119f9498a8fb90189a6a
Carlos <cemargato@gmail.com>
2021-03-11 14:54:17 -0300

    acertando requisitos 6 e 7

--- script.js
+++ script.js
@@ -2,33 +2,51 @@
 function tableMaker() {
   const tabela = document.querySelector("#pixel-board")//chamada tabela
   let tamanho = 5;
-  let count = 0
   for (let index = 1; index <= tamanho; index += 1) {
     let linha = document.createElement('tr');
     for (let index2 = 1; index2 <= tamanho; index2 += 1) {
       let pixel = document.createElement('td');
-      count += 1;
       pixel.className = 'pixel';
-      
       linha.appendChild(pixel);
     }
     linha.className = 'line';
     tabela.appendChild(linha);
   }
 }
-
 tableMaker();
+
 let listCollors = document.querySelectorAll('.color')
 let black = document.querySelector('.color');
 black.classList.add('selected')
 let fullColors = document.getElementsByClassName('pixel')
 let selectedColor = document.getElementsByClassName('selected')
 
-function selection(){
-  for(index=0; index < listCollors.length; index+=1){
-    listCollors[index].addEventListener('click',changeClass)
+//para a função de pegar cor e repassar para o pixel contei com a aula/ajuda de Luiz Paulo 'LP' T10B para entender sobre addEventlistener e seu acompanhamento para a elaboração das funções
+function selection() {
+  for (index = 0; index < listCollors.length; index += 1) {
+    listCollors[index].addEventListener('click', changeClass)
   }
 }
 selection()
+
 function changeClass(styleClass) {
+  for (index = 0; index < listCollors.length; index += 1) {
+    if (listCollors[index].classList.contains('selected')) {
+      listCollors[index].classList.remove('selected')
+    }
+  }
+  styleClass.target.classList.add('selected')
+}
+
+function colorAcquire() {
+
+  for (let index = 0; index < fullColors.length; index += 1) {
+    fullColors[index].addEventListener('click', colorSelector)
+  }
+}
+colorAcquire();
+
+function colorSelector(clickar) {
+
+  clickar.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor
 }
