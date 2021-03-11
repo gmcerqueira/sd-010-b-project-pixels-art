1f579bc1ee7d231d06c4fcdd2f2dcc0515a56148
Aladino Borges <eualadino@gmail.com>
2021-03-11 14:56:09 -0300

    Requisito 2: Cria duas funçoes para criar a paleta de cores e formata a base delas com CSS e JavaScript.

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-const generateHeader = document.querySelector('body');
-const headerGenerator = document.createElement('h1');
-headerGenerator.id = "title";
-headerGenerator.innerText = 'Paleta de Cores';
-generateHeader.appendChild(headerGenerator);
+//1. Adiciona o título:
+function headerMaker() {
+  const generateHeader = document.querySelector('body');
+  const headerGenerator = document.createElement('h1');
+  headerGenerator.id = 'title';
+  headerGenerator.innerText = 'Paleta de Cores';
+  generateHeader.appendChild(headerGenerator);
+}
+headerMaker();
+
+//2. Adiciona a paleta de cores:
+function tablesFatherCreator() {
+  const generateTable = document.querySelector('body');
+  const tablesFather = document.createElement('div');
+  tablesFather.id = 'color-palette';
+  generateTable.appendChild(tablesFather);
+}
+
+tablesFatherCreator();
+
+function colorsTableMaker() {
+  const colors = [
+    'black',
+    'crimson',
+    'darkgoldenrod',
+    'yellow'
+  ];
+
+  for (let index = 0; index < colors.length; index += 1) {
+    const allTables = document.querySelector('#color-palette');
+    const tables = document.createElement('div');
+    tables.id = colors[index];
+    tables.className = 'color';
+    tables.style.backgroundColor = colors[index];
+    ;
+    allTables.appendChild(tables);
+  }
+}
+
+colorsTableMaker();
 
 console.log(document.querySelector('body'));
