01aae0394619ce2dd1ab947bbe807b7ecf78c53b
Camila <camilafabiabh9@gmail.com>
2021-03-11 11:24:29 -0300

    criando função de cores

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 let colorDivPallette = document.querySelector('#color-palette');
 let color = document.querySelectorAll('.color');
 let colorInicial = 'black';
-let auxiliar = 0;
+let letAuxiliar = 0;
 color[0].style.backgroundColor = 'black';
 color[1].style.backgroundColor = 'pink';
 color[2].style.backgroundColor = 'teal';
 color[3].style.backgroundColor = 'green';
-color[4].style.backgroundColor = 'red';
+color[4].style.backgroundColor = 'yellow';
 // Function paleta de 4 cores
+function palletteColor(event) {
+  letAuxiliar = event.target;
+  color.getElementsByClassName = 'color';
+  if(letAuxiliar.className === 'color'){
+    letAuxiliar.className = 'color selected';
+    colorInicial = letAuxiliar.style.backgroundColor;
+  }
+}
+colorDivPallette.addEventListener('click', palletteColor)
