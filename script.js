4c437a36cdce47a5d9360c3e268fe1806e180fe8
Igor Machado <rf59imft@gmail.com>
2021-03-11 10:38:39 -0300

    desafio 8

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// let colorBlack = 'black';
-
 function setColorClass() {
-  const colorSelected = document.querySelectorAll('.selected');
   const color = document.querySelectorAll('.color');
 
   for (let index = 0; index < color.length; index += 1) {
-    // color[index].classList.remove('selected');
     color[index].addEventListener('click', function (event) {
       console.log(event.target);
       console.log(color[index].style);
@@ -18,43 +14,39 @@ function setColorClass() {
       } else {
         color[index].classList.add('selected');
       }
-      // procurar tirar o elemeto que esta selecionado previamente
-      // esta selecionado pela classe
     });
   }
-
-  /*
-  if (eventTarget.classList.contains('color')) {
-    eventTarget.classList.add('selected');
-  }
-  */
-
-  /*
-  colorSelected[1].addEventListener('click', function (event) {
-    console.log(event.target);
-    console.log(colorSelected[item].style);
-    
-    // procurar tirar o elemeto que esta selecionado previamente
-    // esta selecionado pela classe
-
-    
-  });*/
 }
 
 setColorClass();
 
 function setPixelColor() {
-  let selectedColor = document.querySelector('.selected');
-  let pixel = document.querySelectorAll('.pixel');
-  let color = document.querySelectorAll('.color');
-  // let paintColorPixel = color.style.backgroundColor;
+  let selectedColor = document.getElementsByClassName('selected');
+  let pixels = document.querySelectorAll('.pixel');
+  let ColorDiv = document.querySelector('.color');
+  let getColorDiv = ColorDiv.style.backgroundColor;
+  //  console.log(selectedColor);
+  for (let item = 0; item < selectedColor.length; item += 1) {
+    selectedColor[item];
+    console.log(selectedColor[item]);
 
-  for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].addEventListener('click', function (event) {
-      if (selectedColor !== pixel[index].style.color) {
-        pixel[0].style.backgroundColor = 'red';
-      }
-    });
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index];
+      console.log(pixels[index]);
+
+      pixels[index].addEventListener('click', function (event) {
+        console.log(pixels[index]);
+        if (selectedColor[item].classList.contains('black')) {
+          pixels[index].style.backgroundColor = 'black';
+        } else if (selectedColor[item].classList.contains('red')) {
+          pixels[index].style.backgroundColor = 'red';
+        } else if (selectedColor[item].classList.contains('green')) {
+          pixels[index].style.backgroundColor = 'green';
+        } else {
+          pixels[index].style.backgroundColor = 'blue';
+        }
+      });
+    }
   }
 }
 
