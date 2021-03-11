2f4ce6d6a609df7d567133e3be587595811ccbdf
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-11 18:18:07 -0300

    Executado requisito 6

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-window.onload 
+///////////////////////////////////////////////////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Quadrado de pixels 
 
 let paleta = document.createElement('table')
 document.body.appendChild(paleta) 
@@ -33,6 +16,65 @@ for (let index = 0; index < 5; index += 1){
         elementoPixel.className = 'pixel'    
 }
 }
+///////////////////////////////////////////////////////////
+
+// Atribui a clase 'selected'
+
+// let colorClass = document.querySelectorAll('.color')
+
+// colorClass.addEventListener('click', classAtributte)
+
+// function classAtributte(evento){
+// evento.target.className = "selected"
+// }
 
 
-        
+//////////////////////////////////////////////////////////
+
+// Cor preta como cor inicial
+
+window.onload = function(){
+let corPreta = document.getElementById('black')    
+corPreta.classList.add = "selected"   
+}
+
+//////////////////////////////////////////////////////////
+//Selecionando a cor
+
+// let cor = document.querySelectorAll('.color')
+
+// function select(){
+//     for (let index = 0; index < cor.length; index += 1){
+//        cor[index].addEventListener('click', function(){
+//            localStorage.setItem('colorSelected', JSON.stringify(cor[index]))
+//        }) 
+//     }
+// }
+
+///////////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////
+
+// Aplicando as cores
+
+// let pintarPixel = document.getElementById('pixel-board')
+
+// pintarPixel.addEventListener('click', pintar)
+
+// function pintar(evento){
+//     let cor = JSON.parse(localStorage.getItem('cores'))
+//     if (cor === blackColor){
+//         evento.target = blackColor
+//     }
+// }
+
+//////////////////////////////////////////////////////////
+
+// Botão 'Limpar'
+
+// let botaoLimpar = document.createElement('button')
+// document.body.appendChild(botaoLimpar)
+// botaoLimpar.innerHTML = "Limpar"
