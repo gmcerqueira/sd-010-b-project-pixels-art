4406b11c17f211b73aa720bc7c584b887d2c939b
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-11 16:07:07 -0300

    exercicio 8

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const quadradoAzul = document.getElementsByClassName('color')[1];
 const quadradoAmarelo = document.getElementsByClassName('color')[2];
 const quadradoVerde = document.getElementsByClassName('color')[3];
 let todasAsCores = document.querySelectorAll('.color');
+//cores da paleta de cores
+quadradoPreto.style.backgroundColor = 'black';
+quadradoAzul.style.backgroundColor = 'blue';
+quadradoAmarelo.style.backgroundColor = 'yellow';
+quadradoVerde.style.backgroundColor = 'green';
 
 //Função de criar o quadro com 25 quadradinhos
 function adicionandoPixels() {
@@ -42,15 +47,13 @@ for (let i = 0; i < todasAsCores.length; i += 1) {
 
 //colorindo o quadradinhos branco
 function pintandoOQuadradinho() {
-  let selecionaCor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+  const selecionaCor = document.getElementsByClassName('selected')[0].style.backgroundColor;
   event.target.style.backgroundColor = selecionaCor;
 }
 
-for (let j = 0; j < todosOs25Quadradinhos.length; j += 1) {
-  todosOs25Quadradinhos[j].addEventListener("click", pintandoOQuadradinho);
+function pintarTabela() {
+  let tabelaToda = document.getElementById('pixel-board');
+    tabelaToda.addEventListener("click", pintandoOQuadradinho);
 }
+pintarTabela();
 
-quadradoPreto.style.backgroundColor = 'black';
-quadradoAzul.style.backgroundColor = 'blue';
-quadradoAmarelo.style.backgroundColor = 'yellow';
-quadradoVerde.style.backgroundColor = 'green';
