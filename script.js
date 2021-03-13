5fbb3ee23da233f79e2a6e32ff8b8f4b3127afb3
renatogsouza <renatomacedo1995@gmail.com>
2021-03-13 15:04:26 -0300

    requisito 10 em andamento

--- script.js
+++ script.js
@@ -43,10 +43,15 @@ corInicial();
 
 
 // Pinta os pixels com a cor selecionada
-  pixelBorder.addEventListener('click', function(){
+function invoca() {
+for (let i = 0; i < pixelMuda.length; i++) {
+  pixelMuda[i].addEventListener('click', function(){
   let corNova = document.getElementsByClassName('selected')[0].style.backgroundColor;
   event.target.style.backgroundColor = corNova;    
   });
+}
+}
+invoca();
 
 // Transforma todos os pixels em branco
 btnLimpar.addEventListener('click', function(){
@@ -57,18 +62,32 @@ btnLimpar.addEventListener('click', function(){
 
 // Altera o tamanho dos pixels conforme o usario decide
 btnTamanho.addEventListener('click', function() {
+// Verifica se ha um valor no input
   if (nPixel.value == '') {
     alert('Board inválido!');
     nPixel.value = '';  
     nPixel.focus();
-  } else if (parseInt(nPixel.value) < 5 || parseInt(nPixel.value) > 50){
-    alert('Digite um valor entre 5 a 50 para ser aceito');  
+// Verifica se o valor no input é menor que 5
+  } else if (parseInt(nPixel.value) < 5 ){
+    number = 5;
+    pixelBorder.innerHTML = '';
+    criaPixels();
+    invoca();
+    nPixel.value = '';
+// Verifica se o valor do input é maior que 50
+  } else if(parseInt(nPixel.value) > 50){
+    number = 50;
+    pixelBorder.innerHTML = '';
+    criaPixels();
+    invoca(); 
     nPixel.value = '';  
     nPixel.focus();
+// Se estiver tudo ok a função roda normalmente com o valor descrito no input
   } else {
     number = parseInt(nPixel.value);
     pixelBorder.innerHTML = '';
     criaPixels();
+    invoca();
     nPixel.value = '';
     for (let i = 0; i < pixelMuda.length; i++) {
       pixelMuda[i].style.backgroundColor = 'white';
