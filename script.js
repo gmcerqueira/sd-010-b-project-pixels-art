fcb8e2b1e2b669b5f54c7fa5b48e861c86c7fa86
esionascimento <esio.392@protonmail.com>
2021-03-13 06:59:17 -0300

    arrumado lint css, js. concluido usuario definir tabela

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let corSalva = 'black';
 let aux;
 const corDivBoard = document.querySelector('#color-palette');
 const cor = document.querySelectorAll('.color');
-/* cores nas paletas */
+const corDivPixel = document.querySelector('#pixel-board');
+const corPixel = document.querySelectorAll('.pixel');
+const inputTxt = document.querySelector('#board-size');
+const buttonInput = document.querySelector('#generate-board');
 
+/* cores nas paletas */
 cor[0].style.backgroundColor = 'black';
 cor[1].style.backgroundColor = 'red';
 cor[2].style.backgroundColor = 'orange';
@@ -23,9 +27,8 @@ function clickPalette(eventoDeOrigem) {
   }
 }
 corDivBoard.addEventListener('click', clickPalette);
+
 /* Tabela */
-const corDivPixel = document.querySelector('#pixel-board');
-const corPixel = document.querySelectorAll('.pixel');
 function clickPixel(eventoDeOrigem) {
   aux = eventoDeOrigem.target;
   if (corPixel.className === 'color') {
@@ -35,6 +38,7 @@ function clickPixel(eventoDeOrigem) {
   }
 }
 corDivPixel.addEventListener('click', clickPixel);
+
 /* Criando button */
 const buttonBoard = document.querySelector('#clear-board');
 function limparBoard() {
@@ -43,3 +47,42 @@ function limparBoard() {
   }
 }
 buttonBoard.addEventListener('click', limparBoard);
+
+/* d */
+
+function addTabelaPixel(count) {
+  for (let i = 0; i < count; i += 1) {
+    const novaDiv = document.createElement('div');
+    novaDiv.className = `lista ${i}`;
+    corDivPixel.appendChild(novaDiv);
+  }
+  const addLi = document.querySelectorAll('div');
+  for (let i = 1; i < addLi.length; i += 1) {
+    for (let j = 0; j < count; j += 1) {
+      const novaDiv = document.createElement('div');
+      novaDiv.className = 'pixel';
+      addLi[i].appendChild(novaDiv);
+    }
+  }
+}
+
+function funCompromisso() {
+  const addPixel = document.createElement('div');
+  const auxValue = inputTxt.value;
+  addPixel.innerHTML = auxValue;
+  if (auxValue.innerHTML === '' || auxValue <= 0) {
+    corDivPixel.innerHTML = '';
+    alert('Erro');
+  } else {
+    corDivPixel.innerHTML = '';
+    addTabelaPixel(auxValue);
+    /* corDivPixel.appendChild(corDivBoard); */
+  }
+}
+function funEnter(e) {
+  if (e.which === 13) {
+    funCompromisso();
+  }
+}
+inputTxt.addEventListener('keypress', funEnter);
+buttonInput.addEventListener('click', funCompromisso);
