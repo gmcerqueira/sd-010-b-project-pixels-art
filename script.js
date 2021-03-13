adc514b4fa6518e44c4b83149546a8268084da75
Anderson <trkz.anderson@gmail.com>
2021-03-13 18:46:44 -0300

    [Requisito 12] Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+  randomizePalette();
+
   generateBoard();
 
   addPixel();
@@ -16,82 +18,82 @@ let palette = document.getElementsByClassName('color');
 let selectedColor = 1;
 
 function createPixel () {
-  //seleciona a classe "corpo", 
-  //x será um array pois pode haver mais de uma classe
-  let pixelBoard = document.getElementById("pixel-board");
-  let newPixel = document.createElement("div");
+//seleciona a classe "corpo", 
+//x será um array pois pode haver mais de uma classe
+let pixelBoard = document.getElementById("pixel-board");
+let newPixel = document.createElement("div");
 
-  newPixel.setAttribute('class', 'pixel color5');
+newPixel.setAttribute('class', 'pixel color5');
 
-  pixelBoard.appendChild(newPixel)
+pixelBoard.appendChild(newPixel)
 }
 
 function addPixel () {
-  for (let indice = 0; indice < 25; indice++) {
-    createPixel();
-  }
+for (let indice = 0; indice < 25; indice++) {
+  createPixel();
+}
 }
 
 function changeClass () {
-  if (palette[0].addEventListener('click', function () {
-      changeColor(palette[0]);
-
-      //addClassName
-      palette[0].className = "color color1 selected";
-      selectedColor = 1;
-      //resetClassName
-      palette[1].className = "color color2";
-      palette[2].className = "color color3";
-      palette[3].className = "color color4";
-    })
-  ) {
-    console.log('1 if');
-  }
-
-  if (palette[1].addEventListener('click', function () {
-    changeColor(palette[1]);
+if (palette[0].addEventListener('click', function () {
+    changeColor(palette[0]);
 
     //addClassName
-    palette[1].className = "color color2 selected";
-    selectedColor = 2;
+    palette[0].className = "color color1 selected";
+    selectedColor = 1;
     //resetClassName
-    palette[0].className = "color color1";
+    palette[1].className = "color color2";
     palette[2].className = "color color3";
     palette[3].className = "color color4";
-    })
-  ) {
-    console.log('2 if');
-  }
+  })
+) {
+  console.log('1 if');
+}
 
-  if (palette[2].addEventListener('click', function () {
-    changeColor(palette[2]);
+if (palette[1].addEventListener('click', function () {
+  changeColor(palette[1]);
 
-    //addClassName
-    palette[2].className = "color color3 selected";
-    selectedColor = 3;
-    //resetClassName
-    palette[0].className = "color color1";
-    palette[1].className = "color color2";
-    palette[3].className = "color color4";
-    })
-  ) {
-    console.log('3 if');
-  }
+  //addClassName
+  palette[1].className = "color color2 selected";
+  selectedColor = 2;
+  //resetClassName
+  palette[0].className = "color color1";
+  palette[2].className = "color color3";
+  palette[3].className = "color color4";
+  })
+) {
+  console.log('2 if');
+}
 
-  if (palette[3].addEventListener('click', function () {
-    changeColor(palette[3]);
-    
-    //addClassName
-    palette[3].className = "color color4 selected";
-    selectedColor = 4;
-    //resetClassName
-    palette[0].className = "color color1";
-    palette[2].className = "color color3";
-    palette[1].className = "color color2";
-    })
-  ) {
-    console.log('4 if');
-  }
+if (palette[2].addEventListener('click', function () {
+  changeColor(palette[2]);
+
+  //addClassName
+  palette[2].className = "color color3 selected";
+  selectedColor = 3;
+  //resetClassName
+  palette[0].className = "color color1";
+  palette[1].className = "color color2";
+  palette[3].className = "color color4";
+  })
+) {
+  console.log('3 if');
+}
+
+if (palette[3].addEventListener('click', function () {
+  changeColor(palette[3]);
+  
+  //addClassName
+  palette[3].className = "color color4 selected";
+  selectedColor = 4;
+  //resetClassName
+  palette[0].className = "color color1";
+  palette[2].className = "color color3";
+  palette[1].className = "color color2";
+  })
+) {
+  console.log('4 if');
+}
 }
 
 function changeColor (palette) {
@@ -102,7 +104,7 @@ function paintPixel () {
   let pixel = document.getElementsByClassName('pixel');
 
   for (let index = 0; index < 25; index ++) {
-    if (pixel[index].addEventListener('click', function () {
+    pixel[index].addEventListener('click', function () {
       switch (selectedColor) {
         case 1:
           pixel[index].className = "pixel color1"
@@ -125,47 +127,36 @@ function paintPixel () {
           break;
       }
     })
-    ) {
-      console.log('Não foi possível identificar o pixel selecionado!');
-    }
   }
 }
 
 function clearBoard () {
   let clearButton = document.getElementById('clear-board');
 
-  if (clearButton.addEventListener('click', function () {
+  clearButton.addEventListener('click', function () {
     for (let indice = 0; indice < 25; indice++) {
       let pixel = document.getElementsByClassName('pixel');
 
       pixel[indice].setAttribute('class', 'pixel color5');
     }
   })
-  ) {
-    console.log('3 if');
-  }
 }
 
 function generateBoard () {
   let generateButton = document.getElementById('generate-board');
 
-  if (generateButton.addEventListener('click', function () {
+  generateButton.addEventListener('click', function () {
     for (let indice = 0; indice < 25; indice++) {
       let pixel = document.getElementsByClassName('pixel');
       
       pixel[indice].setAttribute('class', 'pixel color5');
     }
   })
-  ) {
-    console.log('3 if');
-  }
 }
-  
+
 function defineSize () {
   let buttonSize = document.getElementById('generate-board');
   let size = document.getElementById('board-size');
- 
-  
 
   buttonSize.addEventListener('click', function () {
     let sizeValue = size.value
@@ -182,12 +173,31 @@ function defineSize () {
   })
 }
 
-  function sizingPixel (size) {
-    let pixels = document.getElementsByClassName('pixel');
+function sizingPixel (size) {
+  let pixels = document.getElementsByClassName('pixel');
 
-    for (let indice = 0; indice < 25; indice++) {
-      console.log(size)
-      pixels[indice].style.width = size + 'px';
-      pixels[indice].style.height = size + 'px';
-    }
+  for (let indice = 0; indice < 25; indice++) {
+    console.log(size)
+    pixels[indice].style.width = size + 'px';
+    pixels[indice].style.height = size + 'px';
   }
+}
+
+function ramdomizeColor () {
+  let rgb = [];
+
+  for (let indice = 0; indice < 3; indice++){
+    rgb.push(Math.random() * 255);
+  }
+
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+}
+
+//https://pt.stackoverflow.com/questions/348981/como-colocar-valor-da-variavel-javascript-no-css
+function randomizePalette () {
+  var root = document.documentElement;
+
+  root.style.setProperty('--color2', ramdomizeColor());
+  root.style.setProperty('--color3', ramdomizeColor());
+  root.style.setProperty('--color4', ramdomizeColor());
+}
