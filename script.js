905c2b6292d25b7e9a0259fb0e0dba84b3e9b64b
renatogsouza <renatomacedo1995@gmail.com>
2021-03-12 22:03:48 -0300

    deixando o usario decidir o tamnho dos pixels

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let chamando = document.querySelectorAll('.color');
 let pixelMuda = document.getElementsByClassName('pixel');
 let btnLimpar = document.getElementById('clear-board');
 let pixelBorder = document.getElementById('pixel-board');
+let nPixel = document.getElementById('hW');
+let btnTamanho = document.getElementById('tamanho');
 
+// Variaveis para criar o rgb aleatoriamente
 let r = Math.floor(Math.random()*255);
 let g = Math.floor(Math.random()*255);
 let b = Math.floor(Math.random()*255);
@@ -27,8 +30,7 @@ function corInicial() {
   let corPreta = document.querySelector('.c1');
   corPreta.classList.add('selected');
 }
-// Funcao para ouvir o chamado de pegar a cor
-// adicionando funcao de click para pegar a cor
+// Função para pegar a cor selecionada
 for (let i = 0; i < chamando.length; i++) {
   chamando[i].addEventListener('click', function(){
     let remove = document.getElementsByClassName('selected')[0];
@@ -39,23 +41,33 @@ for (let i = 0; i < chamando.length; i++) {
 corInicial();
 
 
-// função para pintar os pixels 
+// Pinta os pixels com a cor selecionada
 for (let index = 0; index < pixelMuda.length; index++) {
   pixelMuda[index].addEventListener('click', function(){
     let corNova = document.getElementsByClassName('selected')[0].style.backgroundColor;
     pixelMuda[index].style.backgroundColor = corNova;
   });
 }
-
+// Transforma todos os pixels em branco
 btnLimpar.addEventListener('click', function(){
   for (let i = 0; i < pixelMuda.length; i++) {
   pixelMuda[i].style.backgroundColor = 'white';
   }
 });
 
+// Altera o tamanho dos pixels conforme o usario decide
+btnTamanho.addEventListener('click', function() {
+  if (parseInt(nPixel.value) < 5 || parseInt(nPixel.value) > 50){
+    alert('Digite um valor entre 5 a 50 para ser aceito');  
+  } 
+    for (let i = 0; i < pixelMuda.length; i++) {
+      pixelMuda[i].style.height = parseInt(nPixel.value) + 'px';
+      pixelMuda[i].style.width = parseInt(nPixel.value) + 'px';
+    }
+});
 
 
-
+// criando cores aleatorias
 document.getElementsByClassName('color')[0].style.backgroundColor = 'rgb(0,0,0)';
 document.getElementsByClassName('color')[1].style.backgroundColor = `rgb(${r},${g},${b})`;
 document.getElementsByClassName('color')[2].style.backgroundColor = `rgb(${b},${g},${r})`;
