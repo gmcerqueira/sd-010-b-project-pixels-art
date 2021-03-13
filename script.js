26a978f7af557b6d4e6ffad6417b3b7adb57fa08
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-03-13 00:20:42 -0300

    Commit 3 - Projeto Javascript - Pixels Art - Envio do item 9

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 // orientações para correção pela colega Trybe Duda Wiltiner
 window.onload = function() {
   pixelBoardContainer = document.getElementById('pixel-board');
-  function criaTela() {
+
+
+function createScreen() {
     let lines = columns = 5;
     for (let i = 0; i < lines; i += 1) {
       criaLinha = document.createElement('div');
@@ -15,5 +17,22 @@ window.onload = function() {
       }
     }
   }
-criaTela();
+createScreen();
 }
+
+
+// Função utilizada pelo colega Trybe Nikolas Silva
+let clearGrid= document.getElementById('clear-board');
+
+clearGrid.addEventListener('click', clearColor);
+
+function clearColor() {
+    let clearGrid = document.querySelectorAll('.pixel');
+    for (let i = 0; i < clearGrid.length; i +=1) {
+        clearGrid[i].style.backgroundColor = 'white';
+    }
+  }
+
+  
+
+
