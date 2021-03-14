a7f6257734ba5dd73e72d63338e8ea62a779b2b4
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-03-14 00:26:33 -0300

    alteração 23

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const pixelBoard = document.getElementById('pixel-board');
 
 function createBox() {
-  for (let i = 0; i < 5; i+= 1) {
+  for (let i = 0; i < 5; i += 1) {
     const line = document.createElement('div');
     line.className = 'pixelLine';
-    for (let z = 0; z < 5; z+= 1) {
+    for (let z = 0; z < 5; z += 1) {
       const cell = document.createElement('div');
       cell.className = 'pixel';
       line.appendChild(cell);
