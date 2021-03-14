8ff6b2860b0b60504b655d7564a6892d5d361320
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-03-14 00:16:26 -0300

    alterações exercicios 4/5/6

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const pixelBoard = document.getElementById("pixel-board");
+const pixelBoard = document.getElementById('pixel-board');
 
 function createBox() {
-  for (let i = 0; i < 5; i++) {
-    const line = document.createElement("div");
-    line.className = "pixelLine";
-    for (let z = 0; z < 5; z++) {
-      const cell = document.createElement("div");
-      cell.className = "pixel";
+  for (let i = 0; i < 5; i+= 1) {
+    const line = document.createElement('div');
+    line.className = 'pixelLine';
+    for (let z = 0; z < 5; z+= 1) {
+      const cell = document.createElement('div');
+      cell.className = 'pixel';
       line.appendChild(cell);
     }
     pixelBoard.appendChild(line);
