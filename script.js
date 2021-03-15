25ae58287c767257da22d28e940603f13950c13b
Gabriel Ribeiro <grbeiro237@gmail.com>
2021-03-15 15:22:19 -0300

    parte 2 corrigida 6

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const palletes = Array.from(document.querySelectorAll('.color'));
+let colorPalette = document.getElementById('color-palette');
+let black = colorPalette.children[0].children[0];
+let cor1 = colorPalette.children[0].children[1];
+let cor2 = colorPalette.children[0].children[2];
+let cor3 = colorPalette.children[0].children[3];
 
-const palleteColors = () => {
-  let r = Math.ceil(Math.random() * 100);
-  let g = Math.ceil(Math.random() * 30);
-  let b = Math.ceil(Math.random() * 0);
-  palletes.map((pallete, index) => {
-    if (!index) {
-      pallete.style.backgroundColor = 'black';
-    } else {
-      pallete.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-      r += 55;
-      g += 75;
-      b += 105;
-    }
-  });
-};
-palleteColors();
+// código retirado do forum https://stackoverflow.com/questions/1484506/random-color-generator
+
+function getRancomColor() {
+  let color = '#';
+   const letters = '0123456789ABCDEF';
+  for (let index = 0; index < 6; index += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+black.style.backgroundColor = 'black';
+cor1.style.backgroundColor = getRancomColor();
+cor2.style.backgroundColor = getRancomColor();
+cor3.style.backgroundColor = getRancomColor();
