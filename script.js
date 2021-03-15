34b5293135e7ef4045e18c7813cec9b4c24aeb29
Holistic <paulofreire.si@outlook.com>
2021-03-15 04:34:06 -0300

    I saw a penguim yesterday

--- script.js
+++ script.js
@@ -1,58 +1,77 @@
-const boxes = 5;
-const table = document.getElementById('pixel-board');
-const pixelID = document.getElementsByClassName('pixel');
-const blackP = document.getElementById('one');
-const redP = document.getElementById('two');
-const greenP = document.getElementById('three');
-const yellowP = document.getElementById('four');
+function createDiv() {
+  const paletteDiv = document.createElement('div');
+  paletteDiv.className = 'color';
+  return paletteDiv;
+}
 
-function pixelBoard() {
-  for (let i = 0; i < boxes; i += 1) {
-    const createRow = document.createElement('tr');
-    table.appendChild(createRow);
-    for (let i2 = 0; i2 < boxes; i2 += 1) {
-      const createBox = document.createElement('td');
-      table.appendChild(createBox).className = 'pixel';
-      pixelID[pixelID.length - 1].id = `cell-${i + 1}-${i2 + 1}`;
-      pixelID[pixelID.length - 1].style.background = '#ffffff';
+function spawn(element) {
+  const elementMain = document.querySelector('main');
+  elementMain.appendChild(element);
+  return elementMain;
+}
+
+const title = document.createElement('h1');
+title.id = 'title';
+title.innerText = 'Paleta de Cores';
+spawn(title);
+
+const colorP = document.createElement('section');
+colorP.id = 'color-palette';
+
+for (let index = 0; index < 4; index += 1) {
+  colorP.appendChild(createDiv());
+}
+spawn(colorP);
+
+const black = document.querySelector('#color-palette').firstChild;
+black.id = 'black';
+const violet = document.querySelector('#color-palette').firstChild.nextSibling;
+violet.id = 'violet';
+const blue = document.querySelector('#color-palette').firstChild.nextSibling.nextSibling;
+blue.id = 'blue';
+const choco = document.querySelector('#color-palette').lastChild;
+choco.id = 'choco';
+document.querySelector('#black').classList = 'color selected';
+
+const boardPixel = document.createElement('div');
+boardPixel.id = 'pixel-board';
+
+for (let column = 0; column < 5; column += 1) {
+  const lineDiv = document.createElement('div');
+  lineDiv.className = 'line';
+  for (let line = 0; line < 5; line += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixel.id = 'white';
+    lineDiv.appendChild(pixel);
+  }
+  boardPixel.appendChild(lineDiv);
+}
+spawn(boardPixel);
+
+const divsColors = document.getElementsByClassName('color');
+let idColor = '';
+for (let index = 0; index < divsColors.length; index += 1) {
+  divsColors[index].addEventListener('click', (event) => {
+    divsColors[0].className = 'color';
+    divsColors[1].className = 'color';
+    divsColors[2].className = 'color';
+    divsColors[3].className = 'color';
+    if (event.target.className === 'color') {
+      event.target.className = 'color selected';
+      idColor = event.target.id;
     }
-  } 
+  });
 }
 
-function black() {
-  const select = document.querySelector('.selected');
-  select.classList.remove('selected');
-  blackP.classList.add('selected');
+const divsPixel = document.querySelectorAll('.pixel');
+
+for (let index = 0; index < divsPixel.length; index += 1) {
+  divsPixel[index].addEventListener('click', (event) => {
+    if (idColor === '') {
+      event.target.id = 'black';
+    } else {
+      event.target.id = idColor;
+    }
+  });
 }
-
-function red() {
-  const select = document.querySelector('.selected');
-  select.classList.remove('selected');
-  redP.classList.add('selected');
-}
-
-function green() {
-  const select = document.querySelector('.selected');
-  select.classList.remove('selected');
-  greenP.classList.add('selected');
-}
-
-function yellow() {
-  const select = document.querySelector('.selected');
-  select.classList.remove('selected');
-  yellowP.classList.add('selected');
-}
-
-function cleanNow() {
-  const button = document.getElementById('clean');
-  button.addEventListener('click', clean);
-}
-
-blackP.addEventListener('click', black);
-redP.addEventListener('click', red);
-greenP.addEventListener('click', green);
-yellowP.addEventListener('click', yellow);
-
-pixelBoard();
-listen();
-cleanNow();
