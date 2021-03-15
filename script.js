3b26531a7854c5cafea933fdd46ecff6e75fb27a
Aderson Santos <assaderson@gmail.com>
2021-03-15 14:54:28 -0300

    Criando no script.js os pixels

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const color = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector("#pixel-board");
 
-console.log('Deu certo');
+function criacaoPixels() {
+  for (let i = 0; i < 5; i += 1) {
+    const linha = document.createElement('div');
+    pixelBoard.appendChild(linha);
+
+    for (let x = 0; x < 5; x += 1) {
+      const coluna = document.createElement('div');
+      coluna.className = 'pixel';
+      linha.appendChild(coluna);
+    }
+  }
+};
+criacaoPixels();
