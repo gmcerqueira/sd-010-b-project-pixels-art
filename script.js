c912ae1170d8cfbf0232ddc4b2e41329f683751a
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-15 16:53:33 -0300

    Adicionado o requisito 2 e 3.

---
+++ script.js
@@ -0,0 +1,6 @@
+let escolhaCores = document.getElementsByClassName('color');
+let cores = ['black', 'red', 'blue', 'green'];
+for (i = 0; i < escolhaCores.length; i++) {
+  console.log(escolhaCores[i].style.backgroundColor = cores[i]);
+}
+
