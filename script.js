819607e619dae9d31f28e1ecfd1d27c0a3d0c75b
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-15 04:13:41 -0300

    Refatocao requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let table = document.getElementById('pixel-board');
 
-for (let cl = 0; cl < 6; cl += 1 ){
+for (let cl = 0; cl < 5; cl += 1 ){
     let column = document.createElement('tr');
-        for(let ln = 0; ln < 6; ln += 1){
+        for(let ln = 0; ln < 5; ln += 1){
            let line = document.createElement('td');
            line.setAttribute('class' , 'pixel');
            column.appendChild(line); 
