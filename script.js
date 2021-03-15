6a0ec3edb1f942cc152bf7d64e9615dd48d5584d
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-15 15:09:51 -0400

    Requisito 06, definida cor inicial padrão.

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const colorsList = ['black', 'red', 'green', 'blue'];
 
+let ativo;
+
 const palletColors = document.querySelector('#color-palette');
 
 for (let index = 0; index < colorsList.length; index += 1) {
   const color = colorsList[index];
   const palletColorItem = document.createElement('div');
   palletColorItem.classList.add('color');
+
+  if (index === 0) {
+    palletColorItem.classList.add('selected');
+    ativo = 0;
+  }
   palletColorItem.style.background = color;
 
   palletColors.appendChild(palletColorItem);
 }
 
-const pixelBoard = document.querySelector('#pixel-board')
+const pixelBoard = document.querySelector('#pixel-board');
 
 for (let index = 0; index < 25; index += 1) {
   const pixel = document.createElement('span');
   pixel.classList.add('pixel');
 
   pixelBoard.appendChild(pixel);
+}
+
+let quadradosCores = document.getElementsByClassName('color');
+
+for (let indexEvent = 0; indexEvent < quadradosCores.length; indexEvent += 1) {
+  quadradosCores[indexEvent].addEventListener('click', function (evento) {
+    quadradosCores[ativo].classList.remove('selected');
+    quadradosCores[indexEvent].classList.add('selected');
+    ativo = indexEvent;
+  });
 }
