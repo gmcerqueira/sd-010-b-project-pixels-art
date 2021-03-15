620d0be0f7e50a7e5e15cb92d6dab05f754481fc
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-15 04:52:34 -0300

    Refatocao requisito 6

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let table = document.getElementById('pixel-board');
 
 for (let cl = 0; cl < 5; cl += 1 ){
-    let column = document.createElement('tr');
-        for(let ln = 0; ln < 5; ln += 1){
-           let line = document.createElement('td');
-           line.setAttribute('class' , 'pixel');
+  let column = document.createElement('tr');
+    for(let ln = 0; ln < 5; ln += 1){
+      let line = document.createElement('td');
+        line.setAttribute('class' , 'pixel');
            column.appendChild(line); 
         }
         table.appendChild(column);    
 }
+
+		const btn = document.querySelector('#clear-board');
+		btn.addEventListener('click', function () {
+		const clear = document.getElementsByClassName('pixel');
+
+	for (let index = 0; index < clear.length; index += 1) {
+		clear[index].style.backgroundColor = 'white';
+	} 
+});
+
+	
