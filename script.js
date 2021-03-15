b2e43125f61814fbd26291f4bd3ed5f7378dc91b
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-15 11:22:48 -0400

    Requisito 04, adicionado quadro de pixels com 25 pixels.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const colorsList = ['black', 'red', 'green', 'blue'];
 const palletColors = document.querySelector('#color-palette');
 
 for (let index = 0; index < colorsList.length; index += 1) {
-    const color = colorsList[index];
-    const palletColorItem = document.createElement('div');
-    palletColorItem.classList.add('color');
-    palletColorItem.style.background = color;
+  const color = colorsList[index];
+  const palletColorItem = document.createElement('div');
+  palletColorItem.classList.add('color');
+  palletColorItem.style.background = color;
 
-    palletColors.appendChild(palletColorItem);
+  palletColors.appendChild(palletColorItem);
 }
