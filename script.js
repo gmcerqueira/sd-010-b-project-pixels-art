f9b24a4136d1c77cc613def61d1e74db366f7be2
Aderson Santos <assaderson@gmail.com>
2021-03-16 13:40:43 -0300

    Coloquei no arquivo script.js a seleção de cor para prenncher os campos pixels

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const pixelBoard = document.querySelector("#pixel-board");
+const caixa = document.querySelectorAll(".color");
 
 function criacaoPixels() {
   for (let i = 0; i < 5; i += 1) {
@@ -13,3 +14,22 @@ function criacaoPixels() {
   }
 };
 criacaoPixels();
+
+
+function selecaoPixelCor(){
+  for(let i = 0; i < caixa.length; i += 1){
+    caixa[i].addEventListener("click", function(){
+      if(caixa[i].getAttribute("class") === "selected color"){
+        console.log("Deu certo");
+      }else{
+        for(let x = 0; x < caixa.length; x +=1){
+          if(caixa[x].getAttribute("class") === "selected color"){
+            caixa[x].setAttribute("class", "color");
+          }
+        }
+        caixa[i].setAttribute("class", "selected color");
+      }
+    });
+  }
+}
+selecaoPixelCor();
