e49b050af3e3b8eaf0fa5580bd88abb12d6b7587
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-17 14:49:00 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let colorPixels = document.querySelectorAll('.color');
-let colors = ['black', 'red', 'blue', 'green'];
-let colorPallete = document.querySelector('#color-palette');
-let colorsPalette = ['black', 'red', 'blue', 'green'];
+const colorPixels = document.querySelectorAll('.color');
+const colors = ['black', 'red', 'blue', 'green'];
+const colorPallete = document.querySelector('#color-palette');
+const colorsPalette = ['black', 'red', 'blue', 'green'];
+const pixelBoard = document.querySelector('#pixel-board');
 
 for (let i = 0; i < colorPixels.length; i += 1) {
   colorPixels[i].style.backgroundColor = colors[i];
@@ -16,3 +17,23 @@ function createColorPalette(colors) {
   }
 }
 createColorPalette(colorsPalette);
+
+function createPixels(size) {
+  const pixels = document.createElement('tr');
+  pixels.className = 'pixels';
+  for (let i = 0; i < size; i +=1) {
+    const pixel = document.createElement('td');
+    pixel.className = 'pixel';
+    pixels.appendChild(pixel);
+
+  }
+  return pixels;
+}
+
+function createBoard(size) {
+  for (let i = 0; i < size; i +=1) {
+    pixelBoard.appendChild(createPixels(size));
+  }
+}
+
+createBoard(5);
