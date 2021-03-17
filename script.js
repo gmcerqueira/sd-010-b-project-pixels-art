3a0ad6ff01e1c2ae2048730a1835d3c4c16d22ad
lucas <peixotolucas516@gmail.com>
2021-03-17 20:59:04 -0300

    projeto refeito, paleta e quadro de pixels criados dinamicamente

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
-console.log("ola")
+const containerPalette = document.querySelector('#color-palette');
+const containerQuadro = document.querySelector('#pixel-board');
+const colorsPalette = document.getElementsByClassName('color');
+const pixels = document.getElementsByClassName('pixel');
+const botaoLimparCores = document.querySelector('#clear-board');
+const textoInput = document.querySelector('#board-size')
+const botaoGerarPixels = document.querySelector('#generate-board');
+let quadroPixelInicial = 5;
 
-
-
-
-function colorSet {
-  let colour = document.getElementsByClassName
+function createPalette() {
+  for(let i = 0; i < 4; i += 1) {
+    const selectors = document.createElement('div');
+    selectors.className = 'color';
+    containerPalette.appendChild(selectors);
+    if (i === 0) {
+      selectors.className += ' selected';
+    }
+  }
+    
 }
+
+createPalette();
+
+function createPixels() {
+  for (let index = 0; index < quadroPixelInicial; index += 1) {
+    const line = document.createElement('div');
+    containerQuadro.appendChild(line);
+
+    for (let index2 = 0; index2 < quadroPixelInicial; index2 += 1) {
+      const column = document.createElement('div');
+      column.className = 'pixel';
+      line.appendChild(column);
+    }
+  }
+}
+
+createPixels();
+
+
+
+document.getElementsByClassName('color')[0].style.backgroundColor = "black"
+document.getElementsByClassName('color')[1].style.backgroundColor = "red"
+document.getElementsByClassName('color')[2].style.backgroundColor = "green"
+document.getElementsByClassName('color')[3].style.backgroundColor = "blue"
+
+
+function changeColor () {
+  let paletaDeCores = document.querySelector('#color-palette');
+  
+
+
+
+
+}
+
+
