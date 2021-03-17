cb4662f4273f2152c29dd4b5145c419955cfc2e8
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-17 18:12:24 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const colorPixels = document.querySelectorAll('.color');
-const colors = ['black', 'red', 'blue', 'green'];
+const color = ['black', 'red', 'blue', 'green'];
 const colorPallete = document.querySelector('#color-palette');
 const colorsPalette = ['black', 'red', 'blue', 'green'];
 const pixelBoard = document.querySelector('#pixel-board');
@@ -10,9 +10,11 @@ for (let i = 0; i < colorPixels.length; i += 1) {
 
 function createColorPalette(colors) {
   for (let i = 0; i < colors.length; i += 1) {
-    const colorPixels = document.createElement('div');  
+    const colorPixels = document.createElement('div'); 
     colorPixels.className = 'color';
     colorPixels.style.backgroundColor = colors[i];
+    colorPixels.addEventListener('click', changeSelected);
+    firstSelect(colorPixels)
     colorPallete.appendChild(colorPixels);
   }
 }
@@ -24,6 +26,7 @@ function createPixels(size) {
   for (let i = 0; i < size; i += 1) {
     const pixel = document.createElement('td');
     pixel.className = 'pixel';
+    pixel.addEventListener('click', paint);
     pixels.appendChild(pixel);
   }
   return pixels;
@@ -35,3 +38,23 @@ function createBoard(size) {
   }
 }
 createBoard(5);
+
+function firstSelect(element) {
+  if (element.style.backgroundColor === 'black') {
+    element.className = 'color selected';
+  }
+}
+
+function changeSelected(event) {
+  let lastSelected = document.querySelector('.selected')
+  lastSelected.className = 'color';
+  let newSelected = event.target;
+  newSelected.className = 'color selected';
+}
+
+function paint(event) {
+let selectedColor = document.querySelector('.selected');
+let color = selectedColor.style.backgroundColor;
+let selectedPixel = event.target;
+selectedPixel.style.backgroundColor = color;
+}
