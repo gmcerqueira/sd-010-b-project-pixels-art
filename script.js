c17e925ac51d2634df29bb68d8ec9ecd6a794787
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-17 16:57:12 -0300

    Corrigindo novamente o requisito 4.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function botaoCores () {
 let escolhaCores = document.getElementsByClassName('color');
 let cores = ['black', 'red', 'blue', 'green'];
 
-for (i = 0; i < escolhaCores.length; i++) {
+  for (i = 0; i < escolhaCores.length; i++) {
   escolhaCores[i].style.backgroundColor = cores[i];
-}
+  }
 }
 botaoCores();
 
@@ -12,11 +12,11 @@ function quadroColorir () {
 let quadro = document.querySelector('#pixel-board');
 
   for (i = 0; i < 5; i++) {
-  quadroLinha = document.createElement('tr');
+  let quadroLinha = document.createElement('tr');
   quadro.appendChild(quadroLinha);
   
     for (ii = 0; ii < 5; ii++) {
-    quadroColuna = document.createElement('td');
+    let quadroColuna = document.createElement('td');
     quadroColuna.className = 'pixel';
     quadroLinha.appendChild(quadroColuna);
     }
