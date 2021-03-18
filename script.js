da1b1a959e7f479fad1ee683e5ebc0a037628cc0
Cesar Lopez <cglopez874@hotmail.com>
2021-03-18 03:12:46 -0300

    commit final

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let clickb = document.getElementById("color-palette");
 clickb.addEventListener("click", recebeclick);
+let clicka = document.getElementById("pixel-board");
+clicka.addEventListener("click", paletaclick);
+
+
 function recebeclick(eventodeorigem) {
 
     let botaoClicado = eventodeorigem.target;
@@ -17,6 +21,29 @@ else {
 }
 }
 
+//pintando o quadro
+function paletaclick(eventodedestino) {
+let botaopaleta = eventodedestino.target;
+let verselected = document.querySelector(".selected");
+if (verselected.id == "firstbox") {
+    botaopaleta.style.backgroundColor = "black";
+}
+else if (verselected.id == "secondbox") {
+    botaopaleta.style.backgroundColor = "red";
+}
+else if (verselected.id == "thirdbox") {
+    botaopaleta.style.backgroundColor = "blue";
+    }
+    else if (verselected.id == "fourthbox"){
+        botaopaleta.style.backgroundColor = "green";
+    }
+
+}
+
+
+
+
+
 
 let button = document.getElementById('clear-board');
 button.addEventListener("click", pintarBranco);
@@ -24,7 +51,8 @@ button.addEventListener("click", pintarBranco);
 function pintarBranco() {
     let classe = document.querySelectorAll(".colorpixel");
     for(let index = 0; index < classe.length; index +=1) {
-    classe[index].style.backgroundColor = 'white';
+    classe[index].style.backgroundColor = "white";
+    console.log (classe);
     }
 
 }
