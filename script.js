4699447dd4c22bd648e85a04980a37f27e65738f
lucas <peixotolucas516@gmail.com>
2021-03-17 21:58:59 -0300

    revisao

--- script.js
+++ script.js
@@ -67,19 +67,14 @@ pixelsPaint()
 
 
 
+
+
 document.getElementsByClassName('color')[0].style.backgroundColor = "black"
 document.getElementsByClassName('color')[1].style.backgroundColor = "red"
 document.getElementsByClassName('color')[2].style.backgroundColor = "green"
 document.getElementsByClassName('color')[3].style.backgroundColor = "blue"
 
 
-function changeColor () {
-  let paletaDeCores = document.querySelector('#color-palette');
-  
 
-
-
-
-}
 
 
