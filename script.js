d39b23f26a8aa0f831470492f155523f514f09b1
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-03-18 04:04:02 -0300

    maybe exercise 7?

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// const colors = document.getElementsByClassName('color');
-// function myfunction(event) {
-//   let selected = document.getElementsByClassName('selected');
-//   event.target.classList.remove('color');
-//   event.target.classList.add('selected');
-
-// let selected = document.getElementsByClassName('selected');
-
-// for (let index = 0; index < colors.length; index = +1) {
-//   if (colors[index] === selectedColor) {
-//     alert('ping');
-// surprise for next time
-
-// document.querySelector('#color-palette').addEventListener('click', myfunction);
 document.getElementsByClassName('color')[0].className = 'color selected';
+
+const color = document.getElementsByClassName('color');
+const palette = document.getElementById('color-palette');
+function colorPicker() {
+  for (let index = 0; index < color.length; index += 1) {
+    color[index].addEventListener('click', function () {
+      palette.querySelector('.selected').classList.remove('selected');
+      this.classList.add('selected');
+    });
+  }
+}
+
+colorPicker();
