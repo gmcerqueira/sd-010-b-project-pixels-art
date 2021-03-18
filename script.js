5038513026fa3d3f5f8f2de95a9c39b5681652e1
Cesar Lopez <cglopez874@hotmail.com>
2021-03-17 23:48:34 -0300

    exercicio 7-check

--- script.js
+++ script.js
@@ -9,20 +9,14 @@ function recebeclick(eventodeorigem) {
 for(let index = 0; index < classe.length; index +=1) {
 let corClicada = classe[index];
 
-if (corClicada = botaoClicado){
-corClicada.className = "caixa color selected";
-}
-else {
+if (corClicada != botaoClicado){
 corClicada.className = "caixa color";
 }
+
+else {
+    corClicada.className = "caixa color selected";
 }
-
-    
-    botaoClicado.className = "caixa color selected";
-    console.log (botaoClicado);
-    console.log (classe);
-
-
+}
 }
 
 
